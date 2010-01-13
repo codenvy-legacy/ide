601ce506a5fb84671542fb40a7361f743fbf842e
Dmytro Nochevnov <dnochevnov@exoplatform.com>
2010-01-13 15:15:20 +0000

    fixed gadget name DevTool on IDE

--- client/src/main/java/org/exoplatform/ideall/client/IDEGadget.java
+++ client/src/main/java/org/exoplatform/ideall/client/IDEGadget.java
@@ -9,7 +9,7 @@ import com.google.gwt.gadgets.client.Gadget.ModulePrefs;
  * @author <a href="mailto:dmitry.ndp@exoplatform.com.ua">Dmytro Nochevnov</a>
  * @version $Id: $
 */
-@ModulePrefs(title = "DevTool", author = "eXo Platform", author_email = "info@exoplatform.com.ua", height = 500, description = "DevTool")
+@ModulePrefs(title = "IDEall", author = "eXo Platform", author_email = "info@exoplatform.com.ua", height = 500, description = "IDEall")
 public class IDEGadget extends Gadget<UserPreferences>
 {
 
