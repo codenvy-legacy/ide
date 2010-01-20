6d8129cde4d9365aa39384076c267fa84552ce64
Vitaliy Parfonov <vparfonov@exoplatform.com>
2010-01-20 14:03:58 +0000

    WBT-302 : Move Loopback and other dedicated services from WS to IDE components

---
+++ component/exo.ideall.component.commons/src/test/java/org/exoplatform/ideall/shindig/oauth/CreateSecurityTokenTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ideall.shindig.oauth;
+
+import java.io.IOException;
+
+import org.apache.shindig.common.crypto.BlobCrypterException;
+import org.exoplatform.ideall.AbstractResourceTest;
+import org.exoplatform.ideall.shindig.oauth.SecurityTokenGenerator;
+import org.exoplatform.ideall.shindig.oauth.TokenRequest;
+import org.exoplatform.ideall.shindig.oauth.TokenResponse;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+/**
+ * Created by The eXo Platform SAS.
+ * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
+ * @version $Id: $
+*/
+public class CreateSecurityTokenTest extends AbstractResourceTest
+{
+   private String gadgetURL = "http://loaclhost:8080/gadgets/somegadget.xml";
+
+   private String owner = "root";
+
+   private String viewer = "root";
+
+   private Long moduleId = 0L;
+
+   private String container = "default";
+
+   private String domain = null;
+   
+   private String keyFile = "src/test/resources/conf/key.txt";
+
+   
+   public void testCreateSecurityToken() throws IOException, BlobCrypterException
+   {
+      TokenRequest tokenRequest = new TokenRequest(gadgetURL, owner, viewer, moduleId, container, domain);
+      System.out.println("CreateSecurityTokenTest.testCreateSecurityToken()" + tokenRequest.toString());
+      TokenResponse token = SecurityTokenGenerator.createToken(tokenRequest, keyFile);
+      System.out.println("CreateSecurityTokenTest.testCreateSecurityToken()"  + token.toString());
+   }
+   
+}
