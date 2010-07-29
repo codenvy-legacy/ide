dc1db26eed14ad70b3717d7e9c34dd39bd081ec4
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2010-07-29 13:58:43 +0000

    IDE-194

---
+++ exo-ide-module-gadget-server/src/main/java/org/exoplatform/ide/shindig/oauth/RestSecurityTokenGenerator.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ide.shindig.oauth;
+
+import java.io.IOException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.shindig.common.crypto.BlobCrypterException;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.ValueParam;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.rest.resource.ResourceContainer;
+
+/**
+ * Created by The eXo Platform SAS.
+ * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
+ * @version $Id: $
+*/
+@Path("/services/shindig/securitytoken")
+public class RestSecurityTokenGenerator implements ResourceContainer
+{
+   /**
+     * Class logger.
+     */
+   private final Log log = ExoLogger.getLogger("rest.RestSecurityTokenGenerator");
+   
+   private String keyFile;
+   
+   private static String DEFAULT_KEY_FILE = "key.txt";
+   
+   public RestSecurityTokenGenerator(InitParams initParams)
+   {
+      if (initParams != null) {
+         ValueParam param = initParams.getValueParam("keyFile");
+         if (param != null)
+            keyFile = param.getValue();
+         else
+            keyFile = DEFAULT_KEY_FILE;
+      }
+      else 
+         keyFile = DEFAULT_KEY_FILE;
+   }
+   
+   @POST
+   @Path("/createToken")
+   @Produces(MediaType.APPLICATION_JSON)
+   @Consumes(MediaType.APPLICATION_JSON)
+   public Response createToken(TokenRequest tokenRequest)  {
+      try
+      {
+         return Response.ok(SecurityTokenGenerator.createToken(tokenRequest,keyFile)).build();
+      }
+      catch (IOException e)
+      {
+         if (log.isDebugEnabled())
+            e.printStackTrace();
+         throw new WebApplicationException(e);
+      }
+      catch (BlobCrypterException e)
+      {
+         if (log.isDebugEnabled())
+           e.printStackTrace();
+         throw new WebApplicationException(e);
+      } 
+      
+   }
+  
+}
