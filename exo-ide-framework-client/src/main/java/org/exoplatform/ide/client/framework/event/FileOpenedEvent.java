5e1390fb2778392f86414aa0a4aa7aa99fbd7582
Oksana Vereshchaka <overeshchaka@exoplatform.com>
2010-09-20 09:09:09 +0000

    IDE-288: Added EditorGoToPositionEvent and FileOpenedEvent

---
+++ exo-ide-framework-client/src/main/java/org/exoplatform/ide/client/framework/event/FileOpenedEvent.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2003-2010 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ide.client.framework.event;
+
+import org.exoplatform.ide.client.module.vfs.api.File;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * Created by The eXo Platform SAS.
+ * @author <a href="mailto:tnemov@gmail.com">Evgen Vidolob</a>
+ * @version $Id: $
+*/
+public class FileOpenedEvent extends GwtEvent<FileOpenedHandler>
+{
+
+   public static final GwtEvent.Type<FileOpenedHandler> TYPE = new GwtEvent.Type<FileOpenedHandler>();
+
+   private File file;
+
+   private String editor;
+
+   public FileOpenedEvent(File file)
+   {
+      this.file = file;
+   }
+
+   public FileOpenedEvent(File file, String editor)
+   {
+      this(file);
+      this.editor = editor;
+   }
+
+   public File getFile()
+   {
+      return file;
+   }
+
+   public String getEditor()
+   {
+      return editor;
+   }
+   
+   @Override
+   protected void dispatch(FileOpenedHandler handler)
+   {
+      handler.onFileOpened(this);
+   }
+
+   @Override
+   public com.google.gwt.event.shared.GwtEvent.Type<FileOpenedHandler> getAssociatedType()
+   {
+      return TYPE;
+   }
+
+}
