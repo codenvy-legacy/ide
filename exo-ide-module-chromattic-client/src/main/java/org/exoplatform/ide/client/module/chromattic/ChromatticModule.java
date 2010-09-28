9a69c7612058ec7f8efcfcbe7dc15424920e1d43
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2010-09-28 13:32:17 +0000

    IDE-275 add module for chromattic data object

---
+++ exo-ide-module-chromattic-client/src/main/java/org/exoplatform/ide/client/module/chromattic/ChromatticModule.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.client.module.chromattic;
+
+import org.exoplatform.gwtframework.commons.rest.MimeType;
+import org.exoplatform.ide.client.framework.control.NewItemControl;
+import org.exoplatform.ide.client.framework.control.event.RegisterControlEvent;
+import org.exoplatform.ide.client.framework.module.IDEModule;
+
+import com.google.gwt.event.shared.HandlerManager;
+
+/**
+ * Created by The eXo Platform SAS .
+ *
+ * @author <a href="tnemov@gmail.com">Evgen Vidolob</a>
+ * @version $Id: Sep 28, 2010 $
+ *
+ */
+public class ChromatticModule implements IDEModule
+{
+   
+   private HandlerManager eventBus;
+
+   /**
+    * @param eventBus
+    */
+   public ChromatticModule(HandlerManager eventBus)
+   {
+      this.eventBus = eventBus;
+      
+      eventBus.fireEvent(new RegisterControlEvent(new NewItemControl("File/New/New Chromattic Data Object",
+         "Chromattic Data Object", "Create Chromattic Data Object", Images.FileType.CHROMATTIC,
+         MimeType.CHROMATTIC_DATA_OBJECT)));
+   }
+   
+   
+}
