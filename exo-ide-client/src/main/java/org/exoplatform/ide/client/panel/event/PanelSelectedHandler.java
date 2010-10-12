830c128707350507524e0fe8f6e2474b7756bab0
Vitaliy Guliy <vitaliy.gulyy@exoplatform.com>
2010-10-12 14:13:38 +0000

    IDE-336

--- exo-ide-client/src/main/java/org/exoplatform/ide/client/panel/event/PanelSelectedHandler.java
+++ exo-ide-client/src/main/java/org/exoplatform/ide/client/panel/event/PanelSelectedHandler.java
@@ -18,8 +18,6 @@ package org.exoplatform.ide.client.panel.event;
 
 import com.google.gwt.event.shared.EventHandler;
 
-
-
 /**
  * Created by The eXo Platform SAS.
  * @author <a href="mailto:tnemov@gmail.com">Evgen Vidolob</a>
@@ -27,8 +25,7 @@ import com.google.gwt.event.shared.EventHandler;
 */
 public interface PanelSelectedHandler extends EventHandler
 {
-   
+
    void onPanelSelected(PanelSelectedEvent event);
 
 }
-
