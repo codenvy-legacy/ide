6b5e64fa3d7dfad0bbd587dd878e1c97cd133e2e
Maxim Musienko <maxim.musienko@exoplatform.com>
2010-11-05 15:36:47 +0000

    IDE-353 Musienko Maxim (fix)

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/edit/DeleteCurrentLineTest.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/edit/DeleteCurrentLineTest.java
@@ -45,11 +45,11 @@ public class DeleteCurrentLineTest extends BaseTest
       runCommandFromMenuNewOnToolbar(MenuCommands.New.HTML_FILE);
       Thread.sleep(TestConstants.EDITOR_OPEN_PERIOD);
       String text1 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       saveAsUsingToolbarButton("test.html");
       Thread.sleep(TestConstants.SLEEP);
       assertEquals("1 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       //      Click on "Edit->Delete Current Line" top menu command.
       runTopMenuCommand(MenuCommands.Edit.EDIT_MENU, MenuCommands.Edit.DELETE_CURRENT_LINE);
@@ -66,10 +66,10 @@ public class DeleteCurrentLineTest extends BaseTest
       selenium.keyDown("//body[@class='editbox']", "D");
       selenium.keyUp("//body[@class='editbox']", "D");
       selenium.controlKeyUp();
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       String text3 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertFalse(text2.equals(selenium.getText("//body[@class='editbox']")));
       Thread.sleep(TestConstants.SLEEP);
       assertEquals("1 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
@@ -78,50 +78,50 @@ public class DeleteCurrentLineTest extends BaseTest
       for (int i = 0; i < 2; i++)
       {
          selenium.keyDownNative("" + java.awt.event.KeyEvent.VK_DOWN);
-         Thread.sleep(TestConstants.SLEEP_SHORT);
+         Thread.sleep(TestConstants.SLEEP_SHORT*2);
          selenium.keyUpNative("" + java.awt.event.KeyEvent.VK_DOWN);
       }
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       //      Press "Ctrl+D" keys.
       selenium.controlKeyDown();
       selenium.keyDown("//body[@class='editbox']", "D");
       selenium.keyUp("//body[@class='editbox']", "D");
       selenium.controlKeyUp();
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       String text4 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertFalse(text3.equals(selenium.getText("//body[@class='editbox']")));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals("3 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       //    Click on "Edit->Delete Current Line" top menu command.
       selenium.mouseDownAt("//td[@class='exo-menuBarItem' and @menubartitle='Edit']", "");
       selenium.mouseDownAt("//td[@class='exo-popupMenuTitleField']/nobr[text()='Delete Current Line']", "");
       
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       String text5 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertFalse(text4.equals(selenium.getText("//body[@class='editbox']")));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals("3 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       //      Click on "Edit->Delete Current Line" top menu command.
 
       runTopMenuCommand(MenuCommands.Edit.EDIT_MENU, MenuCommands.Edit.DELETE_CURRENT_LINE);
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       String text6 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertFalse(text5.equals(selenium.getText("//body[@class='editbox']")));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals("3 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       //      Create empty "test1.txt" file end open this one in another tab.
       runCommandFromMenuNewOnToolbar(MenuCommands.New.TEXT_FILE);
       saveAsUsingToolbarButton("test1.txt");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       Thread.sleep(TestConstants.SLEEP);
       assertEquals("1 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
       selenium.selectFrame("//div[@class='tabSetContainer']/div/div[3]//iframe");
@@ -132,19 +132,19 @@ public class DeleteCurrentLineTest extends BaseTest
       selenium.keyDown("//body[@class='editbox']", "13");
       selenium.keyUp("//body[@class='editbox']", "13");
       selenium.typeKeys("//body[@class='editbox']", "line2");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       String texty1 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       selenium.selectFrame("relative=top");
       //      Go to line 2 and click on "Edit->Delete Current Line" top menu command.
 
       runTopMenuCommand(MenuCommands.Edit.EDIT_MENU, MenuCommands.Edit.DELETE_CURRENT_LINE);
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       assertFalse(texty1.equals(selenium.getText("//body[@class='editbox']")));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       String texty2 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals("2 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
       //      Press "Ctrl+D" keys.
       selenium.controlKeyDown();
@@ -153,13 +153,13 @@ public class DeleteCurrentLineTest extends BaseTest
       selenium.controlKeyUp();
 
       selectEditorTab(0);
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       selenium.click("scLocator=//TabSet[ID=\"ideEditorFormTabSet\"]/tab[index=1]/");
       selectEditorTab(1);
       Thread.sleep(TestConstants.SLEEP);
       
       assertEquals("2 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals(texty2, selenium.getText("//body[@class='editbox']"));
       //      Go to line 1 and click on "Edit->Delete Current Line" top menu command.
       for (int i = 0; i < 1; i++)
@@ -167,38 +167,38 @@ public class DeleteCurrentLineTest extends BaseTest
          selenium.keyDownNative("" + java.awt.event.KeyEvent.VK_UP);
          selenium.keyUpNative("" + java.awt.event.KeyEvent.VK_UP);
       }
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       runTopMenuCommand(MenuCommands.Edit.EDIT_MENU, MenuCommands.Edit.DELETE_CURRENT_LINE);
 
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       String texty3 = selenium.getText("//body[@class='editbox']");
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals("1 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals(texty3, selenium.getText("//body[@class='editbox']"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       //      Press "Ctrl+D" keys.
       selenium.controlKeyDown();
       selenium.keyDown("//body[@class='editbox']", "D");
       selenium.keyUp("//body[@class='editbox']", "D");
       selenium.controlKeyUp();
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       assertEquals(texty3, selenium.getText("//body[@class='editbox']"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
 
       selectEditorTab(0);
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       //      Return to the tab with file "test.html".
 
       selectEditorTab(1);
       Thread.sleep(TestConstants.SLEEP);
       
       assertEquals("1 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
 
       selectEditorTab(0);
-      Thread.sleep(TestConstants.SLEEP_SHORT);
+      Thread.sleep(TestConstants.SLEEP_SHORT*2);
       
       assertEquals("3 : 1", selenium.getText("//td[@class='exo-statusText-table-middle']/nobr"));
    }
