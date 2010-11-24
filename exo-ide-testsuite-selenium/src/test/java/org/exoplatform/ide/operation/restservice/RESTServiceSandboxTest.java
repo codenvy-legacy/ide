d8ab004275422c4aaf50d6d5665643d0766113c1
Anna  Zhuleva <anna.zhuleva@exoplatform.com>
2010-11-24 13:58:24 +0000

    IDE-430: add test deploy/undeploy to/from sandbox

---
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/restservice/RESTServiceSandboxTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.operation.restservice;
+
+import static org.junit.Assert.*;
+
+import org.exoplatform.common.http.client.ModuleException;
+import org.exoplatform.ide.BaseTest;
+import org.exoplatform.ide.CloseFileUtils;
+import org.exoplatform.ide.MenuCommands;
+import org.exoplatform.ide.TestConstants;
+import org.exoplatform.ide.Utils;
+import org.exoplatform.ide.VirtualFileSystemUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:zhulevaanna@gmail.com">Ann Zhuleva</a>
+ * @version $Id: Oct 14, 2010 $
+ *
+ */
+public class RESTServiceSandboxTest extends BaseTest
+{
+
+   private static String FILE_NAME = RESTServiceSandboxTest.class.getSimpleName()+".groovy";
+   
+   private static String TEST_FOLDER = RESTServiceSandboxTest.class.getSimpleName();
+   
+   private final static String URL = BASE_URL + REST_CONTEXT + "/" + WEBDAV_CONTEXT + "/" + REPO_NAME + "/" + WS_NAME + "/";
+
+   /**
+    * Create test folder.
+    */
+   @BeforeClass
+   public static void setUp()
+   {
+      try
+      {
+         cleanRegistry();
+         VirtualFileSystemUtils.mkcol(URL + TEST_FOLDER);
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+      catch (ModuleException e)
+      {
+         e.printStackTrace();
+      }
+   }
+   
+   /**
+    * Test deploy REST service to sandbox 
+    * and undeploy from sandbox.
+    */
+   @Test
+   public void testDeployUndeploy() throws Exception
+   {
+      Thread.sleep(TestConstants.SLEEP);
+      selectItemInWorkspaceTree(TEST_FOLDER);
+      //Create REST Service file and save it:
+      runCommandFromMenuNewOnToolbar(MenuCommands.New.REST_SERVICE_FILE);
+      Thread.sleep(TestConstants.EDITOR_OPEN_PERIOD);
+      saveAsUsingToolbarButton(FILE_NAME);
+      Thread.sleep(TestConstants.SLEEP);
+
+      //Deploy service to sandbox:
+      runTopMenuCommand(MenuCommands.Run.RUN, MenuCommands.Run.DEPLOY_SANDBOX);
+      Thread.sleep(TestConstants.SLEEP);
+      
+      //Check deploy request:
+      assertTrue(selenium.isElementPresent("scLocator=//VLayout[ID=\"ideOutputForm\"]/"));
+      String mess = selenium.getText("//div[contains(@eventproxy,'Record_0')]");
+      assertTrue(mess.contains("[INFO]"));
+      assertTrue(mess.contains(FILE_NAME + " deployed successfully."));
+      
+      //Undeploy service from sandbox:
+      runTopMenuCommand(MenuCommands.Run.RUN, MenuCommands.Run.UNDEPLOY_SANDBOX);
+      Thread.sleep(TestConstants.SLEEP);
+
+      //Check undeploy request:
+      mess = selenium.getText("//div[contains(@eventproxy,'Record_1')]");
+      assertTrue(mess.contains("[INFO]"));
+      assertTrue(mess.contains(FILE_NAME + " undeployed successfully."));
+   
+      //Try undeploy undeployed service:
+      runTopMenuCommand(MenuCommands.Run.RUN, MenuCommands.Run.UNDEPLOY_SANDBOX);
+      Thread.sleep(TestConstants.SLEEP);
+
+      mess = selenium.getText("//div[contains(@eventproxy,'Record_2')]");
+      assertTrue(mess.contains("[ERROR]"));
+      assertTrue(mess.contains(FILE_NAME + " undeploy failed. Error (400: Bad Request)"));
+      
+      CloseFileUtils.closeTab(0);
+   }
+   
+   /**
+    * Clear test results.
+    */
+   @AfterClass
+   public static void tearDown()
+   {
+      try
+      {
+         Utils.undeployService(BASE_URL, REST_CONTEXT, URL + TEST_FOLDER + "/" + FILE_NAME);
+         VirtualFileSystemUtils.delete(URL + TEST_FOLDER);
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+      catch (ModuleException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
