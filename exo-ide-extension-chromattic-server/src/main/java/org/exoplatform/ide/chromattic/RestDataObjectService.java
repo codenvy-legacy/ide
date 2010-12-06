84d6a7919eaf1828178ded141d5a9a64a47d9c88
Vitaliy Parfonov <vparfonov@exoplatform.com>
2010-12-06 13:31:49 +0000

    IDE-457, IDE-458 : server side impl

---
+++ exo-ide-extension-chromattic-server/src/main/java/org/exoplatform/ide/chromattic/RestDataObjectService.java
@@ -0,0 +1,161 @@
+/**
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+package org.exoplatform.ide.chromattic;
+
+import org.chromattic.dataobject.CompilationSource;
+import org.chromattic.dataobject.DataObjectService;
+import org.chromattic.dataobject.NodeTypeFormat;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.config.RepositoryConfigurationException;
+import org.exoplatform.services.jcr.core.nodetype.NodeTypeDataManager;
+import org.exoplatform.services.jcr.impl.core.nodetype.NodeTypeManagerImpl;
+import org.exoplatform.services.rest.resource.ResourceContainer;
+
+import java.io.InputStream;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/ide/chromattic/")
+public class RestDataObjectService implements ResourceContainer
+{
+
+   /**
+    * WebDav context uses for selecting jcr path of resource which is 
+    * adressed via WebDav or IDE Virtual File System service (server)
+    */
+   public static final String WEBDAV_CONTEXT = "/ide-vfs-webdav/";
+
+   private DataObjectService dataObjectService;
+
+   private RepositoryService repositoryService;
+
+   public RestDataObjectService(DataObjectService dataObjectService, RepositoryService repositoryService)
+   {
+      this.dataObjectService = dataObjectService;
+      this.repositoryService = repositoryService;
+   }
+
+   /**
+    * Compile Data Object Service and return Node Type definition 
+    * 
+    * @param uriInfo - UriInfo
+    * @param location - Resource URL 
+    * @return
+    */
+   @POST
+   @Path("/generate-nodetype-definition")
+   public String getNodeTypeDefinition(@Context UriInfo uriInfo, @QueryParam("dependecyPath") String dependecyPath,
+      @QueryParam("do-location") String location, @QueryParam("nodeTypeFormat") NodeTypeFormat format)
+      throws PathNotFoundException
+   {
+      String[] jcrLocation = parseJcrLocation(uriInfo.getBaseUri().toASCIIString(), location);
+      if (location == null)
+      {
+         throw new IllegalArgumentException("You must specified location of script source.");
+      }
+      if (jcrLocation == null)
+      {
+         throw new PathNotFoundException("Location of script " + location + " not found. ");
+      }
+      String repository = jcrLocation[0];
+      String workspace = jcrLocation[1];
+
+      String pp = jcrLocation[2];
+      if (pp.startsWith("/"))
+      {
+         pp = pp.substring(1);
+      }
+      String path = "/" + jcrLocation[2];
+
+      //TODO: 
+      CompilationSource compilationSource;
+      if (dependecyPath != null)
+      {
+         String[] depJcrLocation = parseJcrLocation(uriInfo.getBaseUri().toASCIIString(), dependecyPath);
+         if (depJcrLocation == null)
+         {
+            throw new PathNotFoundException("Location of dependency  " + dependecyPath + " not found. ");
+         }
+         compilationSource = new CompilationSource(depJcrLocation[0], depJcrLocation[1], depJcrLocation[3]);
+      }
+      else
+      {
+         compilationSource = new CompilationSource(repository, workspace, path);
+      }
+      return dataObjectService.generateSchema(format, compilationSource, path);
+   }
+
+   @POST
+   @Path("/registre-nodetype/{format}/{alreadyExistsBehaviour}")
+   public void registreNodeType(@PathParam("format") NodeTypeFormat format,
+      @PathParam("alreadyExistsBehaviour") Integer alreadyExistsBehaviour, InputStream nodeTypeDefinition)
+      throws RepositoryException, RepositoryConfigurationException
+
+   {
+      NodeTypeManagerImpl nodeTypeManager =
+         (NodeTypeManagerImpl)repositoryService.getDefaultRepository().getNodeTypeManager();
+      switch (format)
+      {
+         case EXO :
+            nodeTypeManager.registerNodeTypes(nodeTypeDefinition, alreadyExistsBehaviour,
+               NodeTypeDataManager.TEXT_XML);
+            break;
+         case CND :
+            nodeTypeManager.registerNodeTypes(nodeTypeDefinition, alreadyExistsBehaviour,
+               NodeTypeDataManager.TEXT_X_JCR_CND);
+            break;
+         default :
+            throw new RepositoryException("Unsupported content type:" + format.name());
+      }
+
+   }
+
+   /**
+    * @param baseUri base URI
+    * @param location location of groovy script
+    * @return array of {@link String}, which elements contain repository name, workspace name and 
+    * path the path to JCR node that contains groovy script to be deployed
+    */
+   private String[] parseJcrLocation(String baseUri, String location)
+   {
+      baseUri += WEBDAV_CONTEXT;
+      if (!location.startsWith(baseUri))
+      {
+         return null;
+      }
+
+      String[] elements = new String[3];
+      location = location.substring(baseUri.length());
+      elements[0] = location.substring(0, location.indexOf('/'));
+      location = location.substring(location.indexOf('/') + 1);
+      elements[1] = location.substring(0, location.indexOf('/'));
+      elements[2] = location.substring(location.indexOf('/') + 1);
+      return elements;
+   }
+
+}
---
+++ exo-ide-extension-chromattic-server/src/test/java/org/exoplatform/ide/chromattic/TestDataObjectService.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+package org.exoplatform.ide.chromattic;
+
+import org.chromattic.dataobject.NodeTypeFormat;
+import org.exoplatform.common.http.HTTPStatus;
+import org.exoplatform.services.rest.impl.ContainerResponse;
+import org.exoplatform.ws.frameworks.json.impl.JsonGeneratorImpl;
+import org.junit.Before;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
+
+import javax.jcr.Node;
+
+/**
+ * 
+ * Created by The eXo Platform SAS .
+ * 
+ * @author <a href="mailto:gavrikvetal@gmail.com">Vitaliy Gulyy</a>
+ * @version $
+ */
+
+public class TestDataObjectService extends BaseTest
+{
+   
+   /** . */
+   private static final String dataObjectGroovy =
+       "@org.chromattic.api.annotations.PrimaryType(name=\"nt:unstructured\")\n" +
+       "class DataObject {\n" +
+       "@org.chromattic.api.annotations.Property(name = \"a\") def String a;\n" +
+       "}";
+   
+   private static final String ntd = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+   "<!--Node type generation prototype-->" + 
+   "<nodeTypes xmlns:jcr=\"http://www.jcp.org/jcr/1.0\" xmlns:nt=\"http://www.jcp.org/jcr/nt/1.0\" xmlns:mix=\"http://www.jcp.org/jcr/mix/1.0\">"+
+     "<nodeType name=\"nt:unst\" isMixin=\"false\" hasOrderableChildNodes=\"false\">"+
+       "<supertypes>"+
+         "<supertype>nt:base</supertype>"+
+         "<supertype>mix:referenceable</supertype>"+
+       "</supertypes>"+
+       "<propertyDefinitions>"+
+         "<propertyDefinition name=\"a\" requiredType=\"String\" autoCreated=\"false\" mandatory=\"false\" onParentVersion=\"COPY\" protected=\"false\" multiple=\"false\">"+
+           "<valueConstraints/>"+
+         "</propertyDefinition>"+
+       "</propertyDefinitions>"+
+       "<childNodeDefinitions/>"+
+     "</nodeType>"+
+   "</nodeTypes>";
+
+   
+   @Before
+   public void setUp() throws Exception {
+      super.setUp();
+      
+      Node groovyRepo = root.addNode("dependencies", "nt:folder");
+      Node test1 = groovyRepo.addNode("DataObject.groovy", "nt:file");
+      test1 = test1.addNode("jcr:content", "nt:resource");
+      test1.setProperty("jcr:mimeType", "script/groovy");
+      test1.setProperty("jcr:lastModified", Calendar.getInstance());
+      test1.setProperty("jcr:data", dataObjectGroovy);
+      session.save();      
+   }
+   
+   
+   public void testNodeTypeGenration() throws Exception {
+      String location = "/ide-vfs-webdav/" + "db1/ws/dependencies/DataObject.groovy";
+      ContainerResponse cres =
+         launcher.service("POST",
+            "/ide/chromattic/generate-nodetype-definition?do-location=" + location + "&nodeTypeFormat=EXO", "", null, null,
+            null, null);
+      assertEquals(HTTPStatus.OK, cres.getStatus());
+      String s = (String)cres.getEntity();
+      System.out.println("Generated node types " + s);
+    }
+   
+   public void testNodeTypeGenrationDOScriptNotFound() throws Exception {
+      String location = "/ide-vfs-webdav/" + "db1/ws/dependencies/DataObjectNotFound.groovy";
+      ContainerResponse cres =
+         launcher.service("POST",
+            "/ide/chromattic/generate-nodetype-definition?do-location=" + location + "&nodeTypeFormat=EXO", "", null, null,
+            null, null);
+      assertEquals(HTTPStatus.INTERNAL_ERROR, cres.getStatus());
+      String s = (String)cres.getEntity();
+      System.out.println("Generated node types " + s);
+    }
+   
+   
+   public void testNodeTypeGenrationLocationNotFound() throws Exception {
+      ContainerResponse cres =
+         launcher.service("POST",
+            "/ide/chromattic/generate-nodetype-definition?nodeTypeFormat=EXO", "", null, null,
+            null, null);
+      assertEquals(HTTPStatus.INTERNAL_ERROR, cres.getStatus());
+      String s = (String)cres.getEntity();
+      System.out.println("Generated node types " + s);
+    }
+   
+   public void testNodeTypeGenrationCND() throws Exception {
+      String location = "/ide-vfs-webdav/" + "db1/ws/dependencies/DataObject.groovy";
+      ContainerResponse cres =
+         launcher.service("POST",
+            "/ide/chromattic/generate-nodetype-definition?do-location=" + location + "&nodeTypeFormat=CND", "", null, null,
+            null, null);
+      assertEquals(HTTPStatus.OK, cres.getStatus());
+      String s = (String)cres.getEntity();
+      System.out.println("Generated node types " + s);
+    }
+   
+   public void testNodeType() throws Exception {
+//      String location = "/ide-vfs-webdav/" + "db1/ws/dependencies/DataObject.groovy";
+      ContainerResponse cres =
+         launcher.service("POST",
+            "/ide/chromattic/registre-nodetype/EXO/4", "", null, ntd.getBytes(),
+            null, null);
+      assertEquals(HTTPStatus.NO_CONTENT, cres.getStatus());
+    
+    }
+   
+
+}
