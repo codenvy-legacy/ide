ba601fa5f9779dbf44a89cee73c7bc07f4c84af0
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2010-12-08 14:01:24 +0000

    IDE-453 add Selenium tests

---
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/file/autocompletion/groovy/GroovyObjectCompletionTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.operation.file.autocompletion.groovy;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.exoplatform.ide.BaseTest;
+import org.exoplatform.ide.MenuCommands;
+import org.exoplatform.ide.TestConstants;
+import org.junit.Test;
+
+/**
+ * Created by The eXo Platform SAS.
+ *
+ * @author <a href="mailto:tnemov@gmail.com">Evgen Vidolob</a>
+ * @version $Id: Dec 8, 2010 12:40:45 PM evgen $
+ *
+ */
+public class GroovyObjectCompletionTest extends BaseTest
+{
+
+   @Test
+   public void testGroovyObjectCompletion() throws Exception
+   {
+      Thread.sleep(TestConstants.SLEEP);
+      IDE.toolbar().runCommandFromNewPopupMenu(MenuCommands.New.REST_SERVICE_FILE);
+      Thread.sleep(TestConstants.SLEEP);
+      
+      for(int i = 0; i<10; i++)
+      {
+         selenium.keyPressNative("" + java.awt.event.KeyEvent.VK_DOWN);
+         Thread.sleep(TestConstants.SLEEP_SHORT);
+      }
+      
+      selenium.keyDown("//body[@class='editbox']", "\\35");
+      typeTextIntoEditor(0, ".");
+      
+      openAutoCompleteForm();
+     
+      selenium.focus("//input[@class='exo-autocomplete-edit']");
+      
+      selenium.typeKeys("//input[@class='exo-autocomplete-edit']", "con");
+      Thread.sleep(TestConstants.SLEEP_SHORT);
+      
+      assertTrue(selenium.isElementPresent("//div[text()='concat(String):String']"));
+      assertTrue(selenium.isElementPresent("//div[text()='contains(CharSequence):boolean']"));
+      assertTrue(selenium.isElementPresent("//div[text()='contentEquals(StringBuffer):boolean']"));
+      assertTrue(selenium.isElementPresent("//div[text()='contentEquals(CharSequence):boolean']"));
+      
+      selenium.keyPressNative("" + java.awt.event.KeyEvent.VK_DOWN);
+      Thread.sleep(TestConstants.SLEEP_SHORT);
+      selenium.keyPressNative("" + java.awt.event.KeyEvent.VK_DOWN);
+      Thread.sleep(TestConstants.SLEEP_SHORT);
+      
+      selenium.keyPressNative("" + java.awt.event.KeyEvent.VK_ENTER);  
+      
+      assertFalse(selenium.isElementPresent("//table[@class='exo-autocomplete-panel']"));
+      assertTrue(getTextFromCodeEditor(0).contains(".contentEquals(StringBuffer)"));
+      IDE.editor().closeUnsavedFileAndDoNotSave(0);
+   }
+
+   /**
+    * @throws Exception 
+    */
+   private void openAutoCompleteForm() throws Exception
+   {
+      runHotkeyWithinEditor(0, true, false, java.awt.event.KeyEvent.VK_SPACE);
+      Thread.sleep(TestConstants.SLEEP);
+      assertTrue(selenium.isElementPresent("//table[@class='exo-autocomplete-panel']"));
+   }
+}
