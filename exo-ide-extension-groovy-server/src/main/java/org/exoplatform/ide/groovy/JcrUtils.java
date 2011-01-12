febfe2411cf4c42f6558f0e43eb2d6c5b6313c8d
Vitaliy Parfonov <vparfonov@exoplatform.com>
2011-01-12 14:49:12 +0000

    IDE-538

---
+++ exo-ide-extension-groovy-server/src/main/java/org/exoplatform/ide/groovy/JcrUtils.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2003-2011 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ide.groovy;
+
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.config.RepositoryConfigurationException;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.jcr.ext.app.ThreadLocalSessionProviderService;
+import org.exoplatform.services.jcr.ext.common.SessionProvider;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+/**
+ * Created by The eXo Platform SAS.
+ * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
+ * @version $Id: $
+*/
+public class JcrUtils
+{
+
+   /**
+    * Get current repository
+    * @return current repository or default repository if current repository is null
+    * @throws RepositoryException
+    * @throws RepositoryConfigurationException
+    */
+   public static ManageableRepository getRepository(RepositoryService repositoryService) throws RepositoryException,
+      RepositoryConfigurationException
+   {
+      return repositoryService.getCurrentRepository() != null ? repositoryService.getCurrentRepository()
+         : repositoryService.getDefaultRepository();
+   }
+
+   /**
+    * Get JCR Session on current repository
+    * 
+    * @param repositoryService
+    * @param sessionProviderService
+    * @param wsName
+    * @return
+    * @throws RepositoryException
+    * @throws RepositoryConfigurationException
+    */
+   public static Session getSession(RepositoryService repositoryService,
+      ThreadLocalSessionProviderService sessionProviderService, String wsName) throws RepositoryException,
+      RepositoryConfigurationException
+   {
+      ManageableRepository repo = getRepository(repositoryService);
+      SessionProvider sp = sessionProviderService.getSessionProvider(null);
+      if (sp == null)
+         throw new RepositoryException("SessionProvider is not properly set. Make the application calls"
+            + "SessionProviderService.setSessionProvider(..) somewhere before ("
+            + "for instance in Servlet Filter for WEB application)");
+
+      return sp.getSession(wsName, repo);
+   }
+
+}
