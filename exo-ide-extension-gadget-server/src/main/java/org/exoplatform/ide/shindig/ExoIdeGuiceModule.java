3b8fde6a2572ca822426b6ff709894e94b6fc32c
Vitaliy Parfonov <vparfonov@exoplatform.com>
2011-01-19 11:38:36 +0000

    Fix key.txt file location

--- exo-ide-extension-gadget-server/src/main/java/org/exoplatform/ide/shindig/ExoIdeGuiceModule.java
+++ exo-ide-extension-gadget-server/src/main/java/org/exoplatform/ide/shindig/ExoIdeGuiceModule.java
@@ -1,50 +1,41 @@
-/*
- * Copyright (C) 2010 eXo Platform SAS.
- *
+/**
+ * Copyright (C) 2009 eXo Platform SAS.
+ * 
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.exoplatform.ide.shindig;
 
-import org.apache.shindig.common.PropertiesModule;
+import org.apache.shindig.config.ContainerConfig;
+import org.apache.shindig.gadgets.DefaultGuiceModule;
 
 /**
- * USE ONLY IN STANDALONE BUILD
- * 
- * 1. This class need for rewrite default location of 
- * shindig properties file. By default it locate in shindig-common.jar 
- * In this properties file described location of container.js file.
- * We can't use container.js from GateIn in standalone build because it use eXoGadgetServer context.
- * We use IDE and describe in in our container.js 
- * (see /exo-ide-module-gadget-server/src/main/resources/containers/default-ide/container.js).  
- * 
- * 2. It generate key file for gadget security (OAuth)
- * 
- * Created by The eXo Platform SAS.
+ * The goal of the module is to bind the {@link org.apache.shindig.common.ContainerConfig} interface to the
+ * {@link org.exoplatform.portal.gadget.core.ExoContainerConfig} implementation instead of the default
+ * implementation annotated on the container config interface.
+ *
  * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
- * @version $Id: $
-*/
-public class ExoIdeModule extends PropertiesModule
+ * @version $Revision$
+ */
+public class ExoIdeGuiceModule extends DefaultGuiceModule
 {
-   private static String DEFAULT_PROPERTIES = "ide.shindig.properties";
-   
-   public ExoIdeModule()
+
+   @Override
+   protected void configure()
    {
-      super(DEFAULT_PROPERTIES);
+      bind(ContainerConfig.class).to(ExoContainerConfig.class);
    }
-   
-   
- 
 }
