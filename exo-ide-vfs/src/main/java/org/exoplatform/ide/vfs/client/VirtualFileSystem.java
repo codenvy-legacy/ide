d908ec3c52cdea91dffc8be7c3356f598f2c4c00
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2011-02-03 09:34:28 +0000

    IDE-589 move VFS service from exo-ide-client module to exo-ide-vfs

--- exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/client/VirtualFileSystem.java
+++ exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/client/VirtualFileSystem.java
@@ -18,23 +18,64 @@
  */
 package org.exoplatform.ide.vfs.client;
 
-import org.exoplatform.gwtframework.commons.exception.ServerExceptionEvent;
+import org.exoplatform.gwtframework.commons.exception.ExceptionThrownEvent;
 import org.exoplatform.gwtframework.commons.loader.Loader;
 import org.exoplatform.gwtframework.commons.rest.AsyncRequest;
 import org.exoplatform.gwtframework.commons.rest.AsyncRequestCallback;
-import org.exoplatform.ide.vfs.shared.File;
-import org.exoplatform.ide.vfs.shared.Folder;
-import org.exoplatform.ide.vfs.shared.Link;
+import org.exoplatform.ide.vfs.client.event.ChildrenReceivedEvent;
+import org.exoplatform.ide.vfs.client.event.FileContentReceivedEvent;
+import org.exoplatform.ide.vfs.client.event.FolderCreatedEvent;
+import org.exoplatform.ide.vfs.client.event.VirtualFileSystemInfoReceivedEvent;
+import org.exoplatform.ide.vfs.client.marshal.ChildrenUnmarshaller;
+import org.exoplatform.ide.vfs.client.marshal.FileContentUnmarshaller;
+import org.exoplatform.ide.vfs.client.marshal.VFSInfoUnmarshaller;
+import org.exoplatform.ide.vfs.shared.Item;
+import org.exoplatform.ide.vfs.shared.ItemList;
+import org.exoplatform.ide.vfs.shared.VirtualFileSystemInfo;
 
-import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.http.client.RequestBuilder;
 
 public class VirtualFileSystem
 {
+   public interface Messages
+   {
+
+      static final String GET_FILE_CONTENT = "Reading file content...";
+
+      static final String GET_FOLDER_CONTENT = "Reading folder content...";
+
+      static final String CREATE_FOLDER = "Creating folder...";
+
+      static final String DELETE_FOLDER = "Deleting folder...";
+
+      static final String DELETE_FILE = "Deleting file...";
+
+      static final String SAVE_FILE_CONTENT = "Saving file...";
+
+      static final String GET_PROPERTIES = "Reading properties...";
+
+      static final String SAVE_PROPERTIES = "Saving properties...";
+
+      static final String SEARCH = "Searching...";
+
+      static final String MOVE_FILE = "Moving file...";
+
+      static final String MOVE_FOLDER = "Moving folder...";
+
+      static final String COPY_FILE = "Copying file...";
+
+      static final String COPY_FOLDER = "Copying folder...";
+
+      static final String LOCK = "Locking...";
+
+   }
+
    private static VirtualFileSystem instance;
 
    private final HandlerManager eventBus;
+
+   private String workspace;
 
    private Loader loader;
 
@@ -43,12 +84,30 @@ public class VirtualFileSystem
       return instance;
    }
 
-   public VirtualFileSystem(HandlerManager eventBus,  Loader loader, String restServiceURI, String repository, String workspace)
+   public VirtualFileSystem(HandlerManager eventBus, Loader loader, String workspace)
    {
       instance = this;
-      
+
       this.eventBus = eventBus;
       this.loader = loader;
+      this.workspace = workspace;
+   }
+
+   public void getVFSInfo()
+   {
+      String url = encodeURI(workspace + "/");
+
+      VirtualFileSystemInfo virtualFileSystemInfo = new VirtualFileSystemInfo();
+
+      VFSInfoUnmarshaller unmarshaller = new VFSInfoUnmarshaller(virtualFileSystemInfo);
+      VirtualFileSystemInfoReceivedEvent event = new VirtualFileSystemInfoReceivedEvent(virtualFileSystemInfo);
+
+      String errorMessage = " Service is not deployed.<br>Resource not found.";
+      ExceptionThrownEvent errorEvent = getErrorEvent(errorMessage);
+
+      AsyncRequestCallback callback = new AsyncRequestCallback(eventBus, unmarshaller, event, errorEvent);
+
+      AsyncRequest.build(RequestBuilder.GET, url, loader).send(callback);
    }
 
    /**
@@ -56,11 +115,21 @@ public class VirtualFileSystem
     * 
     * @param path
     */
-   public void getChildren(Folder folder, GwtEvent<?> postEvent, ServerExceptionEvent<?> errorEvent)
+   public void getChildren(String id)
    {
-      String url = folder.getLinks().get(Link.REL_CHILDREN).getHref();
-      AsyncRequestCallback callback = new AsyncRequestCallback(eventBus, new JSONUnmarshaller(), postEvent, errorEvent);
-      AsyncRequest.build(RequestBuilder.POST, url, loader).data(new JSONMarshaller()).send(callback);
+      String url = encodeURI(workspace + "/children/" + id);
+
+      ItemList<Item> items = new ItemList<Item>();
+      ChildrenReceivedEvent event = new ChildrenReceivedEvent(items);
+      ChildrenUnmarshaller unmarshaller = new ChildrenUnmarshaller(id, items);
+
+      String errorMessage = "Service is not deployed.<br>Parent folder not found.";
+      ExceptionThrownEvent errorEvent = getErrorEvent(errorMessage);
+
+      loader.setMessage(Messages.GET_FOLDER_CONTENT);
+      AsyncRequestCallback callback = new AsyncRequestCallback(eventBus, unmarshaller, event, errorEvent);
+      AsyncRequest.build(RequestBuilder.GET, url, loader).send(callback);
+
    }
 
    /**
@@ -68,18 +137,30 @@ public class VirtualFileSystem
     * 
     * @param path
     */
-   public void createFolder(Folder parent, String name)
+   public void createFolder(String parentId, String name)
    {
-      
-//      String path = new StringBuilder() //
-//      .append(SERVICE_URI) //
-//      .append("folder") //
-//      .append(CREATE_TEST_PATH) //
-//      .append("?") //
-//      .append("name=") //
-//      .append(name).toString();
-//   ContainerResponse response = launcher.service("POST", path, BASE_URI, null, null, null);      
-      
+      String url = workspace + "/folder" + parentId;
+      if (url.endsWith("/"))
+      {
+         url = url.substring(0, url.length() - 1);
+      }
+
+      url += "?name=" + name;
+      url = encodeURI(url);
+
+      String newFolderID = parentId;
+      if (!newFolderID.endsWith("/"))
+         newFolderID += "/";
+      newFolderID += name;
+
+      FolderCreatedEvent event = new FolderCreatedEvent(newFolderID);
+
+      String errorMessage = "Service is not deployed.<br>Resource already exist.<br>Parent folder not found.";
+      ExceptionThrownEvent errorEvent = getErrorEvent(errorMessage);
+
+      loader.setMessage(Messages.COPY_FOLDER);
+      AsyncRequestCallback callback = new AsyncRequestCallback(eventBus, event, errorEvent);
+      AsyncRequest.build(RequestBuilder.POST, url, loader).send(callback);
    }
 
    /**
@@ -87,8 +168,67 @@ public class VirtualFileSystem
     * 
     * @param file
     */
-   public void getContent(File file)
+   public void getFileContent(String id)
+   {
+      String url = workspace + "/content" + id;
+      
+      FileContentReceivedEvent event = new FileContentReceivedEvent();
+      
+      FileContentUnmarshaller unmarshaller = new FileContentUnmarshaller(event);
+      
+      String errorMessage = " Service is not deployed.<br>Resource not found.";
+      ExceptionThrownEvent errorEvent = getErrorEvent(errorMessage);
+
+      loader.setMessage(Messages.GET_FILE_CONTENT);
+      AsyncRequestCallback callback = new AsyncRequestCallback(eventBus, unmarshaller, event, errorEvent);
+      AsyncRequest.build(RequestBuilder.GET, url, loader).send(callback);
+      
+   }
+
+   public void saveFileContent(String id, String mediaType, String content, String lockToken)
+   {
+      
+   }
+
+   public void copy(String source, String destination)
    {
    }
+
+   public void move(String id, String parentId, String lockToken)
+   {
+
+   }
+
+   public void delete(String id, String lockToken)
+   {
+
+   }
+
+   public void rename(String id, String mediaType, String newname, String lockToken)
+   {
+
+   }
+
+   public void lock(String id)
+   {
+   }
+
+   public void unlock(String id, String lockToken)
+   {
+
+   }
+
+   private ExceptionThrownEvent getErrorEvent(String message)
+   {
+      return new ExceptionThrownEvent(message);
+   }
+   
+   /**
+    * @param url
+    * @return result of javaScript function <code>encodeURI(url)</code>
+    */
+   public static native String encodeURI(String url) /*-{
+       return encodeURI(url);
+     }-*/;
    
 }
