dc38b83fcff5e74c53edc4e94d02c44db8adcc5e
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2011-02-03 15:17:26 +0000

    IDE-556

--- exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/server/PropertyFilter.java
+++ exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/server/PropertyFilter.java
@@ -18,6 +18,7 @@
  */
 package org.exoplatform.ide.vfs.server;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -31,12 +32,41 @@ import org.exoplatform.ide.vfs.server.exceptions.InvalidArgumentException;
 public class PropertyFilter
 {
    /** Property filter for all properties. */
+   public static final String ALL = "*";
+
+   /** Property filter for all properties. */
+   public static final String NONE = "none";
+
+   /** Property filter for all properties. */
    public static final PropertyFilter ALL_FILTER;
+
+   /** Property filter for all properties. */
+   public static final PropertyFilter NONE_FILTER;
 
    static
    {
       ALL_FILTER = new PropertyFilter();
       ALL_FILTER.retrievalAllProperties = true;
+      NONE_FILTER = new PropertyFilter();
+      NONE_FILTER.propertyNames = Collections.emptySet();
+   }
+
+   /**
+    * Construct new Property Filter.
+    * 
+    * @param filterString the string that contains either '*' or comma-separated
+    *           list of properties names. An arbitrary number of space allowed
+    *           before and after each comma. If filterString is 'none' it minds
+    *           all properties should be regicted by filter.
+    * @throws InvalidArgumentException if <code>filterString</code> is invalid
+    */
+   public static PropertyFilter valueOf(String filterString) throws InvalidArgumentException
+   {
+      if (filterString == null || filterString.length() == 0 || ALL.equals(filterString = filterString.trim()))
+         return ALL_FILTER;
+      else if (filterString.equalsIgnoreCase(NONE))
+         return NONE_FILTER;
+      return new PropertyFilter(filterString);
    }
 
    /** Characters that split. */
@@ -44,9 +74,6 @@ public class PropertyFilter
 
    /** Characters that not allowed in property name. */
    private static final String ILLEGAL_CHARACTERS = ",\"'\\.()";
-
-   /** Property filter for all properties. */
-   private static final String ALL = "*";
 
    /** Property names. */
    private Set<String> propertyNames;
@@ -62,37 +89,30 @@ public class PropertyFilter
     *           before and after each comma.
     * @throws InvalidArgumentException if <code>filterString</code> is invalid
     */
-   public PropertyFilter(String filterString) throws InvalidArgumentException
+   private PropertyFilter(String filterString) throws InvalidArgumentException
    {
-      if (filterString == null || filterString.length() == 0 || ALL.equals(filterString = filterString.trim()))
+      this.propertyNames = new HashSet<String>();
+      for (String token : SPLITTER.split(filterString))
       {
-         this.retrievalAllProperties = true;
-      }
-      else
-      {
-         this.propertyNames = new HashSet<String>();
-         for (String token : SPLITTER.split(filterString))
+         if (token.length() > 0 && !token.equals(ALL))
          {
-            if (token.length() > 0 && !token.equals(ALL))
+            for (char ch : token.toCharArray())
             {
-               for (char ch : token.toCharArray())
-               {
-                  if (Character.isWhitespace(ch) || ILLEGAL_CHARACTERS.indexOf(ch) != -1)
-                     throw new InvalidArgumentException("Invalid filter '" + filterString
-                        + "' contains illegal characters.");
-               }
-               this.propertyNames.add(token);
+               if (Character.isWhitespace(ch) || ILLEGAL_CHARACTERS.indexOf(ch) != -1)
+                  throw new InvalidArgumentException("Invalid filter '" + filterString
+                     + "' contains illegal characters.");
             }
-            else
-            {
-               throw new InvalidArgumentException("Invalid filter '" + filterString
-                  + "'. Filter must contains either '*' OR comma-separated list of properties.");
-            }
+            this.propertyNames.add(token);
+         }
+         else
+         {
+            throw new InvalidArgumentException("Invalid filter '" + filterString
+               + "'. Filter must contains either '*' OR comma-separated list of properties.");
          }
       }
    }
 
-   protected PropertyFilter()
+   private PropertyFilter()
    {
    }
 
