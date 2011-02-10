554673a59b776db8f73ec24941c3b9954315a0cb
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2011-02-10 13:47:15 +0000

    IDE-613 fix license

--- exo-ide-editor/src/main/java/org/exoplatform/ide/editor/api/event/EditorFocusReceivedEvent.java
+++ exo-ide-editor/src/main/java/org/exoplatform/ide/editor/api/event/EditorFocusReceivedEvent.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2009 eXo Platform SAS.
+/*
+ * Copyright (C) 2011 eXo Platform SAS.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -11,11 +11,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have Recieved a copy of the GNU Lesser General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
  */
 
 package org.exoplatform.ide.editor.api.event;
