f2086ffad148ee2fe9aa4b70cadd2b6d3ea34f22
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2011-02-22 14:00:16 +0000

    IDE-621 CodeMirror use callback instead event for code assistant features

---
+++ exo-ide-editor/src/main/java/org/exoplatform/ide/editor/codemirror/codeassistant/CssCodeAssistant.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.editor.codemirror.codeassistant;
+
+import java.util.List;
+
+import org.exoplatform.ide.editor.api.Editor;
+import org.exoplatform.ide.editor.api.codeassitant.CodeAssistant;
+import org.exoplatform.ide.editor.api.codeassitant.CodeError;
+import org.exoplatform.ide.editor.api.codeassitant.Token;
+
+/**
+ * @author <a href="mailto:tnemov@gmail.com">Evgen Vidolob</a>
+ * @version $Id: CssCodeAssistant Feb 22, 2011 2:15:17 PM evgen $
+ *
+ */
+public class CssCodeAssistant implements CodeAssistant
+{
+
+   /**
+    * @see org.exoplatform.ide.editor.api.codeassitant.CodeAssistant#errorMarckClicked(org.exoplatform.ide.editor.api.Editor, java.util.List, int, int, java.lang.String)
+    */
+   @Override
+   public void errorMarckClicked(Editor editor, List<CodeError> codeErrorList, int markOffsetX, int markOffsetY,
+      String fileMimeType)
+   {      
+      System.out.println("CssCodeAssistant.errorMarckClicked()");
+   }
+
+   /**
+    * @see org.exoplatform.ide.editor.api.codeassitant.CodeAssistant#autocompleteCalled(org.exoplatform.ide.editor.api.Editor, java.lang.String, int, int, java.lang.String, int, int, java.util.List, java.lang.String, org.exoplatform.ide.editor.api.codeassitant.Token)
+    */
+   @Override
+   public void autocompleteCalled(Editor editor, String mimeType, int cursorOffsetX, int cursorOffsetY,
+      String lineContent, int cursorPositionX, int cursorPositionY, List<Token> tokenList, String lineMimeType,
+      Token currentToken)
+   {
+      System.out.println("CssCodeAssistant.autocompleteCalled()");
+   }
+
+}
