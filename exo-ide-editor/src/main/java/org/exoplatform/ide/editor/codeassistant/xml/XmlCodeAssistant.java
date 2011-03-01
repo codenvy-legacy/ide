93d578732ac164d16988bba4b059ff5af2b12857
Evgen Vidolob <evgen.vidolob@exoplatform.com>
2011-03-01 15:45:58 +0000

    IDE-621 refactoring XML autocomplete

---
+++ exo-ide-editor/src/main/java/org/exoplatform/ide/editor/codeassistant/xml/XmlCodeAssistant.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2011 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.editor.codeassistant.xml;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.exoplatform.ide.editor.api.Editor;
+import org.exoplatform.ide.editor.api.codeassitant.CodeAssistant;
+import org.exoplatform.ide.editor.api.codeassitant.CodeError;
+import org.exoplatform.ide.editor.api.codeassitant.StringProperty;
+import org.exoplatform.ide.editor.api.codeassitant.Token;
+import org.exoplatform.ide.editor.api.codeassitant.TokenProperties;
+import org.exoplatform.ide.editor.api.codeassitant.TokenProperty;
+import org.exoplatform.ide.editor.api.codeassitant.TokenType;
+import org.exoplatform.ide.editor.api.codeassitant.ui.TokenWidget;
+import org.exoplatform.ide.editor.api.codeassitant.ui.TokenWidgetFactory;
+import org.exoplatform.ide.editor.codeassistant.html.HtmlTokenWidget;
+
+/**
+ * @author <a href="mailto:tnemov@gmail.com">Evgen Vidolob</a>
+ * @version $Id: XmlCodeAssistant Mar 1, 2011 5:19:53 PM evgen $
+ *
+ */
+public class XmlCodeAssistant extends CodeAssistant implements TokenWidgetFactory
+{
+
+   private Map<String, Token> tokens = new HashMap<String, Token>();
+
+   /**
+    * @see org.exoplatform.ide.editor.api.codeassitant.CodeAssistant#errorMarckClicked(org.exoplatform.ide.editor.api.Editor, java.util.List, int, int, java.lang.String)
+    */
+   @Override
+   public void errorMarckClicked(Editor editor, List<CodeError> codeErrorList, int markOffsetX, int markOffsetY,
+      String fileMimeType)
+   {
+   }
+
+   /**
+    * @see org.exoplatform.ide.editor.api.codeassitant.CodeAssistant#autocompleteCalled(org.exoplatform.ide.editor.api.Editor, java.lang.String, int, int, java.lang.String, int, int, java.util.List, java.lang.String, org.exoplatform.ide.editor.api.codeassitant.Token)
+    */
+   @Override
+   public void autocompleteCalled(Editor editor, String mimeType, int cursorOffsetX, int cursorOffsetY,
+      String lineContent, int cursorPositionX, int cursorPositionY, List<Token> tokenList, String lineMimeType,
+      Token currentToken)
+   {
+      this.editor = editor;
+      try
+      {
+         parseTokenLine(lineContent, cursorPositionX);
+         tokens.clear();
+         List<Token> tok = new ArrayList<Token>();
+         filterTokens(tokenList);
+         tok.addAll(tokens.values());
+         openForm(cursorOffsetX, cursorOffsetY, tok, this, this);
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   /**
+    * @param tokenList
+    * @return
+    */
+   @SuppressWarnings("unchecked")
+   private void filterTokens(List<Token> tokenList)
+   {
+      for (Token t : tokenList)
+      {
+         if (t.getName() != null && t.getType() == TokenType.TAG)
+         {
+            tokens.put(t.getName(), t);
+            t.setProperty(TokenProperties.CODE, new StringProperty("<" + t.getName() + "></" + t.getName() +">"));
+         }
+         if (t.hasProperty(TokenProperties.SUB_TOKEN_LIST)
+            && t.getProperty(TokenProperties.SUB_TOKEN_LIST).isArrayProperty().arrayValue() != null)
+         {
+            filterTokens((List<Token>)t.getProperty(TokenProperties.SUB_TOKEN_LIST).isArrayProperty().arrayValue());
+         }
+      }
+   }
+
+   private void parseTokenLine(String line, int cursorPos)
+   {
+      String tokenLine = "";
+      afterToken = line.substring(cursorPos - 1, line.length());
+      tokenLine = line.substring(0, cursorPos - 1);
+      if (tokenLine.contains("<"))
+      {
+         beforeToken = tokenLine.substring(0, tokenLine.lastIndexOf("<") + 1);
+         tokenLine = tokenLine.substring(tokenLine.lastIndexOf("<") + 1, tokenLine.length());
+         if (tokenLine.contains(">"))
+         {
+            tokenToComplete = "";
+            beforeToken = line.substring(0, cursorPos - 1);
+         }
+         else
+         {
+            tokenToComplete = tokenLine;
+         }
+      }
+      else
+      {
+         beforeToken = tokenLine;
+         tokenToComplete = "";
+      }
+
+   }
+
+   /**
+    * @see org.exoplatform.ide.editor.api.codeassitant.ui.TokenWidgetFactory#buildTokenWidget(org.exoplatform.ide.editor.api.codeassitant.Token)
+    */
+   @Override
+   public TokenWidget buildTokenWidget(Token token)
+   {
+      return new HtmlTokenWidget(token);
+   }
+}
