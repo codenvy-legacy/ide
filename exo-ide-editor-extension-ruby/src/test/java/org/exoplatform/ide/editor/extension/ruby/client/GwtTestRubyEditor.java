67dcca9184efae54698d233c0776a1e779615740
Dmytro Nochevnov <dnochevnov@exoplatform.com>
2011-04-22 13:26:21 +0000

    updated extension (IDE-733)

---
+++ exo-ide-editor-extension-ruby/src/test/java/org/exoplatform/ide/editor/extension/ruby/client/GwtTestRubyEditor.java
@@ -0,0 +1,40 @@
+package org.exoplatform.ide.editor.extension.ruby.client;
+
+import org.exoplatform.ide.editor.extension.ruby.client.RubyEditorExtension;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit <b>integration</b> tests must extend GWTTestCase.
+ * Using <code>"GwtTest*"</code> naming pattern exclude them from running with
+ * surefire during the test phase.
+ */
+public class GwtTestRubyEditor extends GWTTestCase {
+   
+   String content = "# Ruby Sample program\n\n"+
+                    "class HelloClass\n"+                                                                                                                                                                                               
+                    "  def sayHello\n" +                                                                                                                                                                                                   
+                    "    puts( \"Hello, wolrd!\" )\n" +                                                                                                                                                                                       
+                    "  end\n"+                                                                                                                                                                                                            
+                    "end\n\n"+                                                                                                                                                                                                            
+                    "ob = HelloClass.new\n"+                                                                                                                                                                                            
+                    "ob.sayHello";
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  public String getModuleName() {
+    return "org.exoplatform.ide.editor.extension.ruby.client.RubyEditorJUnit";
+  }
+  
+  public void testdefaultContent()
+  {
+     String content = RubyEditorExtension.DEFAULT_CONTENT.getSource().getText();
+     assertNotNull(content);
+     assertEquals(this.content, content);
+  }
+
+  
+
+
+}
