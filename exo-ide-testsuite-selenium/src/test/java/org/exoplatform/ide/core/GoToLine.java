88df59687c1e2d5b833a16f28cf40c735b1afb76
Maxim Musienko <maxim.musienko@exoplatform.com>
2011-05-05 10:19:52 +0000

    Musienko Maksim: IDE-750 add new method for "GotoLine" form

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/GoToLine.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/GoToLine.java
@@ -31,35 +31,124 @@ import org.exoplatform.ide.Utils;
  * @version $
  */
 
-public class GoToLine extends AbstractTestModule {
-	
-	public void checkAppearGoToLineForm() {
+public class GoToLine extends AbstractTestModule
+{
 
-		assertTrue(selenium().isElementPresent("ideGoToLineForm"));
-		assertTrue(selenium()
-				.isElementPresent(
-						"exoWarningDialogOkButton"));
-		assertTrue(selenium().isElementPresent(
-				"//div[@id='ideGoToLineForm']//div/img[@title='Close']"));
-		assertTrue(selenium().isElementPresent("ideGoToLineFormGoButton"));
-		assertTrue(selenium().isElementPresent("ideGoToLineFormCancelButton"));
-	}
-	
-	
-	public void checkAppearExoWarningDialogGoToLineForm(String warnmessage) {
+   /**
+    * check Go To LineForm present
+    */
+   public void checkAppearGoToLineForm()
+   {
 
-		assertTrue(selenium().isElementPresent("exoWarningDialog"));
-		assertTrue(selenium()
-				.isElementPresent(
-					"//div[@id='ideGoToLineForm']//div[@class='Caption']/span[text()='Go to Line']"));
-		assertTrue(selenium()
-				.isElementPresent(
-					"//div[@id='exoWarningDialog']//table//td//div[@class='gwt-Label' and text()=" + warnmessage + "]"));
-	}
+      assertTrue(selenium().isElementPresent("ideGoToLineForm"));
+      assertTrue(selenium().isElementPresent("//div[@id='ideGoToLineForm']//div/img[@title='Close']"));
+      assertTrue(selenium().isElementPresent("ideGoToLineFormGoButton"));
+      assertTrue(selenium().isElementPresent("ideGoToLineFormCancelButton"));
+   }
 
-	public void checkLineNumberLabel(String label){
-		assertTrue(selenium().isElementPresent(
-		"//form[@id='ideGoToLineFormDynamicForm']/div/nobr/span[text()=" + "'" + label + "'"+ "]"));
-	}
-	
+   /**
+    * check warning message with label of message. If input invalid parameter
+    * @param warnmessage
+    */
+   public void checkAppearExoWarningDialogGoToLineForm(String warnmessage)
+   {
+
+      assertTrue(selenium().isElementPresent("exoWarningDialog"));
+      assertTrue(selenium().isElementPresent("//div[@id='exoWarningDialog']//div[@class='Caption']/span[text()=\"Error\"]"));
+      assertTrue(selenium().isElementPresent(
+         "//div[@id='exoWarningDialog']//table//td//div[@class='gwt-Label' and text()=" + "\"" + warnmessage + "\""
+            + "]"));
+   }
+
+   /**
+    * close WarningDialog and check disappear
+    * @throws InterruptedException
+    */
+   public void closeExoWarningDialogGoToLineForm() throws InterruptedException
+   {
+      selenium().click("exoWarningDialogOkButton");
+      Thread.sleep(TestConstants.REDRAW_PERIOD);
+      assertFalse(selenium().isElementPresent("exoWarningDialog"));
+      assertFalse(selenium().isElementPresent("//div[@id='exoWarningDialog']//div[@class='Caption']/span[text()=\"Error\"]"));
+      assertFalse(selenium().isElementPresent("exoWarningDialogOkButton"));
+   }
+
+   /**
+    * check if all ok and warning massage not appear 
+    */
+   public void checkNotAppearExoWarningDialogGoToLineForm()
+   {
+
+      assertFalse(selenium().isElementPresent("exoWarningDialog"));
+      assertFalse(selenium().isElementPresent(
+         "//div[@id='ideGoToLineForm']//div[@class='Caption']/span[text()='Go to Line']"));
+   }
+
+   /**
+    * check "GoToLineForm" label on form with string range.
+    * @param label
+    */
+   public void checkLineNumberLabel(String label)
+   {
+      assertTrue(selenium().isElementPresent(
+         "//form[@id='ideGoToLineFormDynamicForm']/div/nobr/span[text()=" + "'" + label + "'" + "]"));
+   }
+
+   public void typeIntoGoToLineFormField(String typeText)
+   {
+      selenium().type("ideGoToLineFormLineNumberField", typeText);
+   }
+
+   /**
+    * clicks on go button of form. Checks closing form
+    * @throws InterruptedException 
+    */
+   public void pressGoButtonWithCorrectValue() throws InterruptedException
+   {
+      selenium().click("ideGoToLineFormGoButton");
+      Thread.sleep(TestConstants.REDRAW_PERIOD);
+      assertFalse(selenium().isElementPresent("ideGoToLineForm"));
+      assertFalse(selenium().isElementPresent("exoWarningDialogOkButton"));
+      assertFalse(selenium().isElementPresent("//div[@id='ideGoToLineForm']//div/img[@title='Close']"));
+      assertFalse(selenium().isElementPresent("ideGoToLineFormGoButton"));
+      assertFalse(selenium().isElementPresent("ideGoToLineFormCancelButton"));
+   }
+
+   /**
+    * clicks on cancel button of form. Checks closing form
+    * @throws InterruptedException 
+    */
+   public void pressCancelButtonWithCorrectValue() throws InterruptedException
+   {
+      selenium().click("ideGoToLineFormCancelButton");
+      Thread.sleep(TestConstants.REDRAW_PERIOD);
+      assertFalse(selenium().isElementPresent("ideGoToLineForm"));
+      assertFalse(selenium().isElementPresent("exoWarningDialogOkButton"));
+      assertFalse(selenium().isElementPresent("//div[@id='ideGoToLineForm']//div/img[@title='Close']"));
+      assertFalse(selenium().isElementPresent("ideGoToLineFormGoButton"));
+      assertFalse(selenium().isElementPresent("ideGoToLineFormCancelButton"));
+   }
+
+   /**
+    * press on button not check disappear form
+    * @throws InterruptedException
+    */
+   public void pressCancelButton() throws InterruptedException
+   {
+      selenium().click("ideGoToLineFormCancelButton");
+      Thread.sleep(TestConstants.REDRAW_PERIOD);
+
+   }
+
+   /**
+    * press on button not check disappear form
+    * @throws InterruptedException
+    */
+   public void pressGoButton() throws InterruptedException
+   {
+      selenium().click("ideGoToLineFormGoButton");
+      Thread.sleep(TestConstants.REDRAW_PERIOD);
+
+   }
+
 }
