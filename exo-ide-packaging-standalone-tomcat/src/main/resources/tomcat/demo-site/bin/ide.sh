f572d6c367eda810af7d8ddd6b86c1e00ce2c4c6
Vitaliy Parfonov <vparfonov@exoplatform.com>
2011-06-02 14:57:50 +0000

    IDE-581

--- exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/ide.sh
+++ exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/ide.sh
@@ -6,7 +6,7 @@ cd `dirname "$0"`
 # Sets some variables
 LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger"
 SECURITY_OPTS="-Djava.security.auth.login.config=../conf/jaas.conf"
-EXO_CONFIG_OPTS="-Xshare:auto -Xms128m -Xmx512m"
+EXO_CONFIG_OPTS="-Xshare:auto -Xms512m -Xmx1024m"
 JAVA_SRC="$JAVA_HOME/src.zip"
 EXO_OPTS="-Djavasrc=$JAVA_HOME/src.zip -Djre.lib=$JAVA_HOME/jre/lib -Dorg.exoplatform.mimetypes=conf/mimetypes.properties"
 #REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
