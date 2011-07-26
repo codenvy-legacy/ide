0bc37608019d813407fd800b0de7e63180697310
Dmytro Nochevnov <dnochevnov@exoplatform.com>
2011-07-26 13:26:12 +0000

    fixed parameter to load java classes

--- exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/setenv.bat
+++ exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/setenv.bat
@@ -4,7 +4,7 @@ set IDE_OPTS="-Dorg.exoplatform.logreader.logpath=%CATALINA_HOME%/logs"
 set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
 set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger"
 set SECURITY_OPTS="-Djava.security.auth.login.config=%CATALINA_HOME%\conf\jaas.conf"
-set EXO_OPTS="-Djavasrc=%JAVA_HOME%\src.zip" "-Djre.lib=%JAVA_HOME%\jre\lib -Dorg.exoplatform.mimetypes=conf\mimetypes.properties -Dorg.exoplatform.ide.server.fs-root-path=git"
+set EXO_OPTS="-Djavasrc=%JAVA_HOME%\src.zip" "-Djre.lib=%JAVA_HOME%\jre\lib" "-Dorg.exoplatform.mimetypes=conf\mimetypes.properties" "-Dorg.exoplatform.ide.server.fs-root-path=git"
 set JAVA_OPTS=-Xshare:auto -Xms128m -Xmx512m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS% %IDE_OPTS%
 set CLASSPATH=%CATALINA_HOME%/conf/;%CATALINA_HOME%/lib/jul-to-slf4j.jar;%CATALINA_HOME%/lib/slf4j-api.jar;%CATALINA_HOME%/lib/logback-classic.jar;%CATALINA_HOME%/lib/logback-core.jar
 echo "======="
