f769eb27761f63ae25f486dedfaf674bb1bb515f
Vitaliy Parfonov <vparfonov@exoplatform.com>
2011-08-01 10:08:52 +0000

    Add ide shell project to svn

---
+++ exo-ide-shell/src/main/java/org/exoplatform/cloudshell/client/CloudShell.java
@@ -0,0 +1,173 @@
+package org.exoplatform.cloudshell.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class CloudShell implements EntryPoint {
+
+  private final Messages messages = GWT.create(Messages.class);
+
+  /**
+   * This is the entry point method.
+   */
+  
+  private static ShellService remote = new ShellService();
+  
+  final public static Term term = new Term(remote, 32);
+  
+  public void onModuleLoad()
+   {
+
+     //
+     Button clear = new Button();
+     clear.setText("Clear");
+     clear.addClickHandler(new ClickHandler() {
+       public void onClick(ClickEvent event) {
+         term.clear();
+       }
+     });
+
+     //
+     VerticalPanel vertical = new VerticalPanel();
+     vertical.setSize("100%", "100%");
+     vertical.add(clear);
+     vertical.add(term);
+     
+
+     //
+     RootPanel.get().add(vertical);
+   }
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//  public void onModuleLoad() {
+//    final Button sendButton = new Button( messages.sendButton() );
+//    final TextBox nameField = new TextBox();
+//    nameField.setText( messages.nameField() );
+//    final Label errorLabel = new Label();
+//    
+//    CommandLineParser parser = new PosixParser();
+//
+//    // We can add style names to widgets
+//    sendButton.addStyleName("sendButton");
+//
+//    // Add the nameField and sendButton to the RootPanel
+//    // Use RootPanel.get() to get the entire body element
+//    RootPanel.get("nameFieldContainer").add(nameField);
+//    RootPanel.get("sendButtonContainer").add(sendButton);
+//    RootPanel.get("errorLabelContainer").add(errorLabel);
+//
+//    // Focus the cursor on the name field when the app loads
+//    nameField.setFocus(true);
+//    nameField.selectAll();
+//
+//    // Create the popup dialog box
+//    final DialogBox dialogBox = new DialogBox();
+//    dialogBox.setText("Remote Procedure Call");
+//    dialogBox.setAnimationEnabled(true);
+//    final Button closeButton = new Button("Close");
+//    // We can set the id of a widget by accessing its Element
+//    closeButton.getElement().setId("closeButton");
+//    final Label textToServerLabel = new Label();
+//    final HTML serverResponseLabel = new HTML();
+//    VerticalPanel dialogVPanel = new VerticalPanel();
+//    dialogVPanel.addStyleName("dialogVPanel");
+//    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+//    dialogVPanel.add(textToServerLabel);
+//    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+//    dialogVPanel.add(serverResponseLabel);
+//    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+//    dialogVPanel.add(closeButton);
+//    dialogBox.setWidget(dialogVPanel);
+//
+//    // Add a handler to close the DialogBox
+//    closeButton.addClickHandler(new ClickHandler() {
+//      public void onClick(ClickEvent event) {
+//        dialogBox.hide();
+//        sendButton.setEnabled(true);
+//        sendButton.setFocus(true);
+//      }
+//    });
+//
+//    // Create a handler for the sendButton and nameField
+//    class MyHandler implements ClickHandler, KeyUpHandler {
+//      /**
+//       * Fired when the user clicks on the sendButton.
+//       */
+//      public void onClick(ClickEvent event) {
+//        sendNameToServer();
+//      }
+//
+//      /**
+//       * Fired when the user types in the nameField.
+//       */
+//      public void onKeyUp(KeyUpEvent event) {
+//        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+//          sendNameToServer();
+//        }
+//      }
+//
+//      /**
+//       * Send the name from the nameField to the server and wait for a response.
+//       */
+//      private void sendNameToServer() {
+//        // First, we validate the input.
+//        errorLabel.setText("");
+//        String textToServer = nameField.getText();
+//        if (!FieldVerifier.isValidName(textToServer)) {
+//          errorLabel.setText("Please enter at least four characters");
+//          return;
+//        }
+//
+//        // Then, we send the input to the server.
+//        sendButton.setEnabled(false);
+//        textToServerLabel.setText(textToServer);
+//        serverResponseLabel.setText("");
+//        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
+//          public void onFailure(Throwable caught) {
+//            // Show the RPC error message to the user
+//            dialogBox.setText("Remote Procedure Call - Failure");
+//            serverResponseLabel.addStyleName("serverResponseLabelError");
+//            serverResponseLabel.setHTML(SERVER_ERROR);
+//            dialogBox.center();
+//            closeButton.setFocus(true);
+//          }
+//
+//          public void onSuccess(String result) {
+//            dialogBox.setText("Remote Procedure Call");
+//            serverResponseLabel.removeStyleName("serverResponseLabelError");
+//            serverResponseLabel.setHTML(result);
+//            dialogBox.center();
+//            closeButton.setFocus(true);
+//          }
+//        });
+//      }
+//    }
+//
+//    // Add a handler to send the name to the server
+//    MyHandler handler = new MyHandler();
+//    sendButton.addClickHandler(handler);
+//    nameField.addKeyUpHandler(handler);
+//  }
+//}
