f995a78741b2512f41674b406bfe57b688c37aa0
Anna  Zhuleva <anna.zhuleva@exoplatform.com>
2011-08-11 08:08:46 +0000

    IDE-1001: add GIT server

--- exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/setenv.sh
+++ exo-ide-packaging-standalone-tomcat/src/main/resources/tomcat/demo-site/bin/setenv.sh
@@ -4,7 +4,7 @@ LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Simpl
 SECURITY_OPTS="-Djava.security.auth.login.config=../conf/jaas.conf"
 EXO_CONFIG_OPTS="-Xshare:auto -Xms512m -Xmx1024m"
 JAVA_SRC="$JAVA_HOME/src.zip"
-EXO_OPTS="-Djavasrc=$JAVA_HOME/src.zip -Djre.lib=$JAVA_HOME/jre/lib -Dorg.exoplatform.mimetypes=conf/mimetypes.properties -Dorg.exoplatform.ide.server.fs-root-path=git"
+EXO_OPTS="-Djavasrc=$JAVA_HOME/src.zip -Djre.lib=$JAVA_HOME/jre/lib -Dorg.exoplatform.mimetypes=conf/mimetypes.properties -Dorg.exoplatform.ide.server.fs-root-path=git -Dorg.exoplatform.ide.git.repo-dir=${CATALINA_HOME}/bin/git"
 #REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
 JAVA_OPTS="$JAVA_OPTS $LOG_OPTS $SECURITY_OPTS $EXO_CONFIG_OPTS $EXO_OPTS $REMOTE_DEBUG $IDE_OPTS"
