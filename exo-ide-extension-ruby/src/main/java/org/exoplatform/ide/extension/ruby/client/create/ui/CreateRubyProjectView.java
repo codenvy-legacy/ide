73d18062c6e26cbcb4c4016201a1c897cc67d243
Vitaliy Parfonov <vparfonov@exoplatform.com>
2011-10-17 09:15:37 +0000

    IDE-1113

--- exo-ide-extension-ruby/src/main/java/org/exoplatform/ide/extension/ruby/client/create/ui/CreateRubyProjectView.java
+++ exo-ide-extension-ruby/src/main/java/org/exoplatform/ide/extension/ruby/client/create/ui/CreateRubyProjectView.java
@@ -18,19 +18,19 @@
  */
 package org.exoplatform.ide.extension.ruby.client.create.ui;
 
+import org.exoplatform.gwtframework.ui.client.component.ImageButton;
+import org.exoplatform.gwtframework.ui.client.component.TextInput;
+import org.exoplatform.ide.client.framework.ui.impl.ViewImpl;
+import org.exoplatform.ide.client.framework.ui.impl.ViewType;
+import org.exoplatform.ide.extension.ruby.client.RubyExtension;
+import org.exoplatform.ide.extension.ruby.client.create.CreateRubyProjectPresenter;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Widget;
-
-import org.exoplatform.gwtframework.ui.client.component.ImageButton;
-import org.exoplatform.gwtframework.ui.client.component.TextField;
-import org.exoplatform.ide.client.framework.ui.impl.ViewImpl;
-import org.exoplatform.ide.client.framework.ui.impl.ViewType;
-import org.exoplatform.ide.extension.ruby.client.RubyExtension;
-import org.exoplatform.ide.extension.ruby.client.create.CreateRubyProjectPresenter;
 
 /**
  * View for creation new java project.
@@ -45,7 +45,7 @@ public class CreateRubyProjectView extends ViewImpl implements CreateRubyProject
 
    private static final int WIDTH = 400;
 
-   private static final int HEIGHT = 160;
+   private static final int HEIGHT = 120;
 
    private static final String CREATE_BUTTON_ID = "ideCreateJavaProjectViewCreateButton";
 
@@ -63,7 +63,7 @@ public class CreateRubyProjectView extends ViewImpl implements CreateRubyProject
     * Project name field.
     */
    @UiField
-   TextField nameField;
+   TextInput nameField;
    
    /**
     * Create project button.
@@ -79,11 +79,10 @@ public class CreateRubyProjectView extends ViewImpl implements CreateRubyProject
    
    public CreateRubyProjectView()
    {
-      super(ID, ViewType.MODAL, RubyExtension.RUBY_CONSTANT.createRubyProjectViewTitle(), null, WIDTH, HEIGHT);
+      super(ID, ViewType.MODAL, RubyExtension.RUBY_CONSTANT.createRubyProjectViewTitle(), null, WIDTH, HEIGHT, false);
       add(uiBinder.createAndBindUi(this));
 
       nameField.setName(NAME_FIELD_ID);
-      nameField.setHeight(22);
       createButton.setButtonId(CREATE_BUTTON_ID);
       cancelButton.setButtonId(CANCEL_BUTTON_ID);
    }
@@ -121,7 +120,7 @@ public class CreateRubyProjectView extends ViewImpl implements CreateRubyProject
    @Override
    public void focusInProjectNameField()
    {
-      nameField.focusInItem();
+      nameField.setFocus(true);
    }
 
    /**
