9f78cb6fbffa6ee4d0132b60111e0cbc5e00017b
Maxim Musienko <maxim.musienko@exoplatform.com>
2011-11-21 16:52:03 +0000

    IDE-1178

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/AboutDialog.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/AboutDialog.java
@@ -23,6 +23,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import junit.framework.Assert;
+
 import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
 
 import org.openqa.selenium.By;
@@ -44,6 +46,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AboutDialog extends AbstractTestModule
 {
+
+   //Locators basic elements Dialog About Menu
    interface Locators
    {
       String HELP_MENU_LOCATOR =
@@ -51,39 +55,19 @@ public class AboutDialog extends AbstractTestModule
 
       String ABOUT_MENU_LOCATOR =
          "//table[@class=\"exo-popupMenuTable\"]//td[@class=\"exo-popupMenuTitleField\"]/nobr[text()=\"About...\"]";
-      
+
       String ABOUT_WINDOW_LOCATOR = "ideAboutView-window";
 
       String OK_BUTTON_DIALOGABOUT = "ideAboutViewOkButton";
 
       String HELP_MENU = "table.exo-popupMenuTable";
-      
-      String INFO="";
-      
-      
-      
 
-      //      String FIND_BUTTON_ID = "ideFindReplaceTextFormFindButton";
-      //
-      //      String REPLACE_BUTTON_ID = "ideFindReplaceTextFormReplaceButton";
-      //
-      //      String REPLACE_FIND_BUTTON_ID = "ideFindReplaceTextFormReplaceFindButton";
-      //
-      //      String REPLACE_ALL_BUTTON_ID = "ideFindReplaceTextFormReplaceAllButton";
-      //
-      //      String CANCEL_BUTTON_ID = "ideFindReplaceTextFormCancelButton";
-      //
-      //      String FIND_FIELD_ID = "ideFindReplaceTextFormFindField";
-      //
-      //      String REPLACE_FIELD_ID = "ideFindReplaceTextFormReplaceField";
-      //
-      //      String CASE_SENSITIVE_FIELD_ID = "ideFindReplaceTextFormCaseSensitiveField";
-      //
-      //      String FIND_RESULT_LABEL_ID = "ideFindReplaceTextFormFindResult";
+      String INFO_CONTETNT = "//div[@view-id=\"ideAboutView\"]//table/tbody//tr//div[@class=\"gwt-Label\"]";
+
+      String LOGO = "//div[@view-id=\"ideAboutView\"]//table//tbody//tbody//td/img[@class=\"gwt-Image\"]";
    }
 
-   //   public static final String NOT_FOUND_RESULT = "String not found.";
-
+   //WebElemnts DialogAbout menu
    @FindBy(xpath = Locators.HELP_MENU_LOCATOR)
    public WebElement help;
 
@@ -93,6 +77,17 @@ public class AboutDialog extends AbstractTestModule
    @FindBy(id = Locators.OK_BUTTON_DIALOGABOUT)
    private WebElement okButton;
 
+   @FindBy(xpath = Locators.INFO_CONTETNT)
+   private WebElement content;
+
+   @FindBy(xpath = Locators.LOGO)
+   private WebElement logo;
+
+   /**
+    * Call help menu (with the help of click)
+    * @throws Exception
+    * 
+    */
    public void callHelpMenu() throws Exception
    {
       WebElement check = driver().findElement(By.xpath(Locators.HELP_MENU_LOCATOR));
@@ -100,42 +95,42 @@ public class AboutDialog extends AbstractTestModule
       waitOpenedMenuHelp();
    }
 
+   /**
+    * click on About submenu
+    * @throws Exception
+    */
    public void callAboutMenu() throws Exception
    {
       about.click();
       waitOpenedDialogAbout();
    }
 
-   public void closeDialogAbout() throws Exception{
+   /**
+    * Click on button 'ok' and closing About menu 
+    * @throws Exception
+    */
+   public void closeDialogAbout() throws Exception
+   {
       okButton.click();
       waitClosedDialogAbout();
    }
-   
-   //
-   //   @FindBy(id = Locators.REPLACE_BUTTON_ID)
-   //   private WebElement replaceButton;
-   //
-   //   @FindBy(id = Locators.REPLACE_FIND_BUTTON_ID)
-   //   private WebElement replaceFindButton;
-   //
-   //   @FindBy(id = Locators.REPLACE_ALL_BUTTON_ID)
-   //   private WebElement replaceAllButton;
-   //
-   //   @FindBy(id = Locators.CANCEL_BUTTON_ID)
-   //   private WebElement cancelButton;
-   //
-   //   @FindBy(name = Locators.FIND_FIELD_ID)
-   //   private WebElement findField;
-   //
-   //   @FindBy(name = Locators.REPLACE_FIELD_ID)
-   //   private WebElement replaceField;
-   //
-   //   @FindBy(name = Locators.CASE_SENSITIVE_FIELD_ID)
-   //   private WebElement caseSensitiveField;
-   //
-   //   @FindBy(id = Locators.FIND_RESULT_LABEL_ID)
-   //   private WebElement resultLabel;
-   //
+
+   /**
+    * Check basic elements DialogAbout form 
+    */
+   public void checkAboutWindow()
+   {
+
+      logo.isDisplayed();
+      String textContentAbout = content.getText();
+      Assert.assertTrue(textContentAbout.contains("eXo IDE"));
+      Assert.assertTrue(textContentAbout.contains("Version:"));
+      Assert.assertTrue(textContentAbout.contains("eXo Platform SAS (c)"));
+      Assert.assertTrue(textContentAbout.contains("Revision:"));
+      Assert.assertTrue(textContentAbout.contains("Build Time:"));
+
+   }
+
    /**
     * Wait DialogAbout dialog opened.
     * 
@@ -161,6 +156,10 @@ public class AboutDialog extends AbstractTestModule
       });
    }
 
+   /**
+    * Wait opened Help Menu
+    * @throws Exception
+    */
    public void waitOpenedMenuHelp() throws Exception
    {
       new WebDriverWait(driver(), 3).until(new ExpectedCondition<Boolean>()
@@ -204,173 +203,7 @@ public class AboutDialog extends AbstractTestModule
             }
          }
       });
-   
+
    }
-   //
-   //   /**
-   //    * Check the state of the find replace form: all elements to be present and the state of buttons.
-   //    */
-   //   public void checkFindReplaceFormAppeared()
-   //   {
-   //      assertTrue(view.isDisplayed());
-   //      assertTrue(findButton.isDisplayed());
-   //      assertTrue(replaceButton.isDisplayed());
-   //      assertTrue(replaceFindButton.isDisplayed());
-   //      assertTrue(replaceAllButton.isDisplayed());
-   //      assertTrue(cancelButton.isDisplayed());
-   //      assertTrue(findField.isDisplayed());
-   //      assertTrue(replaceField.isDisplayed());
-   //      assertTrue(caseSensitiveField.isDisplayed());
-   //
-   //      // Check buttons state
-   //      assertFalse(isReplaceButtonEnabled());
-   //      assertFalse(isReplaceFindButtonEnabled());
-   //      assertFalse(isFindButtonEnabled());
-   //      assertFalse(isReplaceAllButtonEnabled());
-   //      assertTrue(isCancelButtonEnabled());
-   //   }
-   //
-   //   public void checkFindReplaceFormNotAppeared()
-   //   {
-   //      assertNull(view);
-   //   }
-   //
-   //   /**
-   //    * Get enabled state of cancel button.
-   //    * 
-   //    * @return boolean enabled state of cancel button
-   //    */
-   //   public boolean isCancelButtonEnabled()
-   //   {
-   //      return IDE().BUTTON.isButtonEnabled(cancelButton);
-   //   }
-   //
-   //   /**
-   //    * Get enabled state of find button.
-   //    * 
-   //    * @return boolean enabled state of find button
-   //    */
-   //   public boolean isFindButtonEnabled()
-   //   {
-   //      return IDE().BUTTON.isButtonEnabled(findButton);
-   //   }
-   //
-   //   /**
-   //    * Get enabled state of replace button.
-   //    * 
-   //    * @return boolean enabled state of replace button
-   //    */
-   //   public boolean isReplaceButtonEnabled()
-   //   {
-   //      return IDE().BUTTON.isButtonEnabled(replaceButton);
-   //   }
-   //
-   //   /**
-   //    * Get enabled state of replace/find button.
-   //    * 
-   //    * @return boolean enabled state of replace/find button
-   //    */
-   //   public boolean isReplaceFindButtonEnabled()
-   //   {
-   //      return IDE().BUTTON.isButtonEnabled(replaceFindButton);
-   //   }
-   //
-   //   /**
-   //    * Get enabled state of replace all button.
-   //    * 
-   //    * @return boolean enabled state of replace all button
-   //    */
-   //   public boolean isReplaceAllButtonEnabled()
-   //   {
-   //      return IDE().BUTTON.isButtonEnabled(replaceAllButton);
-   //   }
-   //
-   //   public void typeInFindField(String text) throws InterruptedException
-   //   {
-   //      IDE().INPUT.typeToElement(findField, text, true);
-   //   }
-   //
-   //   public void typeInReplaceField(String text) throws InterruptedException
-   //   {
-   //      IDE().INPUT.typeToElement(replaceField, text, true);
-   //   }
-   //
-   //   public void checkFindFieldNotEmptyState()
-   //   {
-   //      assertTrue(isFindButtonEnabled());
-   //      assertTrue(isReplaceAllButtonEnabled());
-   //      assertFalse(isReplaceButtonEnabled());
-   //      assertFalse(isReplaceFindButtonEnabled());
-   //      assertTrue(isCancelButtonEnabled());
-   //   }
-   //
-   //   public void checkFindFieldEmptyState()
-   //   {
-   //      assertTrue(isFindButtonEnabled());
-   //      assertTrue(isReplaceAllButtonEnabled());
-   //      assertFalse(isReplaceButtonEnabled());
-   //      assertFalse(isReplaceFindButtonEnabled());
-   //      assertTrue(isCancelButtonEnabled());
-   //   }
-   //
-   //   public void clickFindButton() throws InterruptedException
-   //   {
-   //      findButton.click();
-   //   }
-   //
-   //   public void clickReplaceButton() throws InterruptedException
-   //   {
-   //      replaceButton.click();
-   //   }
-   //
-   //   public void clickReplaceFindButton() throws InterruptedException
-   //   {
-   //      replaceFindButton.click();
-   //   }
-   //
-   //   public void clickReplaceAllButton() throws InterruptedException
-   //   {
-   //      replaceAllButton.click();
-   //   }
-   //
-   //   public void clickCancelButton() throws InterruptedException
-   //   {
-   //      cancelButton.click();
-   //   }
-   //
-   //   /**
-   //    * Check buttons when text is found.
-   //    */
-   //   public void checkStateWhenTextFound()
-   //   {
-   //      assertTrue(isFindButtonEnabled());
-   //      assertTrue(isReplaceAllButtonEnabled());
-   //      assertTrue(isReplaceButtonEnabled());
-   //      assertTrue(isReplaceFindButtonEnabled());
-   //      assertTrue(isCancelButtonEnabled());
-   //      assertEquals("", getFindResultText());
-   //   }
-   //
-   //   /**
-   //    * Check buttons when text is not found.
-   //    */
-   //   public void checkStateWhenTextNotFound()
-   //   {
-   //      assertTrue(isFindButtonEnabled());
-   //      assertTrue(isReplaceAllButtonEnabled());
-   //      assertFalse(isReplaceButtonEnabled());
-   //      assertFalse(isReplaceFindButtonEnabled());
-   //      assertTrue(isCancelButtonEnabled());
-   //      assertEquals(NOT_FOUND_RESULT, getFindResultText());
-   //   }
-   //
-   //   public String getFindResultText()
-   //   {
-   //      return resultLabel.getText();
-   //   }
-   //
-   //   public void clickCaseSensitiveField()
-   //   {
-   //      caseSensitiveField.click();
-   //   }
+
 }
