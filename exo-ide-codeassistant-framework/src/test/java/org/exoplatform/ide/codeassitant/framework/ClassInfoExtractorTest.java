5a607fe807b2e7357ae17901f013c26128ba9afc
Sergey  Kabashnyuk <sergey.kabashnyuk@exoplatform.com>
2011-12-26 08:02:08 +0000

    IDE-1295 : cleanup

--- exo-ide-codeassistant-framework/src/test/java/org/exoplatform/ide/codeassitant/framework/ClassInfoExtractorTest.java
+++ exo-ide-codeassistant-framework/src/test/java/org/exoplatform/ide/codeassitant/framework/ClassInfoExtractorTest.java
@@ -18,24 +18,25 @@
  */
 package org.exoplatform.ide.codeassitant.framework;
 
+import junit.framework.TestCase;
+
+import org.exoplatform.ide.codeassistant.framework.server.extractors.TypeInfoExtractor;
+import org.exoplatform.ide.codeassistant.jvm.FieldInfo;
+import org.exoplatform.ide.codeassistant.jvm.JavaType;
+import org.exoplatform.ide.codeassistant.jvm.MethodInfo;
+import org.exoplatform.ide.codeassistant.jvm.TypeInfo;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Collections;
 
-import junit.framework.TestCase;
-
-import org.exoplatform.ide.codeassistant.jvm.FieldInfo;
-import org.exoplatform.ide.codeassistant.jvm.MethodInfo;
-import org.exoplatform.ide.codeassistant.jvm.TypeInfo;
-import org.exoplatform.ide.codeassistant.jvm.JavaType;
-import org.exoplatform.ide.codeassistant.framework.server.extractors.TypeInfoExtractor;
-
 /**
  * Created by The eXo Platform SAS.
+ * 
  * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
  * @version $Id: $
-*/
+ */
 public class ClassInfoExtractorTest extends TestCase
 {
 
@@ -47,39 +48,38 @@ public class ClassInfoExtractorTest extends TestCase
       assertEquals(A.class.getDeclaredMethods().length, cd.getDeclaredMethods().length);
       assertEquals(A.class.getMethods().length, cd.getMethods().length);
       assertEquals(A.class.getFields().length, cd.getFields().length);
-      assertEquals(A.class.getDeclaredFields().length, cd.getDeclaredFields().length); 
+      assertEquals(A.class.getDeclaredFields().length, cd.getDeclaredFields().length);
       assertEquals(A.class.getCanonicalName(), cd.getQualifiedName());
       assertEquals(A.class.getSimpleName(), cd.getName());
-      
+
       Method[] c = Collections.class.getMethods();
-      for (int i = 0; i < c.length; i++)
+      for (Method element : c)
       {
-         System.out.print(c[i].getName() + "(");
-         Type[] types = c[i].getParameterTypes();
-         for (int j = 0; j < types.length; j++)
+         System.out.print(element.getName() + "(");
+         Type[] types = element.getParameterTypes();
+         for (Type type : types)
          {
-            System.out.print(types[j] + ",");
+            System.out.print(type + ",");
          }
          System.out.print(")");
          System.out.println();
       }
    }
-   
-   
+
    public void testExctractField()
    {
       TypeInfo cd = TypeInfoExtractor.extract(A.class);
       FieldInfo[] fds = cd.getDeclaredFields();
       Field[] fields = A.class.getDeclaredFields();
-      for (int i = 0; i < fields.length; i++)
+      for (Field field : fields)
       {
-         FieldInfo fd =getFieldInfo(fds, fields[i].getName());
+         FieldInfo fd = getFieldInfo(fds, field.getName());
          if (fd == null)
          {
             fail();
          }
-         assertEquals(fields[i].getModifiers(), fd.getModifiers().intValue());
-         assertEquals(fields[i].getType().getSimpleName(), fd.getType());
+         assertEquals(field.getModifiers(), fd.getModifiers());
+         assertEquals(field.getType().getSimpleName(), fd.getType());
       }
    }
 
@@ -88,43 +88,45 @@ public class ClassInfoExtractorTest extends TestCase
       for (FieldInfo fd : fds)
       {
          if (fd.getName().equals(fieldName))
+         {
             return fd;
+         }
       }
-      return null;   
+      return null;
    }
-   
-   
-   public void testExctractMethod() 
+
+   public void testExctractMethod()
    {
       TypeInfo cd = TypeInfoExtractor.extract(B.class);
       MethodInfo[] mds = cd.getDeclaredMethods();
       Method[] methods = B.class.getDeclaredMethods();
-      for (int i = 0; i < methods.length; i++)
+      for (Method method : methods)
       {
-         MethodInfo md =getMethodInfo(mds, methods[i].toGenericString());
+         MethodInfo md = getMethodInfo(mds, method.toGenericString());
          if (md == null)
          {
             fail();
          }
-         assertEquals(methods[i].getModifiers(), md.getModifiers().intValue());
+         assertEquals(method.getModifiers(), md.getModifiers());
       }
    }
-   
+
    public void testEnumExtract()
    {
       TypeInfo en = TypeInfoExtractor.extract(E.class);
       assertEquals(JavaType.ENUM.name(), en.getType());
       assertEquals("ONE", en.getFields()[0].getName());
    }
-   
 
    private MethodInfo getMethodInfo(MethodInfo[] mds, String generic)
    {
       for (MethodInfo md : mds)
       {
          if (md.getGeneric().equals(generic))
+         {
             return md;
+         }
       }
-      return null;   
+      return null;
    }
 }
