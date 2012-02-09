553aad2d6942f2652a96853e4891f55ca8dfb656
Maxim Musienko <maxim.musienko@exoplatform.com>
2012-02-09 09:48:14 +0000

    IDE-1178 IDE-1447 large timeout for open form

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/Templates.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/core/Templates.java
@@ -81,7 +81,7 @@ public class Templates extends AbstractTestModule
     */
    public void waitOpened() throws Exception
    {
-      new WebDriverWait(driver(), 2).until(new ExpectedCondition<Boolean>()
+      new WebDriverWait(driver(), 5).until(new ExpectedCondition<Boolean>()
       {
          @Override
          public Boolean apply(WebDriver input)
