ae104ae4761d5b39ea2ad115778953c33ba928c9
Artem Zatsarynnyy <azatsarynnyy@exoplatform.com>
2012-05-16 17:37:23 +0300

    IDE-1643: Clean up project types, description and change projects order in 'Create new project' dialog

--- exo-ide-framework-client/src/main/java/org/exoplatform/ide/client/framework/util/ProjectResolver.java
+++ exo-ide-framework-client/src/main/java/org/exoplatform/ide/client/framework/util/ProjectResolver.java
@@ -18,11 +18,13 @@
  */
 package org.exoplatform.ide.client.framework.util;
 
-import org.exoplatform.ide.client.framework.ui.IconImageBundle;
-
 import com.google.gwt.resources.client.ImageResource;
 
+import org.exoplatform.ide.client.framework.ui.IconImageBundle;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -38,6 +40,11 @@ public class ProjectResolver
    private static Map<String, ImageResource> types = new HashMap<String, ImageResource>();
 
    /**
+    * List contains sorted project types.
+    */
+   private static List<String> sortedTypes = new ArrayList<String>();
+
+   /**
     * Ruby on Rails
     */
    public static final String RAILS = "Rails";
@@ -50,17 +57,27 @@ public class ProjectResolver
    /**
     * eXo Development Framework : Chromattic, Groovy REST
     */
-   public static final String EXO_APP = "eXo App";
+   public static final String EXO_APP = "eXo";
 
    /**
     * Static Web Project: HTML, JS, CSS
     */
-   public static final String STATIC_WEB = "Static Web";
+   public static final String STATIC_WEB = "Javascript";
 
    /**
     * Servlet and JSP API based project
     */
-   public static final String SERVLET_JSP = "Servlet/JSP";
+   public static final String SERVLET_JSP = "JSP";
+
+   /**
+    * PHP Project
+    */
+   public static final String PHP = "PHP";
+
+   /**
+    * Empty Project
+    */
+   public static final String UNDEFINED = "Undefined";
 
    static
    {
@@ -71,12 +88,40 @@ public class ProjectResolver
          types.put(EXO_APP, IconImageBundle.INSTANCE.defaultProject());
          types.put(STATIC_WEB, IconImageBundle.INSTANCE.defaultProject());
          types.put(SERVLET_JSP, IconImageBundle.INSTANCE.javaProject());
+         types.put(PHP, IconImageBundle.INSTANCE.defaultProject());
+         types.put(UNDEFINED, IconImageBundle.INSTANCE.defaultProject());
       }
+
+      sortedTypes.add(UNDEFINED);
+      sortedTypes.add(EXO_APP);
+      sortedTypes.add(STATIC_WEB);
+      sortedTypes.add(SERVLET_JSP);
+      sortedTypes.add(PHP);
+      sortedTypes.add(RAILS);
+      sortedTypes.add(SPRING);
    }
 
    public static Set<String> getProjectsTypes()
    {
       return types.keySet();
+   }
+
+   /**
+    * Returns index of project type.
+    * 
+    * @param type project type
+    * @return index of project type
+    */
+   public static int getIndexOfProjectType(String type)
+   {
+      if (sortedTypes.contains(type))
+      {
+         return sortedTypes.indexOf(type);
+      }
+      else
+      {
+         return -1;
+      }
    }
 
    public static ImageResource getImageForProject(String type)
