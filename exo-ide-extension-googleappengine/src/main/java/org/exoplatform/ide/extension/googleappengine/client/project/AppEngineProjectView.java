0b6cb2d2f127f1f7da119ce1f4486ac0f10a6f1a
Anna Shumilova <azhuleva@exoplatform.com>
2012-05-28 13:52:24 +0300

    IDE-1652: added icons

--- exo-ide-extension-googleappengine/src/main/java/org/exoplatform/ide/extension/googleappengine/client/project/AppEngineProjectView.java
+++ exo-ide-extension-googleappengine/src/main/java/org/exoplatform/ide/extension/googleappengine/client/project/AppEngineProjectView.java
@@ -56,16 +56,18 @@ public class AppEngineProjectView extends ViewImpl implements AppEngineProjectPr
       add(uiBinder.createAndBindUi(this));
 
       mainTabPain = new MainTabPain();
-      applicationTabPanel.addTab(GENERAL_TAB_ID, null,
+      applicationTabPanel.addTab(GENERAL_TAB_ID, new Image(GAEClientBundle.INSTANCE.general()),
          GoogleAppEngineExtension.GAE_LOCALIZATION.manageApplicationGeneralTab(), mainTabPain, false);
 
       cronTabPane = new CronTabPane();
-      applicationTabPanel.addTab(CRONS_TAB_ID, null,
-         GoogleAppEngineExtension.GAE_LOCALIZATION.manageApplicationCronsTab(), cronTabPane, false);
+   /*   applicationTabPanel.addTab(CRONS_TAB_ID, new Image(GAEClientBundle.INSTANCE.crons()),
+         GoogleAppEngineExtension.GAE_LOCALIZATION.manageApplicationCronsTab(), cronTabPane, false);*/
 
       backendsTabPane = new BackendsTabPane();
-      applicationTabPanel.addTab(BACKENDS_TAB_ID, null,
-         GoogleAppEngineExtension.GAE_LOCALIZATION.manageApplicationBackendsTab(), backendsTabPane, false);
+      /*
+       * applicationTabPanel.addTab(BACKENDS_TAB_ID, new Image(GAEClientBundle.INSTANCE.backends()),
+       * GoogleAppEngineExtension.GAE_LOCALIZATION.manageApplicationBackendsTab(), backendsTabPane, false);
+       */
    }
 
    /**
