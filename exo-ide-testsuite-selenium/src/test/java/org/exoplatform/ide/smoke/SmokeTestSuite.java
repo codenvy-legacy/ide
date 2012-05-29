493beb8b1f5bfc32b20d99c50ba914da44010d99
Dmytro Nochevnov <dnochevnov@exoplatform.com>
2012-05-29 11:30:27 +0300

    Restore test package name (IDE-1094)

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/smoke/SmokeTestSuite.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/smoke/SmokeTestSuite.java
@@ -16,7 +16,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.exoplatform.ide.asmoke;
+package org.exoplatform.ide.smoke;
 
 import org.exoplatform.ide.extension.maven.client.BuildSuccessedTest;
 import org.exoplatform.ide.git.AddTest;
