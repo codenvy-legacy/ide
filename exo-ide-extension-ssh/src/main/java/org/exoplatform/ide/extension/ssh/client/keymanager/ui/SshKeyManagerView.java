84947d44f18940081b34df57fdd363b01224e26c
Vitaly Parfonov <vparfonov@exoplatform.com>
2012-07-04 17:27:04 +0300

    IDE-1732

--- exo-ide-extension-ssh/src/main/java/org/exoplatform/ide/extension/ssh/client/keymanager/ui/SshKeyManagerView.java
+++ exo-ide-extension-ssh/src/main/java/org/exoplatform/ide/extension/ssh/client/keymanager/ui/SshKeyManagerView.java
@@ -18,8 +18,6 @@
  */
 package org.exoplatform.ide.extension.ssh.client.keymanager.ui;
 
-import com.google.gwt.uibinder.client.UiFactory;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -29,8 +27,6 @@ import com.google.gwt.user.client.ui.Widget;
 import org.exoplatform.gwtframework.ui.client.component.ImageButton;
 import org.exoplatform.ide.client.framework.ui.impl.ViewImpl;
 import org.exoplatform.ide.client.framework.ui.impl.ViewType;
-import org.exoplatform.ide.extension.ssh.client.SshKeyExtension;
-import org.exoplatform.ide.extension.ssh.client.SshLocalizationConstant;
 import org.exoplatform.ide.extension.ssh.client.keymanager.SshKeyManagerPresenter.Display;
 import org.exoplatform.ide.extension.ssh.shared.KeyItem;
 
@@ -57,20 +53,9 @@ public class SshKeyManagerView extends ViewImpl implements Display
 
    public SshKeyManagerView()
    {
-      super(ID, ViewType.MODAL, "Ssh Keys", null, 333, 400);
+      super(ID, ViewType.MODAL, "Ssh Keys", null, 400, 400, false);
       setCloseOnEscape(true);
       add(uiBinder.createAndBindUi(this));
-   }
-
-   /**
-    * Factory method, uses for UiBinder
-    * 
-    * @return instance of {@link SshLocalizationConstant}
-    */
-   @UiFactory
-   public SshLocalizationConstant getSshLocalizationConstant()
-   {
-      return SshKeyExtension.CONSTANTS;
    }
 
    /**
