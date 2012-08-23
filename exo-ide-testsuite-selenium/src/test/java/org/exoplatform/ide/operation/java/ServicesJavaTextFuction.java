2aac5c8ed54cb23bae64e017d84c6610cf505ba3
Maxim Musienko <mmusienkio@exoplatform.com>
2012-08-23 17:19:28 +0300

    IDE-759 Add selenium - tetsts for coverage text - functions in new java editor

---
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/java/ServicesJavaTextFuction.java
@@ -0,0 +1,57 @@
+package org.exoplatform.ide.operation.java;
+
+import org.exoplatform.ide.BaseTest;
+
+public class ServicesJavaTextFuction extends BaseTest
+{
+  
+   public void waitEditorIsReady(String PROJECT) throws Exception
+   {
+      IDE.EDITOR.waitActiveFile(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SimpleSum.java.java");
+      IDE.PROGRESS_BAR.waitProgressBarControlClose();
+   }
+   
+
+   public void waitFormatTestIsReady(String PROJECT) throws Exception
+   {
+      IDE.EDITOR.waitActiveFile(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SumController.java");
+      IDE.PROGRESS_BAR.waitProgressBarControlClose();
+   }
+   
+   public void openSpringJavaTetsFile(String PROJECT) throws Exception
+   {
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SumController.java");
+      IDE.PROJECT.EXPLORER.openItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SumController.java");
+      waitEditorIsReady(PROJECT);
+   }
+   
+   public void openJavaClassForFormat(String PROJECT) throws Exception
+   {
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller");
+      IDE.PROJECT.EXPLORER.expandItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller");
+      IDE.PROJECT.EXPLORER.waitForItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SimpleSum.java");
+      IDE.PROJECT.EXPLORER.openItem(PROJECT + "/" + "src" + "/" + "main" + "/" + "java/" + "sumcontroller" + "/"
+         + "SimpleSum.java");
+      waitEditorIsReady(PROJECT);
+   }
+   
+   
+   
+}
