10bf4ca500af72a989fa7867b4a924c2d73b4320
Andrey Parfonov <aparfonov@exoplatform.com>
2012-09-19 16:41:06 +0300

    IDE-1920 : add request for run ec2 instance

--- exo-ide-extension-aws/src/main/java/org/exoplatform/ide/extension/aws/shared/ec2/Image.java
+++ exo-ide-extension-aws/src/main/java/org/exoplatform/ide/extension/aws/shared/ec2/Image.java
@@ -18,21 +18,25 @@
  */
 package org.exoplatform.ide.extension.aws.shared.ec2;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
  * @version $Id: $
  */
 public enum Image
 {
-   t1_micro("Micro","t1.micro", 1, "Up to 2 ECUs", "613 MiB"),
-   m1_small("Small","m1.small", 1, "1 ECU", "1.7 GiB"),
-   m1_medium("Medium","m1.medium", 1, "2 ECU", "3.7 GiB"),
-   m1_large("Large","m1.large", 2, "4 ECU", "7.5 GiB"),
+   t1_micro("Micro", "t1.micro", 1, "Up to 2 ECUs", "613 MiB"),
+   m1_small("Small", "m1.small", 1, "1 ECU", "1.7 GiB"),
+   m1_medium("Medium", "m1.medium", 1, "2 ECU", "3.7 GiB"),
+   m1_large("Large", "m1.large", 2, "4 ECU", "7.5 GiB"),
    m1_xlarge("Extra Large", "m1.xlarge", 4, "8 ECU", "15 GiB"),
    m2_xlarge("High-Memory Extra Large", "m2.xlarge", 2, "6.5 ECU", "17.1 GiB"),
    m2_2xlarge("High-Memory Double Extra Large", "m2.2xlarge", 4, "13 ECU", "34.2 GiB"),
    m2_4xlarge("High-Memory Quadruple Extra Large", "m2.4xlarge", 8, "26 ECU", "68.4 GiB"),
-   c1_medium("High-CPU Medium","c1.medium", 2, "5 ECU", "1.7 GiB"),
+   c1_medium("High-CPU Medium", "c1.medium", 2, "5 ECU", "1.7 GiB"),
    c1_xlarge("High-CPU Extra Large", "c1.xlarge", 8, "20 ECU", "7 GiB"),
    hi1_4xlarge("High I/O Quadruple Extra Large", "hi1.4xlarge", 8, "35 ECU", "60.5 GiB");
 
@@ -41,6 +45,22 @@ public enum Image
    private final int cpuCores;
    private final String cpuUnits;
    private final String memory;
+   private static final List<String> availableInstanceTypes;
+
+   static
+   {
+      List<String> types = new ArrayList<String>(11);
+      for (Image image : Image.values())
+      {
+         types.add(image.getId());
+      }
+      availableInstanceTypes = java.util.Collections.unmodifiableList(types);
+   }
+
+   public List<String> availableInstanceTypes()
+   {
+      return availableInstanceTypes;
+   }
 
    private Image(String name, String id, int cpuCores, String cpuUnits, String memory)
    {
