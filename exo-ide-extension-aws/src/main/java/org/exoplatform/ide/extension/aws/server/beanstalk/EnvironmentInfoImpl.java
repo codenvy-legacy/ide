eb8cb5c125a86ee39440012cd1d68a471a4e87da
Andrey Parfonov <aparfonov@exoplatform.com>
2012-09-24 16:09:22 +0300

    IDE-1842 : cleanup code

--- exo-ide-extension-aws/src/main/java/org/exoplatform/ide/extension/aws/server/beanstalk/EnvironmentInfoImpl.java
+++ exo-ide-extension-aws/src/main/java/org/exoplatform/ide/extension/aws/server/beanstalk/EnvironmentInfoImpl.java
@@ -116,19 +116,23 @@ public class EnvironmentInfoImpl implements EnvironmentInfo
 
       public Builder created(Date created)
       {
-         if (created != null)
+         if (created == null)
          {
-            this.created = created.getTime();
+            this.created = -1;
+            return this;
          }
+         this.created = created.getTime();
          return this;
       }
 
       public Builder updated(Date updated)
       {
-         if (updated != null)
+         if (updated == null)
          {
-            this.updated = updated.getTime();
+            this.updated = -1;
+            return this;
          }
+         this.updated = updated.getTime();
          return this;
       }
 
