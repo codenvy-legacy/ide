06989ec33f1128c867ccbf12227e7eedab1ef299
Vitaly Parfonov <vparfonov@exoplatform.com>
2012-11-20 17:15:48 +0200

    Rmove resources from exo-ide-extension-ruby

--- exo-ide-extension-ruby/src/test/resources/RailsDemo/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-RailsDemo::Application.config.session_store :cookie_store, :key => '_RailsDemo_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# RailsDemo::Application.config.session_store :active_record_store
