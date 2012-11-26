7857d031696fa74a5aa5e58cde3e4da1d5f709ca
Maxim Musienko <mmusienkio@exoplatform.com>
2012-11-26 11:19:14 +0200

    IDE-759 remove tests related with GET Item URL Menu

--- exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/browse/BrowseTestSuite.java
+++ exo-ide-testsuite-selenium/src/test/java/org/exoplatform/ide/operation/browse/BrowseTestSuite.java
@@ -18,7 +18,7 @@
  */
 package org.exoplatform.ide.operation.browse;
 
-import org.exoplatform.ide.operation.upload.OpenFileByURLTest;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -30,7 +30,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ExploringWorkspacePanelTest.class, GetItemUrlTes.class, GoToFolderTest.class, OpeningFilesTest.class,
-   UsingKeyboardTest.class, ItemOrderingTest.class, OpenFileByURLTest.class, ProjectsListGridTest.class,
+   UsingKeyboardTest.class, ItemOrderingTest.class,  ProjectsListGridTest.class,
    ShowHideHiddenFilesTest.class})
 public class BrowseTestSuite
 {
