33a2b5432b4b0b77b36cea35609e2d4564d5d396
Andrey Parfonov <aparfonov@exoplatform.com>
2012-11-30 18:22:03 +0200

    IDE-2044

--- exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/shared/AccessControlEntry.java
+++ exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/shared/AccessControlEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 eXo Platform SAS.
+ * Copyright (C) 2012 eXo Platform SAS.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -18,72 +18,31 @@
  */
 package org.exoplatform.ide.vfs.shared;
 
-import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Representation of Access Control Entry used to interaction with client via JSON.
  *
- * @author <a href="mailto:andrey.parfonov@exoplatform.com">Andrey Parfonov</a>
- * @version $Id: AccessControlEntry.java 79579 2012-02-17 13:27:25Z andrew00x $
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id: $
  */
-public class AccessControlEntry
+public interface AccessControlEntry
 {
-   /** Principal name. */
-   private String principal;
-
-   /** Permissions. */
-   private Set<String> permissions;
-
-   /** Empty AccessControlEntry instance. Both principal and permissions are not set. */
-   public AccessControlEntry()
-   {
-   }
+   /** @return principal's permissions */
+   Set<String> getPermissions();
 
    /**
-    * AccessControlEntry instance with specified principal and permissions.
-    *
-    * @param principal principal
-    * @param permissions permissions
+    * @param permissions
+    *    new set of permissions
     */
-   public AccessControlEntry(String principal, Set<String> permissions)
-   {
-      this.principal = principal;
-      this.permissions = permissions;
-   }
-
-   /** @return principal's permissions */
-   public Set<String> getPermissions()
-   {
-      if (permissions == null)
-      {
-         permissions = new HashSet<String>();
-      }
-      return permissions;
-   }
-
-   /** @param permissions new set of permissions */
-   public void setPermissions(Set<String> permissions)
-   {
-      this.permissions = permissions;
-   }
+   void setPermissions(Set<String> permissions);
 
    /** @return principal name */
-   public String getPrincipal()
-   {
-      return principal;
-   }
+   String getPrincipal();
 
-   /** @param principal principal name */
-   public void setPrincipal(String principal)
-   {
-      this.principal = principal;
-   }
-
-   /** @see java.lang.Object#toString() */
-   @Override
-   public String toString()
-   {
-      return "AccessControlEntry [principal=" + principal + ", permissions=" + permissions + ']';
-   }
+   /**
+    * @param principal
+    *    principal name
+    */
+   void setPrincipal(String principal);
 }
