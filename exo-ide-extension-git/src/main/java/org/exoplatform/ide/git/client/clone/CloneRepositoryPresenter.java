f26b3a3e50097153f9c243cda1078215344520d9
Vitaliy Guliy <vguliy@exoplatform.com>
2012-12-10 12:50:17 +0200

    IDE-1795

--- exo-ide-extension-git/src/main/java/org/exoplatform/ide/git/client/clone/CloneRepositoryPresenter.java
+++ exo-ide-extension-git/src/main/java/org/exoplatform/ide/git/client/clone/CloneRepositoryPresenter.java
@@ -19,6 +19,8 @@
 package org.exoplatform.ide.git.client.clone;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
