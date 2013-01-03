e6213f23914a57841ec26fb002e108a943835dbd
Andrey Parfonov <aparfonov@exoplatform.com>
2013-01-03 10:08:13 +0200

    IDE-2139 : Add cache abstraction and Segmented LRU cache implementation in vfs core project.

---
+++ exo-ide-vfs/src/main/java/org/exoplatform/ide/vfs/server/cache/ConcurrentSLRUCache.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.vfs.server.cache;
+
+/**
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id: $
+ */
+public class ConcurrentSLRUCache<K, V> implements Cache<K, V>
+{
+   private final int mask;
+   private final SLRUCache<K, V>[] partitions;
+
+   @SuppressWarnings("unchecked")
+   private ConcurrentSLRUCache(int partitionsNum, int protectedSize, int probationarySize)
+   {
+      int p = 1;
+      while (p < partitionsNum)
+      {
+         p <<= 2;
+      }
+      int partitionProtectedSize = protectedSize / p;
+      int partitionProbationarySize = probationarySize / p;
+      partitions = new SLRUCache[p];
+      for (int i = 0; i < partitions.length; i++)
+      {
+         partitions[i] = new SLRUCache<K, V>(partitionProtectedSize, partitionProbationarySize);
+      }
+      mask = p - 1;
+   }
+
+   @Override
+   public V get(K key)
+   {
+      if (key == null)
+      {
+         throw new IllegalArgumentException("Null key is not allowed. ");
+      }
+      return partitions[key.hashCode() & mask].get(key);
+   }
+
+   @Override
+   public V put(K key, V value)
+   {
+      if (key == null)
+      {
+         throw new IllegalArgumentException("Null key is not allowed. ");
+      }
+      if (value == null)
+      {
+         throw new IllegalArgumentException("Null value is not allowed. ");
+      }
+      return partitions[key.hashCode() & mask].put(key, value);
+   }
+
+   @Override
+   public V remove(K key)
+   {
+      if (key == null)
+      {
+         throw new IllegalArgumentException("Null key is not allowed. ");
+      }
+      return partitions[key.hashCode() & mask].remove(key);
+   }
+
+   @Override
+   public boolean contains(K key)
+   {
+      return key != null && partitions[key.hashCode() & mask].contains(key);
+   }
+
+   @Override
+   public void clear()
+   {
+      for (SLRUCache<K, V> partition : partitions)
+      {
+         partition.clear();
+      }
+   }
+}
