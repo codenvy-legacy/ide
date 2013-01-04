43608abb0771e566990f56d864a0bbb35531bc83
Artem Zatsarynnyy <azatsarynnyy@exoplatform.com>
2013-01-04 16:52:31 +0200

    IDE-2159: add test for delete resources

--- eclipse-jdt-core/src/test/java/org/exoplatform/ide/eclipse/resources/DeleteTest.java
+++ eclipse-jdt-core/src/test/java/org/exoplatform/ide/eclipse/resources/DeleteTest.java
@@ -18,7 +18,16 @@
  */
 package org.exoplatform.ide.eclipse.resources;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.junit.Test;
 
 /**
@@ -32,29 +41,89 @@ public class DeleteTest extends ResourcesBaseTest
 {
    private WorkspaceResource ws;
 
+   private IProject projectResource;
+
+   private IFolder emptyFolderResource;
+
+   private IFolder nonEmptyFolderResource;
+
+   private IFile fileResource;
+
    @Override
    public void setUp() throws Exception
    {
       super.setUp();
       ws = new WorkspaceResource(vfs);
+
+      projectResource = (IProject)ws.newResource(new Path("/project"), IResource.PROJECT);
+      ws.createResource(projectResource);
+
+      emptyFolderResource =
+         (IFolder)ws.newResource(projectResource.getFullPath().append("empty_folder"), IResource.FOLDER);
+      ws.createResource(emptyFolderResource);
+
+      nonEmptyFolderResource =
+         (IFolder)ws.newResource(projectResource.getFullPath().append("non_empty_folder"), IResource.FOLDER);
+      ws.createResource(nonEmptyFolderResource);
+      ws.createResource(ws.newResource(nonEmptyFolderResource.getFullPath().append("file"), IResource.FILE));
+
+      fileResource = (IFile)ws.newResource(projectResource.getFullPath().append("file"), IResource.FILE);
+      ws.createResource(fileResource);
    }
 
    @Test
-   @Ignore("TODO")
    public void testDeleteProject() throws Exception
    {
+      assertTrue(projectResource.exists());
+      IResource[] members = projectResource.members();
+      assertTrue(members.length > 0);
+      for (IResource member : members)
+      {
+         assertTrue(member.exists());
+      }
+
+      projectResource.delete(true, new NullProgressMonitor());
+      assertFalse(projectResource.exists());
+      for (IResource member : members)
+      {
+         assertFalse(member.exists());
+      }
    }
 
    @Test
-   @Ignore("TODO")
-   public void testDeleteFolder() throws Exception
+   public void testDeleteEmptyFolder() throws Exception
    {
+      assertTrue(emptyFolderResource.exists());
+      assertEquals(0, emptyFolderResource.members().length);
+      emptyFolderResource.delete(true, new NullProgressMonitor());
+      assertFalse(emptyFolderResource.exists());
    }
 
    @Test
-   @Ignore("TODO")
+   public void testDeleteNonEmptyFolder() throws Exception
+   {
+      assertTrue(nonEmptyFolderResource.exists());
+      IResource[] members = nonEmptyFolderResource.members();
+      assertTrue(members.length > 0);
+      for (IResource member : members)
+      {
+         assertTrue(member.exists());
+      }
+
+      nonEmptyFolderResource.delete(true, new NullProgressMonitor());
+      assertFalse(nonEmptyFolderResource.exists());
+      for (IResource member : members)
+      {
+         assertFalse(member.exists());
+      }
+   }
+
+   @Test
    public void testDeleteFile() throws Exception
    {
+      assertTrue(fileResource.exists());
+      fileResource.delete(true, new NullProgressMonitor());
+      assertFalse(fileResource.exists());
    }
 
 }
