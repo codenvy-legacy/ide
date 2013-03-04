ba4e503070387dbf0e8cbaa87d03147c3e35d89e
Vitaly Parfonov <vparfonov@codenvy.com>
2013-03-04 10:48:25 +0200

    Prepeare move PaaS credentials to LDAP

---
+++ exo-ide-commons/src/main/java/org/exoplatform/ide/commons/WriteCredentialsException.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.commons;
+
+/**
+ * @author <a href="mailto:vparfonov@codenvy.com">Vitaly Parfonov</a>
+ * @version $Id: WriteCredentilasException.java Mar 1, 2013 vetal $
+ *
+ */
+@SuppressWarnings("serial")
+public class WriteCredentialsException extends Exception
+{
+   /**
+    * @param message the detail message
+    * @param cause the cause
+    */
+   public WriteCredentialsException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+   /**
+    * @param message the detail message
+    */
+   public WriteCredentialsException(String message)
+   {
+      super(message);
+   }
+
+   /**
+    * @param cause the cause
+    */
+   public WriteCredentialsException(Throwable cause)
+   {
+      super(cause);
+   }
+
+
+}
