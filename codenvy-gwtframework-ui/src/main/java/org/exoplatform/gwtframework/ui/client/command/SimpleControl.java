9975d0a6debc590416a25bed01909ca589533d0b
Vladislav Zhukovskii <vzhukovskii@codenvy.com>
2013-03-07 15:28:23 +0200

    IDE-2377: Toolbar update according new spec

--- codenvy-gwtframework-ui/src/main/java/org/exoplatform/gwtframework/ui/client/command/SimpleControl.java
+++ codenvy-gwtframework-ui/src/main/java/org/exoplatform/gwtframework/ui/client/command/SimpleControl.java
@@ -235,8 +235,10 @@ public class SimpleControl extends Control<SimpleControl>
 
    public Map<String, String> getAttributes()
    {
-      if (attributes != null)
+      if (attributes == null)
+      {
          attributes = new HashMap<String, String>();
+      }
       return attributes;
    }
 
