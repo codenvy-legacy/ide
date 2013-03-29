4aa0e722d93da32a17d49b865333014227e3b556
Nikolay Zamosenchuk <nzamosenchuk@exoplatform.com>
2013-03-29 09:57:14 +0200

    IDEX-59 : update CoreGinModule

--- codenvy-ide-core/src/main/java/com/codenvy/ide/core/inject/CoreGinModule.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/core/inject/CoreGinModule.java
@@ -16,29 +16,21 @@
  */
 package com.codenvy.ide.core.inject;
 
-import com.codenvy.ide.extension.ExtensionRegistry;
-
+import com.codenvy.ide.Resources;
 import com.codenvy.ide.api.editor.CodenvyTextEditor;
 import com.codenvy.ide.api.editor.DocumentProvider;
 import com.codenvy.ide.api.editor.EditorAgent;
 import com.codenvy.ide.api.editor.EditorProvider;
 import com.codenvy.ide.api.editor.EditorRegistry;
-
-import com.codenvy.ide.api.paas.PaaSAgent;
-
-
 import com.codenvy.ide.api.expressions.ExpressionManager;
-
 import com.codenvy.ide.api.extension.ExtensionGinModule;
-
-import com.codenvy.ide.api.resources.FileType;
-import com.codenvy.ide.api.resources.ModelProvider;
-
-import com.codenvy.ide.Resources;
+import com.codenvy.ide.api.paas.PaaSAgent;
 import com.codenvy.ide.api.parts.ConsolePart;
 import com.codenvy.ide.api.parts.OutlinePart;
 import com.codenvy.ide.api.parts.ProjectExplorerPart;
 import com.codenvy.ide.api.parts.WelcomePart;
+import com.codenvy.ide.api.resources.FileType;
+import com.codenvy.ide.api.resources.ModelProvider;
 import com.codenvy.ide.api.resources.ResourceProvider;
 import com.codenvy.ide.api.selection.SelectionAgent;
 import com.codenvy.ide.api.ui.keybinding.KeyBindingAgent;
@@ -56,6 +48,7 @@ import com.codenvy.ide.core.editor.EditorAgentImpl;
 import com.codenvy.ide.core.editor.EditorRegistryImpl;
 import com.codenvy.ide.core.editor.ResourceDocumentProvider;
 import com.codenvy.ide.core.expressions.ExpressionManagerImpl;
+import com.codenvy.ide.extension.ExtensionRegistry;
 import com.codenvy.ide.keybinding.KeyBindingManager;
 import com.codenvy.ide.loader.EmptyLoader;
 import com.codenvy.ide.loader.Loader;
@@ -117,31 +110,41 @@ public class CoreGinModule extends AbstractGinModule
    @Override
    protected void configure()
    {
+      // generic bindings
       bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
       bind(Loader.class).to(EmptyLoader.class).in(Singleton.class);
       bind(Resources.class).in(Singleton.class);
       bind(ExtensionRegistry.class).in(Singleton.class);
       bind(StandardComponentInitializer.class).in(Singleton.class);
-      bind(PaaSAgent.class).to(PaaSAgentImpl.class).in(Singleton.class);
 
-      bind(SelectionAgent.class).to(SelectionAgentImpl.class).in(Singleton.class);
+      apiBindingConfigure();
 
       resourcesAPIconfigure();
 
-      uiModelConfigure();
-      uiAPIconfigure();
+      coreUiConfigure();
 
       editorAPIconfigure();
    }
 
    /**
-    * 
+    * API Bindings, binds API interfaces to the implementations
     */
-   private void uiModelConfigure()
+   private void apiBindingConfigure()
    {
+      // Agents
+      bind(KeyBindingAgent.class).to(KeyBindingManager.class).in(Singleton.class);
+      bind(ExpressionManager.class).to(ExpressionManagerImpl.class).in(Singleton.class);
+      bind(SelectionAgent.class).to(SelectionAgentImpl.class).in(Singleton.class);
+      bind(WorkspaceAgent.class).to(WorkspacePresenter.class).in(Singleton.class);
+      bind(MainMenuAgent.class).to(MainMenuPresenter.class).in(Singleton.class);
+      bind(ToolbarAgent.class).to(ToolbarPresenter.class).in(Singleton.class);
+      bind(PreferencesAgent.class).to(PreferencesAgentImpl.class).in(Singleton.class);
+      bind(WizardAgent.class).to(WizardAgentImpl.class).in(Singleton.class);
+      bind(PaaSAgent.class).to(PaaSAgentImpl.class).in(Singleton.class);
+      // UI Model
       bind(PartStack.class).to(PartStackPresenter.class);
       bind(EditorPartStack.class).to(EditorPartStackPresenter.class).in(Singleton.class);
-
+      // Parts
       bind(ConsolePart.class).to(ConsolePartPresenter.class).in(Singleton.class);
       bind(WelcomePart.class).to(WelcomePartPresenter.class).in(Singleton.class);
       bind(OutlinePart.class).to(OutlinePartPrenter.class).in(Singleton.class);
@@ -154,7 +157,6 @@ public class CoreGinModule extends AbstractGinModule
     */
    protected void editorAPIconfigure()
    {
-
       bind(DocumentFactory.class).to(DocumentFactoryImpl.class).in(Singleton.class);
       bind(CodenvyTextEditor.class).to(TextEditorPresenter.class);
       bind(EditorAgent.class).to(EditorAgentImpl.class).in(Singleton.class);
@@ -176,39 +178,20 @@ public class CoreGinModule extends AbstractGinModule
       bind(ModelProvider.class).to(GenericModelProvider.class).in(Singleton.class);
    }
 
-   protected void uiAPIconfigure()
+   /**
+    * Configure Core UI components, resouces and views
+    */
+   protected void coreUiConfigure()
    {
-      bind(WizardAgent.class).to(WizardAgentImpl.class).in(Singleton.class);
-      bind(NewGenericFilePageView.class).to(NewGenericFilePageViewImpl.class).in(Singleton.class);
-
-      bind(PreferencesAgent.class).to(PreferencesAgentImpl.class).in(Singleton.class);
-
-      bind(WorkspaceView.class).to(WorkspaceViewImpl.class).in(Singleton.class);
-      bind(WorkspaceAgent.class).to(WorkspacePresenter.class).in(Singleton.class);
-
-      // expression manager
-      bind(ExpressionManager.class).to(ExpressionManagerImpl.class).in(Singleton.class);
-
-      // main menu
-      bind(MainMenuPresenter.class).in(Singleton.class);
-      bind(MainMenuView.class).to(MainMenuViewImpl.class).in(Singleton.class);
-      bind(MainMenuAgent.class).to(MainMenuPresenter.class).in(Singleton.class);
-
-      // toolbar
-      bind(ToolbarView.class).to(ToolbarViewImpl.class).in(Singleton.class);
-      bind(ToolbarAgent.class).to(ToolbarPresenter.class).in(Singleton.class);
-
-      // part agent
-      bind(PartStackView.class).to(PartStackViewImpl.class);
-
-      // resources: images and css
+      // Resources
       bind(PartStackUIResources.class).to(Resources.class).in(Singleton.class);
-
-      // key binding
-      bind(KeyBindingAgent.class).to(KeyBindingManager.class).in(Singleton.class);
-
+      // Views
+      bind(WorkspaceView.class).to(WorkspaceViewImpl.class).in(Singleton.class);
+      bind(MainMenuView.class).to(MainMenuViewImpl.class).in(Singleton.class);
+      bind(NewGenericFilePageView.class).to(NewGenericFilePageViewImpl.class).in(Singleton.class);
+      bind(ToolbarView.class).to(ToolbarViewImpl.class).in(Singleton.class);
+      bind(PartStackView.class).to(PartStackViewImpl.class);
       bind(ProjectExplorerView.class).to(ProjectExplorerViewImpl.class).in(Singleton.class);
-
       bind(ConsolePartView.class).to(ConsolePartViewImpl.class).in(Singleton.class);
    }
 
