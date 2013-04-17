c4be945e1710812caad5d6b6eee292314d23d33b
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-04-17 17:08:47 +0300

    IDE-2560: add PHP-runtime extension

---
+++ exo-ide-extension-php-runtime/src/main/java/org/exoplatform/ide/extension/php/client/PhpExtensionAutoBeanFactory.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.extension.php.client;
+
+import com.google.web.bindery.autobean.shared.AutoBean;
+import com.google.web.bindery.autobean.shared.AutoBeanFactory;
+
+import org.exoplatform.ide.extension.php.shared.ApplicationInstance;
+
+/**
+ * The interface for the {@link AutoBean} generator.
+ * 
+ * @author <a href="mailto:azatsarynnyy@codenvy.com">Artem Zatsarynnyy</a>
+ * @version $Id: PhpExtensionAutoBeanFactory.java Apr 17, 2013 4:15:48 PM azatsarynnyy $
+ *
+ */
+public interface PhpExtensionAutoBeanFactory extends AutoBeanFactory {
+    AutoBean<ApplicationInstance> applicationInstance();
+}
---
+++ exo-ide-extension-php-runtime/src/main/java/org/exoplatform/ide/extension/php/client/PhpExtensionLocalizationConstant.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.extension.php.client;
+
+/**
+ * 
+ * @author <a href="mailto:azatsarynnyy@codenvy.com">Artem Zatsarynnyy</a>
+ * @version $Id: PhpExtensionLocalizationConstant.java Apr 17, 2013 4:07:00 PM azatsarynnyy $
+ *
+ */
+public interface PhpExtensionLocalizationConstant extends com.google.gwt.i18n.client.Messages {
+    @Key("run.application.control.title")
+    String runApplicationControlTitle();
+
+    @Key("run.application.control.prompt")
+    String runApplicationControlPrompt();
+
+    @Key("starting.project.message")
+    String startingProjectMessage(String project);
+
+    @Key("project.started.message")
+    String projectStartedMessage(String project);
+
+    @Key("start.application.failed")
+    String startApplicationFailed();
+
+    @Key("application.started.url")
+    String applicationStartedUrl(String application, String url);
+
+    @Key("stop.application.control.title")
+    String stopApplicationControlTitle();
+
+    @Key("stop.application.control.prompt")
+    String stopApplicationControlPrompt();
+
+    @Key("stopping.project.message")
+    String stoppingProjectMessage(String project);
+
+    @Key("stop.application.failed")
+    String stopApplicationFailed();
+
+    @Key("project.stopped.message")
+    String projectStoppedMessage(String project);
+
+    @Key("not.php.project")
+    String notPhpProject();
+
+    @Key("no.run.application")
+    String noRunningApplication();
+
+    @Key("show.logs.control.title")
+    String showLogsControlTitle();
+
+    @Key("show.logs.control.prompt")
+    String showLogsControlPrompt();
+
+    @Key("get.logs.error.message")
+    String getLogsErrorMessage();
+}
