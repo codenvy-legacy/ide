b5ac6f00b60b98e8ec979762155e6c15f0656f8a
Valeriy Svydenko <vsvydenko@codenvy.com>
2013-04-22 13:08:21 +0300

    IDE-2565: add node.js-runtime extension

---
+++ exo-ide-extension-nodejs-runtime/src/main/java/org/exoplatform/ide/extension/nodejs/client/NodeJsRuntimeExtension.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.ide.extension.nodejs.client;
+
+import com.google.gwt.core.client.GWT;
+
+import org.exoplatform.ide.client.framework.application.event.InitializeServicesEvent;
+import org.exoplatform.ide.client.framework.application.event.InitializeServicesHandler;
+import org.exoplatform.ide.client.framework.module.Extension;
+import org.exoplatform.ide.client.framework.module.IDE;
+import org.exoplatform.ide.extension.nodejs.client.logs.LogsHandler;
+import org.exoplatform.ide.extension.nodejs.client.run.RunApplicationManager;
+
+/**
+ * PHP runtime extension.
+ * 
+ * @author <a href="mailto:vsvydenko@codenvy.com">Valeriy Svydenko</a>
+ * @version $Id: NodeJsRuntimeExtension.java Apr 18, 2013 4:19:43 PM vsvydenko $
+ *
+ */
+public class NodeJsRuntimeExtension extends Extension implements InitializeServicesHandler {
+    public static final NodeJsExtensionAutoBeanFactory AUTO_BEAN_FACTORY = GWT
+            .create(NodeJsExtensionAutoBeanFactory.class);
+
+    public static final NodeJsExtensionLocalizationConstant NODEJS_LOCALIZATION = GWT.create(NodeJsExtensionLocalizationConstant.class);
+
+    /** @see org.exoplatform.ide.client.framework.application.event.InitializeServicesHandler#onInitializeServices(org.exoplatform.ide
+     * .client.framework.application.event.InitializeServicesEvent) */
+    @Override
+    public void onInitializeServices(InitializeServicesEvent event) {
+        new NodeJsRuntimeServiceImpl(event.getApplicationConfiguration().getContext());
+    }
+
+    /** @see org.exoplatform.ide.client.framework.module.Extension#initialize() */
+    @Override
+    public void initialize() {
+        IDE.addHandler(InitializeServicesEvent.TYPE, this);
+
+        new RunApplicationManager();
+        new LogsHandler();
+    }
+}
