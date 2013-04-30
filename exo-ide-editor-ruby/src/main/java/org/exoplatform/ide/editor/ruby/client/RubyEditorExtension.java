8f2bc4dc30a3476f49077ee4e8813732e9f0fac3
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-04-30 12:47:28 +0300

    IDE-2598: use CollabEditor for Ruby

--- exo-ide-editor-ruby/src/main/java/org/exoplatform/ide/editor/ruby/client/RubyEditorExtension.java
+++ exo-ide-editor-ruby/src/main/java/org/exoplatform/ide/editor/ruby/client/RubyEditorExtension.java
@@ -16,7 +16,6 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
 package org.exoplatform.ide.editor.ruby.client;
 
 import com.google.gwt.core.client.GWT;
@@ -29,22 +28,17 @@ import org.exoplatform.ide.client.framework.application.event.InitializeServices
 import org.exoplatform.ide.client.framework.application.event.InitializeServicesHandler;
 import org.exoplatform.ide.client.framework.control.GroupNames;
 import org.exoplatform.ide.client.framework.control.NewItemControl;
+import org.exoplatform.ide.client.framework.editor.AddCommentsModifierEvent;
 import org.exoplatform.ide.client.framework.module.EditorCreator;
 import org.exoplatform.ide.client.framework.module.Extension;
 import org.exoplatform.ide.client.framework.module.FileType;
 import org.exoplatform.ide.client.framework.module.IDE;
 import org.exoplatform.ide.editor.client.api.Editor;
-import org.exoplatform.ide.editor.codemirror.CodeMirror;
-import org.exoplatform.ide.editor.codemirror.CodeMirrorConfiguration;
-import org.exoplatform.ide.editor.ruby.client.codeassistant.RubyCodeAssistant;
-import org.exoplatform.ide.editor.ruby.client.codemirror.RubyAutocompleteHelper;
-import org.exoplatform.ide.editor.ruby.client.codemirror.RubyOutlineItemCreator;
-import org.exoplatform.ide.editor.ruby.client.codemirror.RubyParser;
 
 /**
  * Provides a text editing area along with UI for executing text commands on the.<br>
  * Support syntax coloration for Ruby language (http://www.ruby-lang.org/en/)
- *
+ * 
  * @author <a href="mailto:vitaly.parfonov@gmail.com">Vitaly Parfonov</a>
  * @author <a href="mailto:dnochevnov@exoplatform.com">Dmytro Nochevnov</a>
  * @version $Revision$
@@ -57,7 +51,7 @@ public class RubyEditorExtension extends Extension implements InitializeServices
     }
 
     /** Localizable constants and messages */
-    private final Messages messages = GWT.create(Messages.class);
+    private final Messages             messages        = GWT.create(Messages.class);
 
     public final static DefaultContent DEFAULT_CONTENT = GWT.create(DefaultContent.class);
 
@@ -66,68 +60,52 @@ public class RubyEditorExtension extends Extension implements InitializeServices
     public void initialize() {
         IDE.addHandler(InitializeServicesEvent.TYPE, this);
 
-        IDE.getInstance().addControl(
-                new NewItemControl("File/New/New Ruby File", "Ruby File", "Create Ruby File",
-                                   RubyClientBundle.INSTANCE.ruby(), RubyClientBundle.INSTANCE.rubyDisabled(), MimeType.APPLICATION_RUBY)
-                        .setGroupName(GroupNames.NEW_SCRIPT));
+        IDE.getInstance().addControl(new NewItemControl("File/New/New Ruby File", "Ruby File", "Create Ruby File",
+                                                        RubyClientBundle.INSTANCE.ruby(), RubyClientBundle.INSTANCE.rubyDisabled(),
+                                                        MimeType.APPLICATION_RUBY).setGroupName(GroupNames.NEW_SCRIPT));
 
         RubyClientBundle.INSTANCE.css().ensureInjected();
     }
 
     public void onInitializeServices(InitializeServicesEvent event) {
-        IDE.getInstance().getFileTypeRegistry().addFileType(
-                new FileType(MimeType.APPLICATION_RUBY, "rb", RubyClientBundle.INSTANCE.ruby()),
-                new EditorCreator() {
-                    @Override
-                    public Editor createEditor() {
-                        return new CodeMirror(MimeType.APPLICATION_RUBY, new CodeMirrorConfiguration()
-                                .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
-                                .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
-                                .setParser(new RubyParser())
-                                .setCanBeOutlined(true)
-                                .setAutocompleteHelper(new RubyAutocompleteHelper())
-                                .setCodeAssistant(new RubyCodeAssistant()));
-                    }
-                });
+        IDE.getInstance().getFileTypeRegistry()
+           .addFileType(
+                        new FileType(MimeType.APPLICATION_RUBY, "rb", RubyClientBundle.INSTANCE.ruby()),
+                        new EditorCreator() {
+                            @Override
+                            public Editor createEditor() {
+                                return new RubyEditor(MimeType.APPLICATION_RUBY);
+                                // return new CodeMirror(MimeType.APPLICATION_RUBY, new CodeMirrorConfiguration()
+                                // .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
+                                // .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
+                                // .setParser(new RubyParser())
+                                // .setCanBeOutlined(true)
+                                // .setAutocompleteHelper(new RubyAutocompleteHelper())
+                                // .setCodeAssistant(new RubyCodeAssistant()));
+                            }
+                        });
 
-        IDE.getInstance().getFileTypeRegistry().addFileType(
-                new FileType("application/x-ruby+html", "rb", RubyClientBundle.INSTANCE.ruby()),
-                new EditorCreator() {
-                    @Override
-                    public Editor createEditor() {
-                        return new CodeMirror("application/x-ruby+html", new CodeMirrorConfiguration()
-                                .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
-                                .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
-                                .setParser(new RubyParser())
-                                .setCanBeOutlined(true)
-                                .setAutocompleteHelper(new RubyAutocompleteHelper())
-                                .setCodeAssistant(new RubyCodeAssistant()));
-                    }
-                });
+        IDE.getInstance().getFileTypeRegistry()
+           .addFileType(
+                        new FileType("MimeType.APPLICATION_RUBY_HTML", "rb", RubyClientBundle.INSTANCE.ruby()),
+                        new EditorCreator() {
+                            @Override
+                            public Editor createEditor() {
+                                return new RubyEditor(MimeType.APPLICATION_RUBY_HTML);
+                                // return new CodeMirror(MimeType.APPLICATION_RUBY_HTML, new CodeMirrorConfiguration()
+                                // .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
+                                // .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
+                                // .setParser(new RubyParser())
+                                // .setCanBeOutlined(true)
+                                // .setAutocompleteHelper(new RubyAutocompleteHelper())
+                                // .setCodeAssistant(new RubyCodeAssistant()));
+                            }
+                        });
 
-//      IDE.getInstance().addEditor(new CodeMirror(MimeType.APPLICATION_RUBY, "CodeMirror Ruby script editor", "rb", 
-//         new CodeMirrorConfiguration()
-//            .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
-//            .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
-//            .setParser(new RubyParser())
-//            .setCanBeOutlined(true)
-//            .setAutocompleteHelper(new RubyAutocompleteHelper())
-//            .setCodeAssistant(new RubyCodeAssistant())
-//      ));
+        // IDE.getInstance().addOutlineItemCreator(MimeType.APPLICATION_RUBY, new RubyOutlineItemCreator());
+        // IDE.getInstance().addOutlineItemCreator(MimeType.APPLICATION_RUBY_HTML, new RubyOutlineItemCreator());
 
-//      IDE.getInstance().addEditor(
-//         new CodeMirrorProducer(MimeType.APPLICATION_RUBY, "CodeMirror Ruby script editor", "rb",
-//            RubyClientBundle.INSTANCE.ruby(), true, new CodeMirrorConfiguration()
-//               .setGenericParsers("['parseruby.js', 'tokenizeruby.js']")
-//               .setGenericStyles("['" + CodeMirrorConfiguration.PATH + "css/rubycolors.css']")
-//               .setParser(new RubyParser()).setCanBeOutlined(true).setAutocompleteHelper(new RubyAutocompleteHelper())
-//               .setCodeAssistant(new RubyCodeAssistant())));
-
-
-        IDE.getInstance().addOutlineItemCreator(MimeType.APPLICATION_RUBY, new RubyOutlineItemCreator());
-        IDE.getInstance().addOutlineItemCreator("application/x-ruby+html", new RubyOutlineItemCreator());
-
-        //TODO   IDE.fireEvent(new AddCommentsModifierEvent(MimeType.APPLICATION_RUBY, new RubyCommentModifier()));
+        IDE.fireEvent(new AddCommentsModifierEvent(MimeType.APPLICATION_RUBY, new RubyCommentModifier()));
     }
 
 }
