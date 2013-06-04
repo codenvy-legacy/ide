3ee0887d1325923ce3b02d2793b931c1375ceea9
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-06-04 11:24:15 +0300

    IDE-2783: Javascript autocomplete is triggered on tab

--- exo-ide-editor-javascript/src/main/java/org/exoplatform/ide/editor/javascript/client/contentassist/JavaScriptExplicitAutocompleter.java
+++ exo-ide-editor-javascript/src/main/java/org/exoplatform/ide/editor/javascript/client/contentassist/JavaScriptExplicitAutocompleter.java
@@ -33,7 +33,7 @@ import com.google.gwt.regexp.shared.RegExp;
 /** Implementation that adds JavaScript-specific cases. */
 class JavaScriptExplicitAutocompleter extends ExplicitAutocompleter {
 
-    private RegExp regExp = RegExp.compile("/(\\*)+\n$", "g");
+    private RegExp regExp         = RegExp.compile("/(\\*)+\n$", "g");
 
     private RegExp commentEndMach = RegExp.compile("(\\*)+[^/]");
 
@@ -67,7 +67,8 @@ class JavaScriptExplicitAutocompleter extends ExplicitAutocompleter {
 
         // 'auto-complete as you type' feature
         final char signalChar = signal.getChar();
-        if (signalChar != '{' && signalChar != ';' && signalChar != ' ' && signalChar != '(' && signalChar != ')' && signalChar != '\'' && signalChar != '"') {
+        if (signalChar != '{' && signalChar != ';' && signalChar != ' ' && signalChar != '(' && signalChar != ')' && signalChar != '\''
+            && signalChar != '"' && signalChar != '\t') {
             if (!popupIsShown && signalChar != 0 && KeyCodes.KEY_ENTER != signalChar) {
                 return ExplicitAction.DEFERRED_COMPLETE;
             }
@@ -105,9 +106,9 @@ class JavaScriptExplicitAutocompleter extends ExplicitAutocompleter {
     /**
      * Checks trigger to be plain "Enter" key press.
      * <p/>
-     * <p>"Shift-Enter" also works to avoid "sticky-shift" issue:
-     * when someone quickly types "Shift-[" (-> "{") and then
-     * press "Enter" while "Shift" is not depressed.
+     * <p>
+     * "Shift-Enter" also works to avoid "sticky-shift" issue: when someone quickly types "Shift-[" (-> "{") and then press "Enter" while
+     * "Shift" is not depressed.
      */
     private static boolean checkEnterTrigger(SignalEventEssence trigger) {
         return KeySignalType.INPUT == trigger.type && KeyCodes.KEY_ENTER == trigger.keyCode && !trigger.altKey
