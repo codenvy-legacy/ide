ecc5e70db7912a82e0bd49b25828b3d7bd03d0c0
Valeriy Svydenko <vsvydenko@codenvy.com>
2013-06-27 18:15:53 +0300

    IDE-2865: disable invitations link for r\o user

--- exo-ide-extension-samples/src/main/java/org/exoplatform/ide/extension/samples/client/startpage/StartPagePresenter.java
+++ exo-ide-extension-samples/src/main/java/org/exoplatform/ide/extension/samples/client/startpage/StartPagePresenter.java
@@ -110,9 +110,7 @@ public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandl
             @Override
             public void onClick(ClickEvent event) {
                 if (IDE.isRoUser()) {
-                    if (readOnlyUserView == null)
-                        readOnlyUserView = new ReadOnlyUserView(IDE.user.getWorkspaces());
-                    IDE.getInstance().openView(readOnlyUserView);
+                    return;
                 } else {
                     IDE.fireEvent(new InviteGoogleDevelopersEvent());
                 }
