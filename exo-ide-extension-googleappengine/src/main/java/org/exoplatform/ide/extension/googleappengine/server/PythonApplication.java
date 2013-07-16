bf9ff1c7fea6ca42e4edbe1dd9a5825d9e8f18d3
Andrey Parfonov <aparfonov@codenvy.com>
2013-07-16 10:17:07 +0300

    IDE-2768

--- exo-ide-extension-googleappengine/src/main/java/org/exoplatform/ide/extension/googleappengine/server/PythonApplication.java
+++ exo-ide-extension-googleappengine/src/main/java/org/exoplatform/ide/extension/googleappengine/server/PythonApplication.java
@@ -16,7 +16,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.exoplatform.ide.extension.googleappengine.server.python;
+package org.exoplatform.ide.extension.googleappengine.server;
 
 import com.google.appengine.repackaged.net.sourceforge.yamlbeans.YamlException;
 import com.google.appengine.repackaged.net.sourceforge.yamlbeans.YamlReader;
