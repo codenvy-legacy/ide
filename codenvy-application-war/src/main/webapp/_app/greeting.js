14f643e926ae38db849b58648051cd84d7d0612d
Vitaly Parfonov <vparfonov@codenvy.com>
2013-08-01 11:52:22 +0300

    Gteeting page movw to script

---
+++ codenvy-application-war/src/main/webapp/_app/greeting.js
@@ -0,0 +1,6 @@
+var greetingPaneContent = {
+   "anonymous": null,
+    "authenticated": null,
+    "anonymous-workspace-temporary": "<%= genIdeStaticResourceUrl(request, "greeting/anonymous-workspace-temporary.html")%>",
+    "authenticated-workspace-temporary": "<%= genIdeStaticResourceUrl(request, "greeting/authenticated-workspace-temporary.html")%>"
+}
