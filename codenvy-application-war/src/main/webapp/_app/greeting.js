21ad8247327eb7a550c2cd921757ae1e2920e300
Vitaliy Guliy <vguliy@codenvy.com>
2013-08-05 17:49:11 +0300

    IDE-2910

--- codenvy-application-war/src/main/webapp/_app/greeting.js
+++ codenvy-application-war/src/main/webapp/_app/greeting.js
@@ -1,6 +1,6 @@
 var greetingPaneContent = {
     "anonymous": null,
 	"authenticated": null,
-	"anonymous-workspace-temporary": ide_base_path + "temporary-workspace-rightpane-not-authenticated.html",
-	"authenticated-workspace-temporary": ide_base_path + "temporary-workspace-rightpane-authenticated.html"
+	"anonymous-workspace-temporary": ide_base_path + "greeting/temporary-workspace-rightpane-not-authenticated.html",
+	"authenticated-workspace-temporary": ide_base_path + "greeting/temporary-workspace-rightpane-authenticated.html"
 };
