4b569436bfea7dc93723be98f12b9dc12cab6663
Andrey Plotnikov <aplotnikov@codenvy.com>
2013-08-12 16:36:53 +0300

    IDEX-122: Put new License header to client module

--- codenvy-ide-client/src/main/webapp/_app/browserNotSupported.js
+++ codenvy-ide-client/src/main/webapp/_app/browserNotSupported.js
@@ -1,3 +1,21 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+
 var USER_AGENT = navigator.userAgent.toLowerCase();
 var IS_WS_SUPPORTED = ("WebSocket" in window);
 if (!IS_WS_SUPPORTED || (USER_AGENT.indexOf("chrome") == -1 && USER_AGENT.indexOf("firefox") == -1 && USER_AGENT.indexOf("safari") == -1)) {
