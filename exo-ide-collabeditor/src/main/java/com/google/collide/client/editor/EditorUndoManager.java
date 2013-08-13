44afd1684322624cf1bcc772e0591252bb393374
Evgen Vidolob <evidolob@codenvy.com>
2013-08-13 11:26:48 +0300

    IDE-2093 show generated code at once

--- exo-ide-collabeditor/src/main/java/com/google/collide/client/editor/EditorUndoManager.java
+++ exo-ide-collabeditor/src/main/java/com/google/collide/client/editor/EditorUndoManager.java
@@ -25,8 +25,12 @@ import com.google.collide.shared.document.LineInfo;
 import com.google.collide.shared.document.Position;
 import com.google.collide.shared.document.TextChange;
 import com.google.collide.shared.document.util.PositionUtils;
-import com.google.collide.shared.ot.*;
+import com.google.collide.shared.ot.Composer;
 import com.google.collide.shared.ot.Composer.ComposeException;
+import com.google.collide.shared.ot.DocOpApplier;
+import com.google.collide.shared.ot.DocOpUtils;
+import com.google.collide.shared.ot.Inverter;
+import com.google.collide.shared.ot.Transformer;
 
 import org.waveprotocol.wave.model.operation.OperationPair;
 import org.waveprotocol.wave.model.operation.TransformException;
@@ -45,7 +49,7 @@ public class EditorUndoManager {
      * Delegates to the document operation algorithms when requested by the Wave
      * undo library.
      */
-    private static final UndoManagerImpl.Algorithms<DocOp> ALGORITHMS = new Algorithms<DocOp>() {
+    private static final UndoManagerImpl.Algorithms<DocOp> ALGORITHMS         = new Algorithms<DocOp>() {
         @Override
         public DocOp invert(DocOp operation) {
             return Inverter.invert(ClientDocOpFactory.INSTANCE, operation);
@@ -71,60 +75,12 @@ public class EditorUndoManager {
             }
         }
     };
+    private final        CheckpointProducer                checkpointProducer = new CheckpointProducer();
 
-    public static EditorUndoManager create(Editor editor, Document document,
-                                           SelectionModel selection) {
-        return new EditorUndoManager(editor, document, selection,
-                                     new UndoManagerImpl<DocOp>(ALGORITHMS));
-    }
-
-  /*
-   * TODO: think about which other events should cause a
-   * checkpoint. Push out to a toplevel class if it gets complicated.
-   */
-
-    /**
-     * Produces undo checkpoints at opportune times, such as when the user
-     * explicitly moves the cursor (arrow keys or mouse click) or when the user's
-     * text mutations change from delete to insert.
+    /*
+     * TODO: think about which other events should cause a
+     * checkpoint. Push out to a toplevel class if it gets complicated.
      */
-    private class CheckpointProducer
-            implements
-            SelectionModel.CursorListener,
-            Editor.BeforeTextListener,
-            Editor.TextListener {
-
-        private TextChange.Type previousTextChangeType;
-
-        @Override
-        public void onCursorChange(LineInfo lineInfo, int column, boolean isExplicitChange) {
-            if (isExplicitChange) {
-                undoManager.checkpoint();
-            }
-        }
-
-        @Override
-        public void onBeforeTextChange(TextChange textChange) {
-            if (previousTextChangeType != textChange.getType()) {
-                undoManager.checkpoint();
-            }
-
-            previousTextChangeType = textChange.getType();
-        }
-
-        @Override
-        public void onTextChange(TextChange textChange) {
-            if (textChange.getText().contains("\n")) {
-        /*
-         * Checkpoint after newlines which tends to be a good granularity for
-         * typical editor use
-         */
-                undoManager.checkpoint();
-            }
-        }
-    }
-
-    private final CheckpointProducer checkpointProducer = new CheckpointProducer();
     private final Document document;
     private final Document.TextListener documentTextListener = new Document.TextListener() {
         @Override
@@ -142,9 +98,8 @@ public class EditorUndoManager {
             }
         }
     };
-
     private final Editor editor;
-    private final Editor.TextListener editorTextListener = new Editor.TextListener() {
+    private final Editor.TextListener                  editorTextListener = new Editor.TextListener() {
         @Override
         public void onTextChange(TextChange textChange) {
 
@@ -158,12 +113,11 @@ public class EditorUndoManager {
                                                                    textChange));
         }
     };
-
-    private boolean isMutatingDocument;
-    private final JsonArray<ListenerRegistrar.Remover> listenerRemovers =
+    private final JsonArray<ListenerRegistrar.Remover> listenerRemovers   =
             JsonCollections.createArray();
     private final SelectionModel         selection;
     private final UndoManagerPlus<DocOp> undoManager;
+    private       boolean                isMutatingDocument;
 
     private EditorUndoManager(Editor editor, Document document, SelectionModel selection,
                               UndoManagerPlus<DocOp> undoManager) {
@@ -177,6 +131,12 @@ public class EditorUndoManager {
         listenerRemovers.add(selection.getCursorListenerRegistrar().add(checkpointProducer));
         listenerRemovers.add(editor.getBeforeTextListenerRegistrar().add(checkpointProducer));
         listenerRemovers.add(editor.getTextListenerRegistrar().add(checkpointProducer));
+    }
+
+    public static EditorUndoManager create(Editor editor, Document document,
+                                           SelectionModel selection) {
+        return new EditorUndoManager(editor, document, selection,
+                                     new UndoManagerImpl<DocOp>(ALGORITHMS));
     }
 
     boolean isMutatingDocument() {
@@ -242,5 +202,46 @@ public class EditorUndoManager {
         selection.setSelection(new LineInfo(textChange.getLine(), textChange.getLineNumber()),
                                textChange.getColumn(), endPosition.getLineInfo(),
                                endPosition.getColumn());
+    }
+
+    /**
+     * Produces undo checkpoints at opportune times, such as when the user
+     * explicitly moves the cursor (arrow keys or mouse click) or when the user's
+     * text mutations change from delete to insert.
+     */
+    private class CheckpointProducer
+            implements
+            SelectionModel.CursorListener,
+            Editor.BeforeTextListener,
+            Editor.TextListener {
+
+        private TextChange.Type previousTextChangeType;
+
+        @Override
+        public void onCursorChange(LineInfo lineInfo, int column, boolean isExplicitChange) {
+            if (isExplicitChange) {
+                undoManager.checkpoint();
+            }
+        }
+
+        @Override
+        public void onBeforeTextChange(TextChange textChange) {
+            if (previousTextChangeType != textChange.getType()) {
+                undoManager.checkpoint();
+            }
+
+            previousTextChangeType = textChange.getType();
+        }
+
+        @Override
+        public void onTextChange(TextChange textChange) {
+            if (textChange.getText().contains("\n")) {
+        /*
+         * Checkpoint after newlines which tends to be a good granularity for
+         * typical editor use
+         */
+                undoManager.checkpoint();
+            }
+        }
     }
 }
