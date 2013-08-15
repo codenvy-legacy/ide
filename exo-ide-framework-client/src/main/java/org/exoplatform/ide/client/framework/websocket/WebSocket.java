10c79e268b08b20866a4e6e73b7ab505475fcf3b
Evgen Vidolob <evidolob@codenvy.com>
2013-08-15 11:30:57 +0300

    fix second web socket creation

--- exo-ide-framework-client/src/main/java/org/exoplatform/ide/client/framework/websocket/WebSocket.java
+++ exo-ide-framework-client/src/main/java/org/exoplatform/ide/client/framework/websocket/WebSocket.java
@@ -46,6 +46,12 @@ public class WebSocket extends JavaScriptObject {
      */
     public static native WebSocket create(String url)
    /*-{
+       if($wnd["ide_websoket"]){
+          soket = $wnd["ide_websoket"];
+          if(url === soket.url){
+              return soket;
+          }
+       }
        return new WebSocket(url);
    }-*/;
 
