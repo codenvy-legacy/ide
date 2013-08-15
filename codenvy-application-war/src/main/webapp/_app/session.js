f481cc43c42f0a16f03d55bbd61172ed15b7c759
Evgen Vidolob <evidolob@codenvy.com>
2013-08-15 11:30:57 +0300

    IDE-3026 fix user activity log

--- codenvy-application-war/src/main/webapp/_app/session.js
+++ codenvy-application-war/src/main/webapp/_app/session.js
@@ -1,3 +1,9 @@
+var url = (('https:' == window.location.protocol) ? 'wss:' : 'ws:') + '//' + window.location.host + '/ide/websocket/' + ws;
+var websocket = new WebSocket(url);
+window["ide_websoket"] = websocket;
+websocket.url = url;
+var messages = [];
+
 function sendSessionStatus(appName, sessionId, status) {
     var nVer = navigator.appVersion;
     var nAgt = navigator.userAgent;
@@ -37,13 +43,32 @@ function sendSessionStatus(appName, sessionId, status) {
         fullVersion = fullVersion.substring(0, ix);
 
     var browserInfo = browserName + '/' + fullVersion;
+    var url = ws + '/session/' + appName + '/' + status;
+    var mes = {body: JSON.stringify({sessionId: sessionId, browserInfo: browserInfo}), method: "POST", path: url, uuid: generate(), "headers": [
+        {name: "content-type", value: "application/json"}
+    ]};
 
-    var url = '//' + window.location.host + '/ide/rest/' + ws + '/session/'
-        + appName + '/' + status + '?sessionId=' + sessionId
-        + '&browserInfo=' + browserInfo;
-    var websocket = new WebSocket('https:' == window.location.protocol ? 'wss:'
-        : 'ws:' + url);
-    websocket.send(appName);
+    if (websocket.readyState == WebSocket.OPEN) {
+        if (messages.length > 0) {
+            for (var message in messages)
+                sendMessage(message);
+            messages = [];
+        }
+        sendMessage(mes);
+    } else {
+        messages[messages.length] = mes;
+    }
+}
+
+function sendMessage(message) {
+    try {
+        websocket.send(JSON.stringify(message));
+    }
+    catch (err) {
+        if (window.console) {
+            window.console.error(err);
+        }
+    }
 }
 
 function generate() {
