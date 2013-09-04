5bc1f5de657a79f174c66b57921d619ecc1a0d29
Vitaliy Guliy <vguliy@codenvy.com>
2013-09-04 12:33:14 +0300

    CLDIDE-1056 restored functionality of HTMLEditor

--- exo-ide-editor-html/src/main/java/org/exoplatform/ide/editor/html/client/HtmlEditor.java
+++ exo-ide-editor-html/src/main/java/org/exoplatform/ide/editor/html/client/HtmlEditor.java
@@ -43,7 +43,6 @@ public class HtmlEditor extends CollabEditor {
      */
     public HtmlEditor(String mimeType) {
         super(mimeType);
-        /*
         CssAutocompleter cssAutocompleter = CssAutocompleter.create();
         HtmlAutocompleter htmlAutocompleter = HtmlAutocompleter.create(cssAutocompleter, new JavaScriptAutocompleter());
         editorBundle.getAutocompleter().addLanguageSpecificAutocompleter(htmlAutocompleter);
@@ -51,7 +50,6 @@ public class HtmlEditor extends CollabEditor {
                 IDocument.DEFAULT_CONTENT_TYPE,
                 new HtmlContentAssistProcessor(new CssContentAssistProcessor(cssAutocompleter),
                                                new JavaScriptContentAssistProcessor()));
-                                               */
     }
 
     /** @see com.google.collide.client.CollabEditor#isCapable(org.exoplatform.ide.editor.client.api.EditorCapability) */
