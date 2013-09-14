341e0a6ec7a4fccf993bfa2d925a975fb9741c8c
Andrey Plotnikov <aplotnikov@codenvy.com>
2013-09-14 16:27:47 +0300

    Added tests for DeleteRepository presenter

--- codenvy-ext-git/src/test/java/com/codenvy/ide/ext/git/client/delete/DeleteRepositoryPresenterTest.java
+++ codenvy-ext-git/src/test/java/com/codenvy/ide/ext/git/client/delete/DeleteRepositoryPresenterTest.java
@@ -17,16 +17,25 @@
  */
 package com.codenvy.ide.ext.git.client.delete;
 
+import com.codenvy.ide.api.event.RefreshBrowserEvent;
+import com.codenvy.ide.commons.exception.ExceptionThrownEvent;
 import com.codenvy.ide.ext.git.client.BaseTest;
+import com.codenvy.ide.resources.model.Project;
 import com.codenvy.ide.rest.AsyncRequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.googlecode.gwt.test.utils.GwtReflectionUtils;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.InjectMocks;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.lang.reflect.Method;
 
 import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Testing {@link DeleteRepositoryPresenter} functionality.
@@ -34,19 +43,80 @@ import static org.mockito.Mockito.verify;
  * @author <a href="mailto:aplotnikov@codenvy.com">Andrey Plotnikov</a>
  */
 public class DeleteRepositoryPresenterTest extends BaseTest {
-    @InjectMocks
     private DeleteRepositoryPresenter presenter;
 
+    @Override
+    public void disarm() {
+        super.disarm();
+
+        presenter = new DeleteRepositoryPresenter(service, eventBus, constant, console, resourceProvider);
+    }
+
     @Test
-    @Ignore
-    // Ignore this test because this method uses native method
-    // (boolean needToDelete = Window.confirm(constant.deleteGitRepositoryQuestion(repository));)
-    public void testDeleteRepository() throws Exception {
+    public void testDeleteRepositoryWhenDeleteRepositoryIsSuccessful() throws Exception {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] arguments = invocation.getArguments();
+                AsyncRequestCallback<Void> callback = (AsyncRequestCallback<Void>)arguments[2];
+                Method onSuccess = GwtReflectionUtils.getMethod(callback.getClass(), "onSuccess");
+                onSuccess.invoke(callback, (Void)null);
+                return callback;
+            }
+        }).when(service).deleteRepository(anyString(), anyString(), (AsyncRequestCallback<Void>)anyObject());
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] arguments = invocation.getArguments();
+                AsyncCallback<Project> callback = (AsyncCallback<Project>)arguments[0];
+                Method onSuccess = GwtReflectionUtils.getMethod(callback.getClass(), "onSuccess");
+                onSuccess.invoke(callback, project);
+                return callback;
+            }
+        }).when(project).refreshProperties((AsyncCallback<Project>)anyObject());
+
         presenter.deleteRepository();
 
         verify(resourceProvider).getActiveProject();
         verify(project).getPath();
-
         verify(service).deleteRepository(eq(VFS_ID), eq(PROJECT_ID), (AsyncRequestCallback<Void>)anyObject());
+        verify(console).print(anyString());
+        verify(constant).deleteGitRepositorySuccess();
+        verify(eventBus).fireEvent((RefreshBrowserEvent)anyObject());
+    }
+
+    @Test
+    public void testDeleteRepositoryWhenDeleteRepositoryIsFailed() throws Exception {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] arguments = invocation.getArguments();
+                AsyncRequestCallback<Void> callback = (AsyncRequestCallback<Void>)arguments[2];
+                Method onFailure = GwtReflectionUtils.getMethod(callback.getClass(), "onFailure");
+                onFailure.invoke(callback, mock(Throwable.class));
+                return callback;
+            }
+        }).when(service).deleteRepository(anyString(), anyString(), (AsyncRequestCallback<Void>)anyObject());
+
+        presenter.deleteRepository();
+
+        verify(resourceProvider).getActiveProject();
+        verify(project).getPath();
+        verify(service).deleteRepository(eq(VFS_ID), eq(PROJECT_ID), (AsyncRequestCallback<Void>)anyObject());
+        verify(console).print(anyString());
+        verify(eventBus).fireEvent((ExceptionThrownEvent)anyObject());
+    }
+
+    @Test
+    public void testDeleteRepositoryWhenRequestExceptionHappened() throws Exception {
+        doThrow(RequestException.class).when(service).deleteRepository(anyString(), anyString(), (AsyncRequestCallback<Void>)anyObject());
+
+        presenter.deleteRepository();
+
+        verify(resourceProvider).getActiveProject();
+        verify(project).getPath();
+        verify(service).deleteRepository(eq(VFS_ID), eq(PROJECT_ID), (AsyncRequestCallback<Void>)anyObject());
+        verify(console).print(anyString());
+        verify(eventBus).fireEvent((ExceptionThrownEvent)anyObject());
     }
 }
