8b6a2790b4d516687d1345d942844c6b864101a4
Valeriy Svydenko <vsvydenko@codenvy.com>
2013-09-20 18:22:47 +0300

    CLDIDE-1180: set git icons when 'Link with Editor' button had been turned on

--- exo-ide-client/src/main/java/org/exoplatform/ide/client/project/explorer/ProjectExplorerPresenter.java
+++ exo-ide-client/src/main/java/org/exoplatform/ide/client/project/explorer/ProjectExplorerPresenter.java
@@ -506,6 +506,7 @@ public class ProjectExplorerPresenter implements SelectItemHandler,
 
             if (linkingWithEditor && editorActiveFile != null) {
                 display.selectItem(editorActiveFile);
+                IDE.fireEvent(new TreeRefreshedEvent(openedProject));
             }
         }
     };
