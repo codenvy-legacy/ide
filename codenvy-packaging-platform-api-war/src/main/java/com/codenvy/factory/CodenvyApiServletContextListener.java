bb086e1a5a887252052372afe2562dc7ad9de26a
Max Shaposhnik <mshaposhnik@codenvy.com>
2013-10-22 11:32:59 +0300

    CLDIDE-1161; factory storage switcher added;

--- codenvy-packaging-platform-api-war/src/main/java/com/codenvy/factory/CodenvyApiServletContextListener.java
+++ codenvy-packaging-platform-api-war/src/main/java/com/codenvy/factory/CodenvyApiServletContextListener.java
@@ -20,19 +20,29 @@ package com.codenvy.factory;
 
 import com.codenvy.api.factory.AdvancedFactoryUrlValidator;
 import com.codenvy.api.factory.FactoryStore;
+import com.codenvy.factory.storage.mongo.MongoDBFactoryStore;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.File;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *  Factory Servlet Context Listener class.
  */
 public class CodenvyApiServletContextListener implements ServletContextListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CodenvyApiServletContextListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext sctx = sce.getServletContext();
-        sctx.setAttribute(FactoryStore.class.getName(), new InMemoryFactoryStore());
+        sctx.setAttribute(FactoryStore.class.getName(), getFactoryStore());
         sctx.setAttribute(AdvancedFactoryUrlValidator.class.getName(), new AdvancedFactoryUrlFormat());
     }
 
@@ -41,5 +51,34 @@ public class CodenvyApiServletContextListener implements ServletContextListener 
         ServletContext sctx = sce.getServletContext();
         sctx.removeAttribute(FactoryStore.class.getName());
         sctx.removeAttribute(AdvancedFactoryUrlValidator.class.getName());
+    }
+
+
+    private FactoryStore getFactoryStore() {
+
+        File dbSettings =
+                new File(new File(System.getProperty("cloud.admin.configuration.dir")), "factory-storage.properties");
+        if (!dbSettings.exists() || dbSettings.isDirectory()) {
+            return new InMemoryFactoryStore();
+        } else {
+            Configuration configuration;
+            try {
+                configuration = new PropertiesConfiguration(dbSettings);
+                String host = configuration.getString("host");
+                int port = configuration.getInt("port");
+                String dbName = configuration.getString("database");
+                String collectionName = configuration.getString("collection");
+                if (host != null && !host.isEmpty() && port != 0 && dbName != null && !dbName.isEmpty() &&
+                    collectionName != null && !collectionName.isEmpty())
+                    return new MongoDBFactoryStore(host, port, dbName, collectionName);
+                else {
+                    LOG.warn("Error while parsing MongoDB configuration file. Default settings will be used.");
+                    return new MongoDBFactoryStore();
+                }
+            } catch (ConfigurationException e) {
+                LOG.warn("Error while reading MongoDB configuration file. Default settings will be used.");
+                return new MongoDBFactoryStore();
+            }
+        }
     }
 }
