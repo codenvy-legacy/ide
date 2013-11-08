575290cd9462b04673835daecaea1076b24d7289
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-11-08 10:08:42 +0200

    IDEX-177: add ability to use codenvy-dto-maven-plugin for client-side

---
+++ codenvy-ide-commons/src/main/java/com/codenvy/ide/dto/DtoFactory.java
@@ -0,0 +1,91 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.dto;
+
+import com.google.inject.Singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides implementations of all registered DTO interfaces.
+ *
+ * @author <a href="mailto:azatsarynnyy@codenvy.com">Artem Zatsarynnyy</a>
+ */
+@Singleton
+public class DtoFactory {
+    private final Map<Class<?>, DtoProvider<?>> dtoInterface2Providers = new HashMap<Class<?>, DtoProvider<?>>();
+
+    /**
+     * Creates new instance of class which implements specified DTO interface.
+     *
+     * @param dtoInterface
+     *         DTO interface
+     * @return new instance of DTO implementation
+     * @throws IllegalArgumentException
+     *         if can't provide any implementation for specified interface
+     */
+    public <T> T createDto(Class<T> dtoInterface) {
+        return getDtoProvider(dtoInterface).newInstance();
+    }
+
+    /**
+     * Creates new instance of class which implements specified DTO interface, parses specified JSON string and uses
+     * parsed data for initializing fields of DTO object.
+     *
+     * @param json
+     *         JSON data
+     * @param dtoInterface
+     *         DTO interface
+     * @return new instance of DTO implementation
+     * @throws IllegalArgumentException
+     *         if can't provide any implementation for specified interface
+     */
+    public <T> T createDtoFromJson(String json, Class<T> dtoInterface) {
+        return getDtoProvider(dtoInterface).fromJson(json);
+    }
+
+    /** Serializes dto to JSON format. */
+    public <T> String toJson(T dto) {
+        if (dto instanceof JsonSerializable) {
+            return ((JsonSerializable)dto).toJson();
+        }
+        throw new IllegalArgumentException("JsonSerializable instance required. ");
+    }
+
+    /**
+     * Registers DtoProvider for DTO interface.
+     *
+     * @param dtoInterface
+     *         DTO interface
+     * @param provider
+     *         provider for DTO interface
+     * @see DtoProvider
+     */
+    public void registerProvider(Class<?> dtoInterface, DtoProvider<?> provider) {
+        dtoInterface2Providers.put(dtoInterface, provider);
+    }
+
+    private <T> DtoProvider<T> getDtoProvider(Class<T> dtoInterface) {
+        DtoProvider<?> dtoProvider = dtoInterface2Providers.get(dtoInterface);
+        if (dtoProvider == null) {
+            throw new IllegalArgumentException("Unknown DTO type " + dtoInterface);
+        }
+        return (DtoProvider<T>)dtoProvider;
+    }
+}
---
+++ codenvy-ide-core/src/main/java/com/codenvy/util/DtoFactoryVisitorRegistryGenerator.java
@@ -0,0 +1,200 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.util;
+
+import com.codenvy.ide.dto.ClientDtoFactoryVisitor;
+
+import org.apache.commons.io.FileUtils;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Generates {DtoFactoryVisitorRegistry} class source.
+ *
+ * @author <a href="mailto:azatsarynnyy@codenvy.com">Artem Zatsarynnyy</a>
+ */
+public class DtoFactoryVisitorRegistryGenerator {
+
+    /**
+     * Path of the output class, it definitely should already exits. To ensure proper config.
+     * File content will be overridden.
+     */
+    protected static final String              REGISTRY_PATH      =
+            "com/codenvy/ide/client/DtoFactoryVisitorRegistry.java";
+    /** Map containing <FullFQN, ClassName> */
+    protected static final Map<String, String> dtoFactoryVisitors = new HashMap<String, String>();
+
+    /**
+     * Entry point. --rootDir is the optional parameter.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            String rootDirPath = ".";
+            // try to read argument
+            if (args.length == 1) {
+                if (args[0].startsWith(GeneratorUtils.ROOT_DIR_PARAMETER)) {
+                    rootDirPath = args[0].substring(GeneratorUtils.ROOT_DIR_PARAMETER.length());
+                } else {
+                    System.err.print("Wrong usage. There is only one allowed argument : "
+                                     + GeneratorUtils.ROOT_DIR_PARAMETER);
+                    System.exit(1);
+                }
+            }
+
+            File rootFolder = new File(rootDirPath);
+            System.out.println(" ------------------------------------------------------------------------ ");
+            System.out.println(String.format("Searching for DTO"));
+            System.out.println(" ------------------------------------------------------------------------ ");
+
+            // find all DtoFactoryVisitors
+            findDtoFactoryVisitors();
+            generateExtensionManager(rootFolder);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            // error
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Find all the Java classes that have proper @ClientDtoFactoryVisitor annotation.
+     *
+     * @throws java.io.IOException
+     */
+    @SuppressWarnings("unchecked")
+    private static void findDtoFactoryVisitors() throws IOException {
+        Reflections reflection = new Reflections(new TypeAnnotationsScanner());
+        Set<Class<?>> classes = reflection.getTypesAnnotatedWith(ClientDtoFactoryVisitor.class);
+        for (Class clazz : classes) {
+            dtoFactoryVisitors.put(clazz.getCanonicalName(), clazz.getSimpleName());
+            System.out.println(String.format("New DtoFactoryVisitor found: %s", clazz.getCanonicalName()));
+        }
+        System.out.println(String.format("Found: %d DtoFactoryVisitor(s)", dtoFactoryVisitors.size()));
+    }
+
+    /**
+     * Generate to source of the class.
+     *
+     * @param rootFolder
+     */
+    public static void generateExtensionManager(File rootFolder) throws IOException {
+        File outFile = new File(rootFolder, REGISTRY_PATH);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("package " + "com.codenvy.ide.client;\n\n");
+        generateImports(builder);
+        generateClass(builder);
+
+        // flush content
+        FileUtils.writeStringToFile(outFile, builder.toString());
+    }
+
+    /**
+     * Generate imports.
+     *
+     * @param builder
+     */
+    public static void generateImports(StringBuilder builder) {
+        builder.append("import com.google.inject.Inject;\n");
+        builder.append("import com.google.inject.Provider;\n");
+        builder.append("import com.google.inject.Singleton;\n");
+
+        builder.append("import com.codenvy.ide.json.JsonStringMap;\n");
+        builder.append("import com.codenvy.ide.json.JsonCollections;\n");
+    }
+
+    /**
+     * Generate class declarations.
+     *
+     * @param builder
+     */
+    public static void generateClass(StringBuilder builder) {
+        // generate class header
+        builder.append("/**\n");
+        builder.append(" * THIS CLASS WILL BE OVERRIDDEN BY MAVEN BUILD. DON'T EDIT CLASS, IT WILL HAVE NO EFFECT.\n");
+        builder.append(" */\n");
+        builder.append("@Singleton\n");
+        builder.append("@SuppressWarnings(\"rawtypes\")\n");
+        builder.append("public class DtoFactoryVisitorRegistry\n");
+        builder.append("{\n");
+        builder.append("\n");
+
+        // field
+        builder.append(GeneratorUtils.TAB
+                       +
+                       "/** Contains the map will all the DtoFactoryVisitor Providers <FullClassFQN, Provider>. */\n");
+        builder.append(GeneratorUtils.TAB
+                       + "protected final JsonStringMap<Provider> providers = JsonCollections.createStringMap();\n\n");
+
+        // generate constructor
+
+        builder.append(GeneratorUtils.TAB + "/** Constructor that accepts all found DtoFactoryVisitor Providers. */\n");
+        builder.append(GeneratorUtils.TAB + "@Inject\n");
+        builder.append(GeneratorUtils.TAB + "public DtoFactoryVisitorRegistry(\n");
+
+        // paste args here
+        Iterator<Entry<String, String>> entryIterator = dtoFactoryVisitors.entrySet().iterator();
+        while (entryIterator.hasNext()) {
+            // <FullFQN, ClassName>
+            Entry<String, String> entry = entryIterator.next();
+            String hasComma = entryIterator.hasNext() ? "," : "";
+            // add constructor argument like:
+            // fullFQN classNameToLowerCase,
+            String classFQN = String.format("Provider<%s>", entry.getKey());
+            String variableName = entry.getValue().toLowerCase();
+            builder.append(GeneratorUtils.TAB2 + classFQN + " " + variableName + hasComma + "\n");
+        }
+
+        builder.append(GeneratorUtils.TAB + ")\n");
+        builder.append(GeneratorUtils.TAB + "{\n");
+
+        // paste add here
+        for (Entry<String, String> extension : dtoFactoryVisitors.entrySet()) {
+            String fullFqn = extension.getKey();
+            String variableName = extension.getValue().toLowerCase();
+
+            String putStatement = String.format("this.providers.put(\"%s\",%s);%n", fullFqn, variableName);
+            builder.append(GeneratorUtils.TAB2 + putStatement);
+        }
+
+        // close constructor
+        builder.append(GeneratorUtils.TAB + "}\n\n");
+
+        // generate getter
+        builder.append(GeneratorUtils.TAB
+                       +
+                       "/** Returns  the map will all the DtoFactoryVisitor Providers <FullClassFQN, Provider>. */\n");
+        builder.append(GeneratorUtils.TAB + "public JsonStringMap<Provider> getDtoFactoryVisitors()\n");
+        builder.append(GeneratorUtils.TAB + "{\n");
+        builder.append(GeneratorUtils.TAB2 + "return providers;\n");
+        builder.append(GeneratorUtils.TAB + "}\n");
+
+        // close class
+        builder.append("}\n");
+    }
+}
