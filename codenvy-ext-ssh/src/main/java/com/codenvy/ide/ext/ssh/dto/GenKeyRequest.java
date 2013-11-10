69abc26c311b13463e22f76b1029600feac39a38
Ann Shumilova <ashumilova@codenvy.com>
2013-11-10 22:18:39 +0200

    IDEX-206: add ssh dto

---
+++ codenvy-ext-ssh/src/main/java/com/codenvy/ide/ext/ssh/dto/GenKeyRequest.java
@@ -0,0 +1,56 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.ext.ssh.dto;
+
+import com.codenvy.ide.dto.DTO;
+
+/**
+ * Interface describe a request for generate a SSH-key.
+ *
+ * @author <a href="mailto:azatsarynnyy@exoplatform.org">Artem Zatsarynnyy</a>
+ * @version $Id: GenKeyRequest.java Mar 21, 2012 6:14:27 PM azatsarynnyy $
+ */
+@DTO
+public interface GenKeyRequest {
+    /**
+     * Returns remote host name for which generate key.
+     *
+     * @return host name
+     */
+    String getHost();
+    
+    GenKeyRequest withHost(String host);
+
+    /**
+     * Returns comment for public key.
+     *
+     * @return comment
+     */
+    String getComment();
+
+    GenKeyRequest withComment(String comment);
+    
+    /**
+     * Returns passphrase for private key.
+     *
+     * @return passphrase
+     */
+    String getPassphrase();
+    
+    GenKeyRequest withPassphrase(String passPhrase);
+}
