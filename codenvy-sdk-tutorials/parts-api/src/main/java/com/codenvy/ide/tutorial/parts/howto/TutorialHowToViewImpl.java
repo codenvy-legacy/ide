31ef46c6fbc3e9b5865d72dd21fc6f8d479bd668
Andrey Plotnikov <aplotnikov@codenvy.com>
2013-11-11 17:44:25 +0200

    IDEX-195: Added parts api tutorial source code

---
+++ codenvy-sdk-tutorials/parts-api/src/main/java/com/codenvy/ide/tutorial/parts/howto/TutorialHowToViewImpl.java
@@ -0,0 +1,49 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ * 
+ * [2012] - [2013] Codenvy, S.A. 
+ * All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.tutorial.parts.howto;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * The implementation of {@link TutorialHowToView}.
+ *
+ * @author <a href="mailto:aplotnikov@codenvy.com">Andrey Plotnikov</a>
+ */
+@Singleton
+public class TutorialHowToViewImpl extends Composite implements TutorialHowToView {
+    interface TutorialHowToViewImplUiBinder extends UiBinder<Widget, TutorialHowToViewImpl> {
+    }
+
+    private static TutorialHowToViewImplUiBinder ourUiBinder = GWT.create(TutorialHowToViewImplUiBinder.class);
+
+    @Inject
+    public TutorialHowToViewImpl() {
+        initWidget(ourUiBinder.createAndBindUi(this));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setDelegate(ActionDelegate delegate) {
+        // do nothing
+    }
+}
