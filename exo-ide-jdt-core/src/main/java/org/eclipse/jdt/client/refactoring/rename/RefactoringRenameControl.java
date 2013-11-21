bddfec705d33c255da708639459ae1488809448a
Vitaliy Guliy <vguliy@codenvy.com>
2013-11-21 13:57:16 +0200

    CLDIDE-1186 fixed behavior of Refactoring-rename feature when Collaboration mode is turned on

--- exo-ide-jdt-core/src/main/java/org/eclipse/jdt/client/refactoring/rename/RefactoringRenameControl.java
+++ exo-ide-jdt-core/src/main/java/org/eclipse/jdt/client/refactoring/rename/RefactoringRenameControl.java
@@ -17,10 +17,24 @@
  */
 package org.eclipse.jdt.client.refactoring.rename;
 
+import com.google.collide.client.collaboration.CollaborationPropertiesUtil;
+
 import org.eclipse.jdt.client.JdtExtension;
 import org.eclipse.jdt.client.internal.corext.codemanipulation.JavaControl;
+import org.exoplatform.gwtframework.commons.rest.MimeType;
 import org.exoplatform.ide.client.framework.annotation.RolesAllowed;
+import org.exoplatform.ide.client.framework.editor.event.EditorActiveFileChangedEvent;
+import org.exoplatform.ide.client.framework.event.CollaborationChangedEvent;
+import org.exoplatform.ide.client.framework.event.CollaborationChangedHandler;
+import org.exoplatform.ide.client.framework.module.IDE;
+import org.exoplatform.ide.client.framework.project.ProjectClosedEvent;
+import org.exoplatform.ide.client.framework.project.ProjectClosedHandler;
+import org.exoplatform.ide.client.framework.project.ProjectOpenedEvent;
+import org.exoplatform.ide.client.framework.project.ProjectOpenedHandler;
+import org.exoplatform.ide.editor.client.api.Editor;
 import org.exoplatform.ide.editor.java.client.JavaClientBundle;
+import org.exoplatform.ide.vfs.client.model.FileModel;
+import org.exoplatform.ide.vfs.client.model.ProjectModel;
 
 /**
  * Control for rename refactoring.
@@ -29,8 +43,27 @@ import org.exoplatform.ide.editor.java.client.JavaClientBundle;
  * @version $Id: RefactoringRenameControl.java Jan 18, 2013 11:32:03 AM azatsarynnyy $
  */
 @RolesAllowed("developer")
-public class RefactoringRenameControl extends JavaControl {
+public class RefactoringRenameControl extends JavaControl
+    implements ProjectOpenedHandler, ProjectClosedHandler, CollaborationChangedHandler {
 
+    /**
+     * Opened project.
+     */
+    private ProjectModel project;
+    
+    /**
+     * Active file.
+     */
+    private FileModel activeFile;
+
+    /**
+     * Editor in which active is being edited.
+     */
+    private Editor activeEditor;    
+    
+    /**
+     * Creates a new instance of this {@link RefactoringRenameControl}
+     */
     public RefactoringRenameControl() {
         super(JdtExtension.LOCALIZATION_CONSTANT.refactoringRenameControlId());
         setTitle(JdtExtension.LOCALIZATION_CONSTANT.refactoringRenameControlTitle());
@@ -41,6 +74,78 @@ public class RefactoringRenameControl extends JavaControl {
 
         // TODO remove from context menu while bug with group name will fixed
         //setShowInContextMenu(true);
+    }
+    
+    /**
+     * @see org.eclipse.jdt.client.internal.corext.codemanipulation.JavaControl#initialize()
+     */
+    @Override
+    public void initialize() {
+        super.initialize();
+        
+        IDE.addHandler(ProjectOpenedEvent.TYPE, this);
+        IDE.addHandler(ProjectClosedEvent.TYPE, this);
+        IDE.addHandler(CollaborationChangedEvent.TYPE, this);
+    }
+
+    /**
+     * @see org.exoplatform.ide.client.framework.project.ProjectOpenedHandler#onProjectOpened(org.exoplatform.ide.client.framework.project.ProjectOpenedEvent)
+     */
+    @Override
+    public void onProjectOpened(ProjectOpenedEvent event) {
+        project = event.getProject();
+        update();
+    }
+
+    /**
+     * @see org.exoplatform.ide.client.framework.project.ProjectClosedHandler#onProjectClosed(org.exoplatform.ide.client.framework.project.ProjectClosedEvent)
+     */
+    @Override
+    public void onProjectClosed(ProjectClosedEvent event) {
+        project = null;
+        update();
+    }
+
+    /**
+     * @see org.exoplatform.ide.client.framework.event.CollaborationChangedHandler#onCollaborationChanged(org.exoplatform.ide.client.framework.event.CollaborationChangedEvent)
+     */
+    @Override
+    public void onCollaborationChanged(CollaborationChangedEvent event) {
+        update();
+    }
+    
+    /**
+     * Updates state of this command.
+     */
+    private void update() {
+        if (project == null || activeFile == null || activeEditor == null) {
+            setEnabled(false);
+            setVisible(false);
+            return;
+        }
+        
+        if (!activeFile.getMimeType().equals(MimeType.APPLICATION_JAVA)) {
+            setEnabled(false);
+            setVisible(false);
+            return;
+        }
+
+        setVisible(true);
+        if (CollaborationPropertiesUtil.isCollaborationEnabled(project)) {
+            setEnabled(false);
+        } else {            
+            setEnabled(true);
+        }
+    }
+        
+    /**
+     * @see org.eclipse.jdt.client.internal.corext.codemanipulation.JavaControl#onEditorActiveFileChanged(org.exoplatform.ide.client.framework.editor.event.EditorActiveFileChangedEvent)
+     */
+    @Override
+    public void onEditorActiveFileChanged(EditorActiveFileChangedEvent event) {
+        activeFile = event.getFile();
+        activeEditor = event.getEditor();
+        update();
     }
 
 }
