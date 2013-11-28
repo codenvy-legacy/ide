cd36a444c172ffbdf8114e409b6ef969027c2e93
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-11-28 17:59:04 +0200

    Cleanup

--- codenvy-ext-extensions/src/main/java/com/codenvy/ide/ext/extensions/client/ExtRuntimeResources.java
+++ codenvy-ext-extensions/src/main/java/com/codenvy/ide/ext/extensions/client/ExtRuntimeResources.java
@@ -60,9 +60,6 @@ public interface ExtRuntimeResources extends ClientBundle {
     @Source("stopApp.png")
     ImageResource stopApp();
 
-    @Source("buildBundle.png")
-    ImageResource buildBundle();
-
     @Source("codenvyExtensionTemplate.png")
     ImageResource codenvyExtensionTemplate();
 
