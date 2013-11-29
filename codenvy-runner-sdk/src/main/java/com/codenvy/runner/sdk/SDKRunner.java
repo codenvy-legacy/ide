955ded5cf7ef118257f2f61006bfd608497ddd30
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2013-11-29 10:46:05 +0200

    minor changes

--- codenvy-runner-sdk/src/main/java/com/codenvy/runner/sdk/SDKRunner.java
+++ codenvy-runner-sdk/src/main/java/com/codenvy/runner/sdk/SDKRunner.java
@@ -387,9 +387,9 @@ public class SDKRunner extends Runner {
             @Override
             public void getLogs(Appendable output) throws IOException {
                 for (File logFile : logFiles) {
-                    output.append("====> ").append(logFile.getName()).append(" <====\n");
+                    output.append("\n====> ").append(logFile.getName()).append(" <====\n");
                     CharStreams.copy(new InputStreamReader(new FileInputStream(logFile)), output);
-                    output.append("\n\n");
+                    output.append("\n");
                 }
             }
 
