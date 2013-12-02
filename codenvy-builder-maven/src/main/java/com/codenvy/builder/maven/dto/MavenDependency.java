e3c8747158707c810169f7cda6bd04a8d9e9fd14
Andrey Parfonov <aparfonov@codenvy.com>
2013-12-02 10:39:40 +0200

    Use Dependency DTO

---
+++ codenvy-builder-maven/src/main/java/com/codenvy/builder/maven/dto/MavenDependency.java
@@ -0,0 +1,66 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.builder.maven.dto;
+
+import com.codenvy.api.builder.dto.Dependency;
+import com.codenvy.dto.shared.DTO;
+
+/**
+ * Maven project dependency.
+ *
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ */
+@DTO
+public interface MavenDependency extends Dependency {
+    String getArtifactId();
+
+    MavenDependency withArtifactId(String artifactId);
+
+    void setArtifactId(String artifactId);
+
+    String getClassifier();
+
+    MavenDependency withClassifier(String classifier);
+
+    void setClassifier(String classifier);
+
+    String getGroupId();
+
+    MavenDependency withGroupId(String groupId);
+
+    void setGroupId(String groupId);
+
+    String getType();
+
+    MavenDependency withType(String type);
+
+    void setType(String type);
+
+    String getVersion();
+
+    MavenDependency withVersion(String version);
+
+    void setVersion(String version);
+
+    String getScope();
+
+    MavenDependency withScope(String scope);
+
+    void setScope(String scope);
+}
+
