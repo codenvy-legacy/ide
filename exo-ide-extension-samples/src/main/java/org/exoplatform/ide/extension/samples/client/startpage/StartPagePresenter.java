9f02c319886bf621e7d204513c2452f10b1965ad
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-01-17 18:15:09 +0200

    CLDIDE-1491: Display a Uservoice widget only to paid users

--- exo-ide-extension-samples/src/main/java/org/exoplatform/ide/extension/samples/client/startpage/StartPagePresenter.java
+++ exo-ide-extension-samples/src/main/java/org/exoplatform/ide/extension/samples/client/startpage/StartPagePresenter.java
@@ -21,25 +21,40 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
 
 import org.exoplatform.gwtframework.commons.exception.ExceptionThrownEvent;
+import org.exoplatform.gwtframework.commons.rest.AsyncRequest;
+import org.exoplatform.gwtframework.commons.rest.AsyncRequestCallback;
+import org.exoplatform.gwtframework.commons.rest.HTTPHeader;
+import org.exoplatform.gwtframework.commons.rest.MimeType;
 import org.exoplatform.ide.client.framework.event.CreateProjectEvent;
 import org.exoplatform.ide.client.framework.module.IDE;
 import org.exoplatform.ide.client.framework.ui.api.IsView;
 import org.exoplatform.ide.client.framework.ui.api.View;
 import org.exoplatform.ide.client.framework.ui.api.event.ViewClosedEvent;
 import org.exoplatform.ide.client.framework.ui.api.event.ViewClosedHandler;
+import org.exoplatform.ide.client.framework.util.StringUnmarshaller;
 import org.exoplatform.ide.extension.samples.client.inviting.google.InviteGoogleDevelopersEvent;
 import org.exoplatform.ide.git.client.clone.CloneRepositoryEvent;
 import org.exoplatform.ide.git.client.github.gitimport.ImportFromGithubEvent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Presenter for welcome view.
- * 
- * @author <a href="oksana.vereshchaka@gmail.com">Oksana Vereshchaka</a>
- * @version $Id: WelcomePresenter.java Aug 25, 2011 12:27:27 PM vereshchaka $
+ * @author Oksana Vereshchaka
  */
-public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandler {
+public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandler, PremiumAccountInfoReceivedHandler {
 
     public interface Display extends IsView {
 
@@ -50,18 +65,21 @@ public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandl
         HasClickHandlers getImportLink();
 
         HasClickHandlers getInvitationsLink();
-        
+
+        Anchor getSupportLink();
+
         void disableInvitationsLink();
 
     }
 
     private Display          display;
-
     private ReadOnlyUserView readOnlyUserView;
+    private boolean          premiumUser;
 
     public StartPagePresenter() {
         IDE.addHandler(OpenStartPageEvent.TYPE, this);
         IDE.addHandler(ViewClosedEvent.TYPE, this);
+        IDE.addHandler(PremiumAccountInfoReceivedEvent.TYPE, this);
     }
 
     private void bindDisplay() {
@@ -114,19 +132,19 @@ public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandl
             }
         });
         if (IDE.isRoUser() || IDE.currentWorkspace.isTemporary())
-          display.disableInvitationsLink();
+            display.disableInvitationsLink();
+
+        display.getSupportLink().setHref(premiumUser ? "javascript:UserVoice.showPopupWidget();"
+                                                     : "http://helpdesk.codenvy.com");
     }
 
-    /**
-     * @see org.exoplatform.ide.client.OpenStartPageHandler.OpenWelcomeHandler#onOpenStartPage(org.exoplatform.ide.client
-     *      .OpenStartPageEvent.OpenWelcomeEvent)
-     */
     @Override
-    public void onOpenStartPage(OpenStartPageEvent event) {
+    public void onPremiumAccountInfoReceived(PremiumAccountInfoReceivedEvent event) {
         if (display == null) {
             Display d = GWT.create(Display.class);
             IDE.getInstance().openView((View)d);
             display = d;
+            premiumUser = event.isUserHasPremiumAccount();
             bindDisplay();
             IDE.fireEvent(new WelcomePageOpenedEvent());
         } else {
@@ -134,14 +152,91 @@ public class StartPagePresenter implements OpenStartPageHandler, ViewClosedHandl
         }
     }
 
-    /**
-     * @see org.exoplatform.ide.client.framework.ui.api.event.ViewClosedHandler#onViewClosed(org.exoplatform.ide.client.framework.ui.api
-     *      .event.ViewClosedEvent)
-     */
+    /** {@inheritDoc} */
+    @Override
+    public void onOpenStartPage(OpenStartPageEvent event) {
+        getUserFromApi();
+    }
+
+    /** {@inheritDoc} */
     @Override
     public void onViewClosed(ViewClosedEvent event) {
         if (event.getView() instanceof Display) {
             display = null;
+        }
+    }
+
+    /** Call User API to fetch information about current loggined user. */
+    private void getUserFromApi() {
+        try {
+            AsyncRequestCallback<StringBuilder> callback =
+                    new AsyncRequestCallback<StringBuilder>(new StringUnmarshaller(new StringBuilder())) {
+                        @Override
+                        protected void onSuccess(StringBuilder json) {
+                            List<String> accountIds = new ArrayList<String>();
+                            JSONValue jsonUserProfile = JSONParser.parseStrict(json.toString());
+                            String currentUserId = jsonUserProfile.isObject().get("id").isString().stringValue();
+                            if (jsonUserProfile.isObject()!= null && jsonUserProfile.isObject().containsKey("accounts")) {
+                                JSONArray jsonAccounts = jsonUserProfile.isObject().get("accounts").isArray();
+                                for (int i = 0; i < jsonAccounts.size(); i++) {
+                                    JSONObject jsonAccount = jsonAccounts.get(i).isObject();
+                                    accountIds.add(jsonAccount.get("id").isString().stringValue());
+                                }
+
+                                accountInfo.clear();
+                                getAccountPremierProperty(currentUserId, accountIds);
+                            }
+                        }
+
+                        @Override
+                        protected void onFailure(Throwable exception) {
+                            IDE.fireEvent(new PremiumAccountInfoReceivedEvent(false));
+                        }
+                    };
+            AsyncRequest.build(RequestBuilder.GET, "/api/user").header(HTTPHeader.ACCEPT, MimeType.APPLICATION_JSON).send(callback);
+        } catch (RequestException e) {
+            IDE.fireEvent(new PremiumAccountInfoReceivedEvent(false));
+        }
+    }
+
+    private HashMap<String, Boolean> accountInfo = new HashMap<String, Boolean>();
+
+    /** Get information about all accounts which user managed. */
+    private void getAccountPremierProperty(final String currentUserId, final List<String> accountIds) {
+        if (accountIds.isEmpty()) {
+            IDE.fireEvent(new PremiumAccountInfoReceivedEvent(false));
+            return;
+        }
+
+        final String accountId = accountIds.get(0);
+        accountIds.remove(0);
+
+        try {
+            AsyncRequestCallback<StringBuilder> callback =
+                    new AsyncRequestCallback<StringBuilder>(new StringUnmarshaller(new StringBuilder())) {
+                        @Override
+                        protected void onSuccess(StringBuilder json) {
+                            JSONObject jsonAccount = JSONParser.parseStrict(json.toString()).isObject();
+                            String accountOwnerId = jsonAccount.get("owner").isObject().get("id").isString().stringValue();
+                            if (accountOwnerId.equals(currentUserId)) {
+                                JSONObject jsonAttributes = jsonAccount.get("attributes").isObject();
+                                if (jsonAttributes.containsKey("tariff_plan")) {
+                                    IDE.fireEvent(new PremiumAccountInfoReceivedEvent(true));
+                                    return;
+                                }
+                            }
+                            getAccountPremierProperty(currentUserId, accountIds);
+                        }
+
+                        @Override
+                        protected void onFailure(Throwable exception) {
+                            IDE.fireEvent(new PremiumAccountInfoReceivedEvent(false));
+                        }
+                    };
+            AsyncRequest.build(RequestBuilder.GET, "/api/account/" + accountId).header(HTTPHeader.ACCEPT, MimeType.APPLICATION_JSON)
+                        .send(callback);
+        } catch (RequestException e) {
+            IDE.fireEvent(new PremiumAccountInfoReceivedEvent(false));
         }
     }
 
