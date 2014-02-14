e0314739bb713b9dc0afca087b6700a0888b3d5b
Ann Shumilova <ashumilova@codenvy.com>
2014-02-14 10:20:35 +0200

    IDEX-232: add about dialog and Help menu

---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/BuildInfo.java
@@ -0,0 +1,36 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide;
+
+
+/**
+ * Represents application's build information.
+ * 
+ * @author Ann Shumilova
+ */
+public interface BuildInfo extends com.google.gwt.i18n.client.Messages {
+
+    @Key("revision")
+    String revision();
+
+    @Key("buildTime")
+    String buildTime();
+
+    @Key("version")
+    String version();
+}
---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/about/AboutLocalizationConstant.java
@@ -0,0 +1,43 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2013] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.about;
+
+import com.google.gwt.i18n.client.Messages;
+
+
+/**
+ * Localization for About Codenvy dialog.
+ * 
+ * @author Ann Shumilova
+ */
+public interface AboutLocalizationConstant extends Messages {
+    @Key("about.view.title")
+    String aboutViewTitle();
+
+    @Key("about.version")
+    String aboutVersion(String version);
+
+    @Key("about.revision")
+    String aboutRevision(String revision);
+
+    @Key("about.buildtime")
+    String aboutBuildTime(String time);
+
+    @Key("about.control.title")
+    String aboutControlTitle();
+}
