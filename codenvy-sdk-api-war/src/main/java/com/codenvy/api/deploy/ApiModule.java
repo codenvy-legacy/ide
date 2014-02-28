55cfe4e1a195b26f80faf6ecacf24536c8c65d1b
Andrey Parfonov <aparfonov@codenvy.com>
2014-02-28 14:07:17 +0200

    IDEX-365: fix runner for SDK

--- codenvy-sdk-api-war/src/main/java/com/codenvy/api/deploy/ApiModule.java
+++ codenvy-sdk-api-war/src/main/java/com/codenvy/api/deploy/ApiModule.java
@@ -17,15 +17,19 @@
  */
 package com.codenvy.api.deploy;
 
+import com.codenvy.api.builder.BuildQueue;
 import com.codenvy.api.builder.BuilderAdminService;
 import com.codenvy.api.builder.BuilderSelectionStrategy;
 import com.codenvy.api.builder.BuilderService;
 import com.codenvy.api.builder.LastInUseBuilderSelectionStrategy;
+import com.codenvy.api.builder.LocalBuildQueue;
 import com.codenvy.api.builder.internal.SlaveBuilderService;
 import com.codenvy.api.core.rest.ApiExceptionMapper;
 import com.codenvy.api.project.server.ProjectService;
 import com.codenvy.api.project.server.ProjectTypeDescriptionService;
 import com.codenvy.api.runner.LastInUseRunnerSelectionStrategy;
+import com.codenvy.api.runner.LocalRunQueue;
+import com.codenvy.api.runner.RunQueue;
 import com.codenvy.api.runner.RunnerAdminService;
 import com.codenvy.api.runner.RunnerSelectionStrategy;
 import com.codenvy.api.runner.RunnerService;
@@ -98,10 +102,12 @@ public class ApiModule extends AbstractModule {
         bind(VirtualFileSystemRuntimeExceptionMapper.class).toInstance(new VirtualFileSystemRuntimeExceptionMapper());
         bind(VirtualFileSystemFactory.class);
         bind(ApiExceptionMapper.class).toInstance(new ApiExceptionMapper());
+        bind(BuildQueue.class).to(LocalBuildQueue.class);
         bind(BuilderSelectionStrategy.class).toInstance(new LastInUseBuilderSelectionStrategy());
         bind(BuilderService.class);
         bind(BuilderAdminService.class);
         bind(SlaveBuilderService.class);
+        bind(RunQueue.class).to(LocalRunQueue.class);
         bind(RunnerSelectionStrategy.class).toInstance(new LastInUseRunnerSelectionStrategy());
         bind(RunnerService.class);
         bind(RunnerAdminService.class);
