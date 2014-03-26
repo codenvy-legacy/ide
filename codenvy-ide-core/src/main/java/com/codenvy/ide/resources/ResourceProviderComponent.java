f7668b8a05098d5b5ed1e06247fdd2a6c8e47b08
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2014-03-26 11:28:40 +0200

    Fix NPE

--- codenvy-ide-core/src/main/java/com/codenvy/ide/resources/ResourceProviderComponent.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/resources/ResourceProviderComponent.java
@@ -422,9 +422,10 @@ public class ResourceProviderComponent implements ResourceProvider, Component {
                 new AsyncRequestCallback<Array<ProjectReference>>(dtoUnmarshallerFactory.newArrayUnmarshaller(ProjectReference.class)) {
                     @Override
                     protected void onSuccess(Array<ProjectReference> result) {
-                        if (result.isEmpty())
-                            eventBus.fireEvent(ProjectActionEvent.createProjectOpenedEvent(null));//TODO
-                        
+                        if (result.isEmpty()) {
+                            eventBus.fireEvent(ProjectActionEvent.createProjectClosedEvent(null));
+                        }
+
                         for (ProjectReference item : result.asIterable()) {
                             Project project = new Project(eventBus, asyncRequestFactory, projectServiceClient, dtoUnmarshallerFactory);
                             project.setName(item.getName());
