9da19fbd68f5f54d668a60f886845c165f1b2edf
Gennady Azarenkov <gazarenkov@codenvy.com>
2014-04-01 10:39:13 +0300

    fix local infra

--- codenvy-ide-env-dummy/src/main/java/com/codenvy/ide/env/SingleEnvironmentFilter.java
+++ codenvy-ide-env-dummy/src/main/java/com/codenvy/ide/env/SingleEnvironmentFilter.java
@@ -1,5 +1,6 @@
 package com.codenvy.ide.env;
 
+import com.codenvy.api.local.Constants;
 import com.codenvy.commons.env.EnvironmentContext;
 import com.codenvy.commons.user.User;
 import com.codenvy.commons.user.UserImpl;
@@ -14,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Set up environment variable. Only for local packaging with single workspace. Don't use it in production packaging.
@@ -22,20 +25,24 @@ import java.util.Arrays;
  * @author andrew00x
  */
 public class SingleEnvironmentFilter implements Filter {
-    private String wsName;
-    private String wsId;
+//    private String wsName;
+//    private String wsId;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        wsName = filterConfig.getInitParameter("ws-name");
-        wsId = filterConfig.getInitParameter("ws-id");
+
+//        wsName = Constants.WORKSPACE.getName();
+//        wsId = Constants.WORKSPACE.getId();
+
+//        wsName = filterConfig.getInitParameter("ws-name");
+//        wsId = filterConfig.getInitParameter("ws-id");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final EnvironmentContext env = EnvironmentContext.getCurrent();
-        env.setWorkspaceName(wsName);
-        env.setWorkspaceId(wsId);
+        env.setWorkspaceName(Constants.WORKSPACE.getName());
+        env.setWorkspaceId(Constants.WORKSPACE.getId());
         final User user = getUser((HttpServletRequest)request);
         env.setUser(user);
         try {
@@ -70,6 +77,13 @@ public class SingleEnvironmentFilter implements Filter {
     }
 
     protected User getUser(HttpServletRequest httpRequest) {
-        return new UserImpl("ide", "dummy_token", Arrays.asList("workspace/developer", "workspace/developer", "system/admin", "system/manager", "user"));
+
+        List roles = new ArrayList();
+        roles.addAll(Arrays.asList("system/admin", "system/manager", "user"));
+        roles.addAll(Constants.MEMBER.getRoles());
+
+        return new UserImpl(Constants.USER.getEmail(), Constants.USER.getId(), Constants.TOKEN.getValue(), roles);
+
+        //return new UserImpl("ide", "dummy_token", Arrays.asList("workspace/developer", "workspace/developer", "system/admin", "system/manager", "user"));
     }
 }
