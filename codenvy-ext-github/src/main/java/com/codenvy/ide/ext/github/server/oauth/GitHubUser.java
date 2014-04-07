317cc9156baa3c665ac9dab31e6e8cbde9ce335a
Vitaly Parfonov <vparfonov@codenvy.com>
2014-04-07 18:26:01 +0300

    IDEX-443

--- codenvy-ext-github/src/main/java/com/codenvy/ide/ext/github/server/oauth/GitHubUser.java
+++ codenvy-ext-github/src/main/java/com/codenvy/ide/ext/github/server/oauth/GitHubUser.java
@@ -15,7 +15,7 @@
  * is strictly forbidden unless prior written permission is obtained
  * from Codenvy S.A..
  */
-package com.codenvy.ide.security.oauth.server;
+package com.codenvy.ide.ext.github.server.oauth;
 
 
 import com.codenvy.ide.security.oauth.shared.User;
