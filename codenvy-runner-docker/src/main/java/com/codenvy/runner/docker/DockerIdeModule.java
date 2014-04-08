d983985a414797dbbca1db05b675625ac3109e28
Andrey Parfonov <aparfonov@codenvy.com>
2014-04-08 16:39:39 +0300

    split 'docker' components in two modules

--- codenvy-runner-docker/src/main/java/com/codenvy/runner/docker/DockerIdeModule.java
+++ codenvy-runner-docker/src/main/java/com/codenvy/runner/docker/DockerIdeModule.java
@@ -17,19 +17,20 @@
  */
 package com.codenvy.runner.docker;
 
-import com.codenvy.api.runner.internal.Runner;
+import com.codenvy.api.project.server.ValueProviderFactory;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 
 /**
- * Docker runner deployer.
+ * Deploys components that provides project attributes required by the 'docker' based runners.
  *
  * @author andrew00x
  */
-public class DockerRunnerModule extends AbstractModule {
+public class DockerIdeModule extends AbstractModule {
     @Override
     protected void configure() {
-        Multibinder.newSetBinder(binder(), Runner.class).addBinding().to(DockerRunner.class);
-        bind(EmbeddedDockerRunnerRegistryPlugin.class).asEagerSingleton();
+        final Multibinder<ValueProviderFactory> vfMultibinder = Multibinder.newSetBinder(binder(), ValueProviderFactory.class);
+        vfMultibinder.addBinding().to(RunnerScriptValueProviderFactory.class);
+        vfMultibinder.addBinding().to(RunnerNameValueProviderFactory.class);
     }
 }
