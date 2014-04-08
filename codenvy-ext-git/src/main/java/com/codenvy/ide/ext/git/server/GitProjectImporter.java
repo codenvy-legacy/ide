8685260242bf76324f03f00dd98943daa50ff2b4
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-04-08 17:07:35 +0300

    IDEX-479: Need to add possibility to clone project into folder which already contains files.

--- codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/server/GitProjectImporter.java
+++ codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/server/GitProjectImporter.java
@@ -1,14 +1,16 @@
 package com.codenvy.ide.ext.git.server;
 
-import com.codenvy.api.project.server.AbstractVirtualFileEntry;
 import com.codenvy.api.project.server.FolderEntry;
 import com.codenvy.api.project.server.ProjectImporter;
 import com.codenvy.api.vfs.server.exceptions.VirtualFileSystemException;
-import com.codenvy.commons.lang.NameGenerator;
 import com.codenvy.dto.server.DtoFactory;
 import com.codenvy.ide.Constants;
 import com.codenvy.ide.ext.git.server.nativegit.NativeGitConnectionFactory;
+import com.codenvy.ide.ext.git.shared.BranchCheckoutRequest;
 import com.codenvy.ide.ext.git.shared.CloneRequest;
+import com.codenvy.ide.ext.git.shared.FetchRequest;
+import com.codenvy.ide.ext.git.shared.InitRequest;
+import com.codenvy.ide.ext.git.shared.RemoteAddRequest;
 import com.codenvy.vfs.impl.fs.LocalPathResolver;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -16,6 +18,7 @@ import com.google.inject.Singleton;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collections;
 
 /**
  * @author Vladyslav Zhukovskii
@@ -25,6 +28,8 @@ public class GitProjectImporter implements ProjectImporter {
 
     private final NativeGitConnectionFactory nativeGitConnectionFactory;
     private final LocalPathResolver          localPathResolver;
+
+    private static final String DEFAULT_REMOTE = "origin";
 
     @Inject
     public GitProjectImporter(NativeGitConnectionFactory nativeGitConnectionFactory, LocalPathResolver localPathResolver) {
@@ -44,26 +49,27 @@ public class GitProjectImporter implements ProjectImporter {
                 throw new IOException("Project cannot be imported into \"" + baseFolder.getName() + "\". It is not a folder.");
             }
 
-            FolderEntry tempFolderToClone = null;
+            String fullPathToClonedProject = localPathResolver.resolve(baseFolder.getVirtualFile());
+            GitConnection gitConnection = nativeGitConnectionFactory.getConnection(fullPathToClonedProject);
 
             if (!isFolderEmpty(baseFolder)) {
-                tempFolderToClone = baseFolder.createFolder(NameGenerator.generate("temp-", 15));
-            }
-
-            String fullPathToClonedProject = localPathResolver.resolve(tempFolderToClone != null ? tempFolderToClone.getVirtualFile()
-                                                                                                 : baseFolder.getVirtualFile());
-
-            GitConnection gitConnection = nativeGitConnectionFactory.getConnection(fullPathToClonedProject);
-            gitConnection.clone(DtoFactory.getInstance().createDto(CloneRequest.class)
-                                          .withWorkingDir(fullPathToClonedProject)
-                                          .withRemoteName("origin")
-                                          .withRemoteUri(location));
-
-            if (tempFolderToClone != null) {
-                for (AbstractVirtualFileEntry virtualFileEntry : tempFolderToClone.getChildren()) {
-                    virtualFileEntry.moveTo(baseFolder.getPath());
-                }
-                tempFolderToClone.remove();
+                gitConnection = gitConnection.init(DtoFactory.getInstance().createDto(InitRequest.class)
+                                                             .withWorkingDir(fullPathToClonedProject)
+                                                             .withInitCommit(false)
+                                                             .withBare(false));
+                gitConnection.remoteAdd(DtoFactory.getInstance().createDto(RemoteAddRequest.class)
+                                                  .withName(DEFAULT_REMOTE)
+                                                  .withUrl(location));
+                gitConnection.fetch(DtoFactory.getInstance().createDto(FetchRequest.class)
+                                              .withRemote(DEFAULT_REMOTE)
+                                              .withRefSpec(Collections.singletonList("refs/heads/master:refs/remotes/origin/master")));
+                gitConnection.branchCheckout(DtoFactory.getInstance().createDto(BranchCheckoutRequest.class)
+                                                       .withName("master"));
+            } else {
+                gitConnection.clone(DtoFactory.getInstance().createDto(CloneRequest.class)
+                                              .withWorkingDir(fullPathToClonedProject)
+                                              .withRemoteName(DEFAULT_REMOTE)
+                                              .withRemoteUri(location));
             }
 
             if (!baseFolder.isProjectFolder()) {
