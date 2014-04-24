765c63f126c277b7b658e5c4c3abe2fa013f0f2a
Evgen Vidolob <evidolob@codenvy.com>
2014-04-24 11:43:40 +0300

    IDEX-590 create base classes for find action action

---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/actions/FindActionAction.java
@@ -0,0 +1,41 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.actions;
+
+import com.codenvy.ide.api.ui.action.Action;
+import com.codenvy.ide.api.ui.action.ActionEvent;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * Action fo find action action
+ * @author Evgen Vidolob
+ */
+@Singleton
+public class FindActionAction extends Action {
+
+    @Inject
+    public FindActionAction() {
+        super("Find Action","Find Action by name", null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/actions/find/FindActionViewImpl.java
@@ -0,0 +1,49 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2012] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.actions.find;
+
+import com.codenvy.ide.ui.window.Window;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+/**
+ * @author Evgen Vidolob
+ */
+public class FindActionViewImpl extends Window implements FindActionView {
+    private static FindActionViewImplUiBinder ourUiBinder = GWT.create(FindActionViewImplUiBinder.class);
+
+    public FindActionViewImpl() {
+        HTMLPanel rootElement = ourUiBinder.createAndBindUi(this);
+
+    }
+
+    @Override
+    public void setDelegate(ActionDelegate delegate) {
+
+    }
+
+    @Override
+    protected void onClose() {
+
+    }
+
+    interface FindActionViewImplUiBinder
+            extends UiBinder<HTMLPanel, FindActionViewImpl> {
+    }
+}
