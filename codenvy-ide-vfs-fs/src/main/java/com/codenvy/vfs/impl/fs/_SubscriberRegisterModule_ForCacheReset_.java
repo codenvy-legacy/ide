ec3d9d8f33105bbf92bc05642fe944d87b6de12d
Sergey Leschenko <sleschenko@codenvy.com>
2014-04-29 11:24:07 +0300

    Updated java docs

--- codenvy-ide-vfs-fs/src/main/java/com/codenvy/vfs/impl/fs/_SubscriberRegisterModule_ForCacheReset_.java
+++ codenvy-ide-vfs-fs/src/main/java/com/codenvy/vfs/impl/fs/_SubscriberRegisterModule_ForCacheReset_.java
@@ -1,3 +1,20 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
 package com.codenvy.vfs.impl.fs;
 
 import com.codenvy.api.core.notification.EventSubscriber;
@@ -14,6 +31,9 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.file.Files;
 
+/**
+ * @author Sergii Leschenko
+ */
 @DynaModule
 public class _SubscriberRegisterModule_ForCacheReset_ extends AbstractModule {
     private static final Logger LOG = LoggerFactory.getLogger(_IdeOldCacheUpdater.class);
--- codenvy-ide-vfs-fs/src/main/java/com/codenvy/vfs/impl/fs/_SubscriberRegister_ForCacheReset_.java
+++ codenvy-ide-vfs-fs/src/main/java/com/codenvy/vfs/impl/fs/_SubscriberRegister_ForCacheReset_.java
@@ -1,3 +1,20 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
 package com.codenvy.vfs.impl.fs;
 
 import com.codenvy.api.core.notification.EventService;
@@ -9,6 +26,9 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Set;
 
+/**
+ * @author Sergii Leschenko
+ */
 @Singleton
 public class _SubscriberRegister_ForCacheReset_ {
     private final EventService eventService;
