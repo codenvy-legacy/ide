498f1fcd5144755f18a4762d8543b29e588a5868
Roman Nikitenko <rnikitenko@codenvy.com>
2014-05-14 16:30:09 +0300

    IDEX-644. Fixed bug related with incorrect work submenu 'Window/Preferences/SSH Keys'. Created info-window.

---
+++ codenvy-ide-ui/src/main/java/com/codenvy/ide/ui/dialogs/info/InfoHandler.java
@@ -0,0 +1,31 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ * 
+ * [2012] - [$today.year] Codenvy, S.A. 
+ * All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.ide.ui.dialogs.info;
+
+/**
+ * Handler for user interaction in Info dialog window
+ *
+ * @author Roman Nikitenko
+ */
+public abstract class InfoHandler {
+
+    /**
+     * Call if user click Ok button
+     */
+    public abstract void onOk();
+}
