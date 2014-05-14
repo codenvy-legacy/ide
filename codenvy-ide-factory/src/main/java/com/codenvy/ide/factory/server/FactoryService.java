6ade4f880faf5cdeb4a9545a46875317cdd26e72
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-05-14 09:55:45 +0300

    Fix with handle Unauthorized exception

--- codenvy-ide-factory/src/main/java/com/codenvy/ide/factory/server/FactoryService.java
+++ codenvy-ide-factory/src/main/java/com/codenvy/ide/factory/server/FactoryService.java
@@ -1,6 +1,7 @@
 package com.codenvy.ide.factory.server;
 
 import com.codenvy.api.core.ApiException;
+import com.codenvy.api.core.UnauthorizedException;
 import com.codenvy.api.core.rest.Service;
 import com.codenvy.api.core.rest.shared.dto.ServiceError;
 import com.codenvy.api.factory.dto.Factory;
@@ -17,7 +18,6 @@ import com.codenvy.commons.lang.Strings;
 import com.codenvy.dto.server.DtoFactory;
 import com.codenvy.ide.ext.git.server.GitConnection;
 import com.codenvy.ide.ext.git.server.GitException;
-import com.codenvy.ide.ext.git.server.NotAuthorizedException;
 import com.codenvy.ide.ext.git.server.nativegit.NativeGitConnectionFactory;
 import com.codenvy.ide.ext.git.shared.BranchCheckoutRequest;
 import com.codenvy.ide.factory.server.variable.VariableReplacer;
@@ -101,7 +101,7 @@ public class FactoryService extends Service {
         try {
             importer.importSources(projectFolder, factory.getVcsurl());
         } catch (IOException | ApiException e) {
-            if (e.getCause() != null && e.getCause() instanceof NotAuthorizedException) {
+            if (e instanceof UnauthorizedException) {
                 throw halt(UNAUTHORIZED, e.getMessage(), e);
             }
             throw halt(INTERNAL_SERVER_ERROR, e.getMessage());
