8897451a0f28c967fc8767b7de4aa56e974b26ca
Max Shaposhnik <mshaposhnik@codenvy.com>
2014-05-20 17:51:03 +0300

    CLDIDE-1749; Factory validator interface changes;

--- codenvy-ide-factory/src/main/java/com/codenvy/ide/factory/server/DummyFactoryValidator.java
+++ codenvy-ide-factory/src/main/java/com/codenvy/ide/factory/server/DummyFactoryValidator.java
@@ -4,19 +4,11 @@ import com.codenvy.api.factory.FactoryUrlException;
 import com.codenvy.api.factory.FactoryUrlValidator;
 import com.codenvy.api.factory.dto.Factory;
 
-import javax.servlet.http.HttpServletRequest;
-import java.net.URI;
-
 /**
  * @author Vladyslav Zhukovskii
  */
 public class DummyFactoryValidator implements FactoryUrlValidator {
     @Override
-    public Factory validate(URI uri, HttpServletRequest httpServletRequest) throws FactoryUrlException {
-        return null;
-    }
-
-    @Override
-    public void validate(Factory factory, boolean b, HttpServletRequest httpServletRequest) throws FactoryUrlException {
+    public void validate(Factory factory, boolean b) throws FactoryUrlException {
     }
 }
