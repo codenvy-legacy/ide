fa8f14ff6a46c9db808a029d398967c41d6c38aa
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2014-06-05 11:02:55 +0300

    IDEX-722: Add button to runner console for showing information about URL and credentials to runner instance

--- codenvy-ext-runner/src/main/java/com/codenvy/ide/extension/runner/client/run/RunnerController.java
+++ codenvy-ext-runner/src/main/java/com/codenvy/ide/extension/runner/client/run/RunnerController.java
@@ -433,6 +433,19 @@ public class RunnerController implements Notification.OpenNotificationHandler {
         return null;
     }
 
+    /** Returns URL of the application which is currently running. */
+    @Nullable
+    public String getCurrentAppShellURL() {
+        // don't show shell URL in console when app is stopped. After some time this URL may be used by other app.
+        if (lastApplicationDescriptor != null && getCurrentAppStopTime() == null) {
+            Link link = getLink(lastApplicationDescriptor, "shell url");
+            if (link != null) {
+                return link.getHref();
+            }
+        }
+        return null;
+    }
+
     /** Returns startTime {@link RunnerMetric}. */
     @Nullable
     public RunnerMetric getCurrentAppStartTime() {
