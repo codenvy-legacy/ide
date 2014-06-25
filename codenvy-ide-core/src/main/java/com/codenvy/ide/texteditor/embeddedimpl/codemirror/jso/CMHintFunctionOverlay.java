6a35d961df53992728a4cf33e850f035eadad613
Mickaël Leduque <mleduque@gmail.com>
2014-06-25 09:09:31 +0200

    Add JSO for CodeMirror editor

---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/texteditor/embeddedimpl/codemirror/jso/CMHintFunctionOverlay.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.ide.texteditor.embeddedimpl.codemirror.jso;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class CMHintFunctionOverlay extends JavaScriptObject {
+
+    protected CMHintFunctionOverlay() {
+    }
+
+    public static final native CMHintFunctionOverlay createHintFunction(String funcName) /*-{
+        
+    }-*/;
+}
