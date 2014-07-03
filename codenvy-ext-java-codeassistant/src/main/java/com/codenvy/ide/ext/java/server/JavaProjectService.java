cc8e18fad8e21b7f950197266f2ded0236376419
Evgen Vidolob <evidolob@codenvy.com>
2014-07-03 14:29:41 +0300

    IDEX-723 remove commented code

--- codenvy-ext-java-codeassistant/src/main/java/com/codenvy/ide/ext/java/server/JavaProjectService.java
+++ codenvy-ext-java-codeassistant/src/main/java/com/codenvy/ide/ext/java/server/JavaProjectService.java
@@ -78,37 +78,6 @@ public class JavaProjectService {
         options.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
         options.put(CompilerOptions.OPTION_Process_Annotations, JavaCore.DISABLED);
     }
-//
-//    public static void removeRecursive(Path path) throws IOException {
-//        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-//            @Override
-//            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-//                    throws IOException {
-//                Files.delete(file);
-//                return FileVisitResult.CONTINUE;
-//            }
-//
-//            @Override
-//            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-//                // try to delete the file anyway, even if its attributes
-//                // could not be read, since delete-only access is
-//                // theoretically possible
-//                Files.delete(file);
-//                return FileVisitResult.CONTINUE;
-//            }
-//
-//            @Override
-//            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-//                if (exc == null) {
-//                    Files.delete(dir);
-//                    return FileVisitResult.CONTINUE;
-//                } else {
-//                    // directory iteration failed; propagate exception
-//                    throw exc;
-//                }
-//            }
-//        });
-//    }
 
     public JavaProject getOrCreateJavaProject(String wsId, String projectPath) {
         String key = wsId + projectPath;
