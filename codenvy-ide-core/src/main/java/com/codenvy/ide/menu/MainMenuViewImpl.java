0ae795732b0c4f112760d94954c9403251d25a45
Vitaly Parfonov <vparfonov@codenvy.com>
2014-07-09 12:49:12 +0300

    Fix top menu visability

--- codenvy-ide-core/src/main/java/com/codenvy/ide/menu/MainMenuViewImpl.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/menu/MainMenuViewImpl.java
@@ -131,7 +131,7 @@ public class MainMenuViewImpl extends Composite implements MainMenuView, CloseMe
         newMenuVisibleActions.clear();
         expandActionGroup(IdeActions.GROUP_MAIN_MENU, newMenuVisibleActions, actionManager);
 
-        if (!newMenuVisibleActions.equals(menuVisibleActions)) {
+//        if (!newMenuVisibleActions.equals(menuVisibleActions)) {
             final List<Action> temp = menuVisibleActions;
             menuVisibleActions = newMenuVisibleActions;
             newMenuVisibleActions = temp;
@@ -140,13 +140,13 @@ public class MainMenuViewImpl extends Composite implements MainMenuView, CloseMe
             for (final Action action : menuVisibleActions) {
                 add(ActionPlaces.MAIN_MENU, action, presentationFactory);
             }
-        }
+//        }
         newRightVisibleActions.clear();
         expandActionGroup(IdeActions.GROUP_RIGHT_MAIN_MENU, newRightVisibleActions, actionManager);
         if (!newRightVisibleActions.equals(rightVisibleActions)) {
-            final List<Action> temp = rightVisibleActions;
+            final List<Action> temp1 = rightVisibleActions;
             rightVisibleActions = newRightVisibleActions;
-            newRightVisibleActions = temp;
+            newRightVisibleActions = temp1;
 
             removeRights();
 
