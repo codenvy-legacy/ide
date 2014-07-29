3104c62a0c2d55ec8a615e23f34175d61af50502
Vitaly Parfonov <vparfonov@codenvy.com>
2014-07-29 10:40:16 +0300

    Fix prompt in oAuth ask window

--- codenvy-ide-core/src/main/java/com/codenvy/ide/Constants.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/Constants.java
@@ -22,7 +22,7 @@ public interface Constants {
     final String NAMELESS_NAME                = "nameless";
     final String BLANK_PROJECT_TYPE           = "Blank Type";
     final String CODENVY_PLUGIN_NAME          = "Codenvy Extension";
-    final String CODENVY_CATEGORY             = "Codenvy Extension";
+    final String CODENVY_CATEGORY = "Codenvy";
     // project attribute names
     final String LANGUAGE                     = "language";
     final String LANGUAGE_VERSION             = "language.version";
