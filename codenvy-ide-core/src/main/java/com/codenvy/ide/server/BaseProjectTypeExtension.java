af450dd80b67e1c93242e5825189d70fe8d1655b
Evgen Vidolob <evidolob@codenvy.com>
2014-08-11 12:13:02 +0300

    IDEX-1006 implement getIconRegistry method

--- codenvy-ide-core/src/main/java/com/codenvy/ide/server/BaseProjectTypeExtension.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/server/BaseProjectTypeExtension.java
@@ -21,6 +21,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /** @author Vitalii Parfonov */
 
@@ -51,5 +52,10 @@ public class BaseProjectTypeExtension implements ProjectTypeExtension {
     @Override
     public List<ProjectTemplateDescription> getTemplates() {
         return Collections.emptyList();
+    }
+
+    @Override
+    public Map<String, String> getIconRegistry() {
+        return null;
     }
 }
