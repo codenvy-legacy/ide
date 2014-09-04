dbbcd0c032cf2ae44194af112bb3f821417fa169
Oleksii Orel <oorel@codenvy.com>
2014-09-04 15:05:20 +0300

    IDEX-1032 Add a placeholder for name/description of the project to create

--- codenvy-ide-core/src/main/java/com/codenvy/ide/wizard/project/main/MainPageViewImpl.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/wizard/project/main/MainPageViewImpl.java
@@ -122,9 +122,9 @@ public class MainPageViewImpl implements MainPageView {
         this.iconRegistry = iconRegistry;
         rootElement = ourUiBinder.createAndBindUi(this);
         reset();
-        projectName.getElement().setAttribute("title", "Define the name of your project...");
+        projectName.getElement().setAttribute("placeholder", "Define the name of your project...");
         projectName.getElement().setAttribute("maxlength", "32");
-        projectDescription.getElement().setAttribute("title", "Add a description to your project...");
+        projectDescription.getElement().setAttribute("placeholder", "Add a description to your project...");
         projectDescription.getElement().setAttribute("maxlength", "256");
     }
 
