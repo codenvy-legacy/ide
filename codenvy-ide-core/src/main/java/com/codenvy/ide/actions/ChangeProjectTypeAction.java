34cf6afea395373c24bc00d8bd3893a753e185ff
Oleksii Orel <oorel@codenvy.com>
2014-09-09 14:50:30 +0300

    IDEUI-176 rename Change Project Type

--- codenvy-ide-core/src/main/java/com/codenvy/ide/actions/ChangeProjectTypeAction.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/actions/ChangeProjectTypeAction.java
@@ -32,7 +32,7 @@ public class ChangeProjectTypeAction extends Action {
 
     @Inject
     public ChangeProjectTypeAction(AppContext appContext, NewProjectWizardPresenter wizardPresenter) {
-        super("Change Project Type", "Change project type", null, null);
+        super("Project Configuration", "Change project type", null, null);
         this.appContext = appContext;
         this.wizardPresenter = wizardPresenter;
     }
