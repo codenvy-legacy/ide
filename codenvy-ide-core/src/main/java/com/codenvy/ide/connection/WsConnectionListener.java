d2f4bc9006ea390c945be8421d0b2f8735c25c6a
Evgen Vidolob <evidolob@codenvy.com>
2014-09-22 15:56:15 +0300

    IDEX-873 add listener and event to notify app that http session was destroyed

---
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/connection/WsConnectionListener.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+
+package com.codenvy.ide.connection;
+
+import com.codenvy.ide.api.event.HttpSessionDestroyedEvent;
+import com.codenvy.ide.websocket.MessageBus;
+import com.codenvy.ide.websocket.events.ConnectionClosedHandler;
+import com.codenvy.ide.websocket.events.WebSocketClosedEvent;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+
+/**
+ * @author Evgen Vidolob
+ */
+public class WsConnectionListener implements ConnectionClosedHandler {
+
+
+    private EventBus eventBus;
+
+    @Inject
+    public WsConnectionListener(MessageBus messageBus, EventBus eventBus) {
+        this.eventBus = eventBus;
+        messageBus.addOnCloseHandler(this);
+    }
+
+    @Override
+    public void onClose(WebSocketClosedEvent event) {
+        if(event.getCode() == WebSocketClosedEvent.CLOSE_NORMAL && "Http session destroyed".equals(event.getReason())){
+            eventBus.fireEvent(new HttpSessionDestroyedEvent());
+        }
+    }
+
+}
