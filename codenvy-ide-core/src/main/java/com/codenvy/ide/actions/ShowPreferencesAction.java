760bdd5ef3861d497a56dc95fd968b3c1ec3d4ab
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2014-11-24 18:24:19 +0200

    merge with master

--- codenvy-ide-core/src/main/java/com/codenvy/ide/actions/ShowPreferencesAction.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/actions/ShowPreferencesAction.java
@@ -14,27 +14,26 @@ import com.codenvy.api.analytics.logger.AnalyticsEventLogger;
 import com.codenvy.ide.Resources;
 import com.codenvy.ide.api.action.Action;
 import com.codenvy.ide.api.action.ActionEvent;
+import com.codenvy.ide.api.app.AppContext;
 import com.codenvy.ide.preferences.PreferencesPresenter;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-/**
- * @author <a href="mailto:evidolob@codenvy.com">Evgen Vidolob</a>
- * @version $Id:
- */
+/** @author Evgen Vidolob */
 @Singleton
 public class ShowPreferencesAction extends Action {
 
     private final PreferencesPresenter presenter;
     private final AnalyticsEventLogger eventLogger;
+    private final AppContext           appContext;
 
     @Inject
     public ShowPreferencesAction(Resources resources, PreferencesPresenter presenter,
-                                 AnalyticsEventLogger eventLogger) {
-        //TODO change icon
+                                 AnalyticsEventLogger eventLogger, AppContext appContext) {
         super("Preferences", "Preferences", null, resources.preferences());
         this.presenter = presenter;
         this.eventLogger = eventLogger;
+        this.appContext = appContext;
     }
 
     /** {@inheritDoc} */
@@ -42,5 +41,16 @@ public class ShowPreferencesAction extends Action {
     public void actionPerformed(ActionEvent e) {
         eventLogger.log(this);
         presenter.showPreferences();
+    }
+
+    @Override
+    public void update(ActionEvent e) {
+        e.getPresentation().setVisible(true);
+        if ((appContext.getCurrentProject() == null && !appContext.getCurrentUser().isUserPermanent()) ||
+            (appContext.getCurrentProject() != null && appContext.getCurrentProject().isReadOnly())) {
+            e.getPresentation().setEnabled(false);
+        } else {
+            e.getPresentation().setEnabled(true);
+        }
     }
 }
