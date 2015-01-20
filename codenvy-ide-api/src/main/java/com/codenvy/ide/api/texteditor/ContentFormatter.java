ac56b1c2e2b62037e8fa52f266011851cb732d7a
Mickaël Leduque <mickael.leduque@serli.com>
2015-01-20 12:45:53 +0100

    PLGEDT-52 Remove definition of ContentFormatter

--- codenvy-ide-api/src/main/java/com/codenvy/ide/api/texteditor/ContentFormatter.java
+++ codenvy-ide-api/src/main/java/com/codenvy/ide/api/texteditor/ContentFormatter.java
@@ -10,34 +10,11 @@
  *******************************************************************************/
 package com.codenvy.ide.api.texteditor;
 
-import com.codenvy.ide.api.text.Document;
-import com.codenvy.ide.api.text.Region;
 
 /**
- * The interface of a document content formatter. The formatter formats ranges
- * within documents. The documents are modified by the formatter.<p>
- * The content formatter is assumed to determine the partitioning of the document
- * range to be formatted. For each partition, the formatter determines based
- * on the partition's content type the formatting strategy to be used. Before
- * the first strategy is activated all strategies are informed about the
- * start of the formatting process. After that, the formatting strategies are
- * activated in the sequence defined by the partitioning of the document range to be
- * formatted. It is assumed that a strategy must be finished before the next strategy
- * can be activated. After the last strategy has been finished, all strategies are
- * informed about the termination of the formatting process.</p>
- *
- * @author <a href="mailto:evidolob@exoplatform.com">Evgen Vidolob</a>
- * @version $Id:
+ * The interface of a document content formatter. The formatter formats ranges within documents.
  */
 public interface ContentFormatter {
-    /**
-     * Formats the given region of the specified document.The formatter may safely
-     * assume that it is the only subject that modifies the document at this point in time.
-     *
-     * @param document
-     *         the document to be formatted
-     * @param region
-     *         the region within the document to be formatted
-     */
-    void format(Document document, Region region);
+
+    // new interface must be defined
 }
