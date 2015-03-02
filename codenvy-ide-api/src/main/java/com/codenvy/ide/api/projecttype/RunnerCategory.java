79279f00939c4f3a36dd7b5b7e2d7be8d7b986f7
Dmitry Shnurenko <dshnurenko@codenvy.com>
2015-03-02 16:37:48 +0200

    IDEX-1920: Added runners categories

---
+++ codenvy-ide-api/src/main/java/com/codenvy/ide/api/projecttype/RunnerCategory.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.ide.api.projecttype;
+
+import javax.annotation.Nonnull;
+
+/**
+ * The class contains values of runner categories.
+ *
+ * @author Dmitry Shnurenko
+ */
+public enum RunnerCategory {
+    CPP("CPP"),
+    GO("GO"),
+    JAVA("JAVA"),
+    JAVASCRIPT("JAVASCRIPT"),
+    PHP("PHP"),
+    PYTHON("PYTHON"),
+    RUBY("RUBY");
+
+    private final String type;
+
+    RunnerCategory(@Nonnull String type) {
+        this.type = type;
+    }
+
+    @Nonnull
+    @Override
+    public String toString() {
+        return type;
+    }
+}
